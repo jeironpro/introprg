4b2489608a867731a5ddec9ba2cb07141ec3c976
Jeiron <jeironespinal0306@gmail.com>
2024-09-23 19:49:54 +0200

    Completat l'exercici en que em presento

---
+++ 00_05_presentemnos/EmPresento.java
@@ -0,0 +1,7 @@
+/* Programa que mostra una petita presentació */
+
+public class EmPresento {
+    public static void main(String[] args) {
+        System.out.println("Hola, sóc Jeiron, un futur bon desenvolupador!");
+    }
+}
