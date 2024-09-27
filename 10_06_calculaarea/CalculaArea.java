c94ebf5daac8d3c27d2f06592c099fcf0e66a152
prgtest <jeironespinal0306@gmail.com>
2024-09-27 12:13:04 +0200

    Completat l'exercici 10_06  ∾ prgtest autocommit 10_06 #3

--- 10_06_calculaarea/CalculaArea.java
+++ 10_06_calculaarea/CalculaArea.java
@@ -12,9 +12,9 @@
 public class CalculaArea {
     public static void main(String[] args) {
         System.out.println("Càlcul de l'àrea d'un cercle");
-        System.out.println("Introduïu el radi: ");
+        System.out.println("Introduïu el radi:");
         String linia = Entrada.readLine();
-        System.out.println("Introduïu les unitats (ex. cm): ");
+        System.out.println("Introduïu les unitats (ex. cm):");
         String unitats = Entrada.readLine();
         float radi = Float.parseFloat(linia);
         float area = (float)Math.PI * radi * radi;
