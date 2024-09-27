b2baadc12ed57623127045354a1246df2003f6f0
Jeiron <jeironespinal0306@gmail.com>
2024-09-27 12:10:59 +0200

    Completat l'exercici 10_06

---
+++ 10_06_calculaarea/CalculaArea.java
@@ -0,0 +1,23 @@
+/*
+* El programa calcula l'area d'un cercle agafant el radi i les unitats introduit per l'usuari.
+* Per Exemple: java CalculaArea
+ Càlcul de l'area d'un cercle
+ Introduïu el radi:
+ 2
+ Introduïu les unitats (ex. cm):
+ cm
+ L'area és 78.53975 cm^2
+*/
+
+public class CalculaArea {
+    public static void main(String[] args) {
+        System.out.println("Calcul de l'àrea d'un cercle");
+        System.out.println("Introduïu el radi: ");
+        String linia = Entrada.readLine();
+        System.out.println("Introduïu les unitats (ex. cm): ");
+        String unitats = Entrada.readLine();
+        float radi = Float.parseFloat(linia);
+        float area = (float)Math.PI * radi * radi;
+        System.out.println("L'area és " + area + unitats);
+    }
+}
