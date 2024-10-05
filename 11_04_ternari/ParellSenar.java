c64bd86b521177ea3d0f4491ebfb14368f325f34
Jeiron <jeironespinal0306@gmail.com>
2024-10-05 18:33:46 +0200

    Canviant de nom el arxiu

--- 11_04_ternari/ParellSenar.java
+++ 11_04_ternari/ParellSenar.java
@@ -4,7 +4,7 @@
 * Si no s'especifica cap número, el resultat és indeterminat.
 */
 
-public class ParellSenarTernari {
+public class ParellSenar {
     public static void main(String[] args) {
         int numero = Integer.parseInt(args[0]);
         
