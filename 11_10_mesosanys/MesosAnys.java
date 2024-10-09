7d2cab4a5842b5865cae27a60248f2b8c27a3d2c
Jeiron <jeironespinal0306@gmail.com>
2024-10-09 16:24:54 +0200

    Completat l'exercici 11_10

---
+++ 11_10_mesosanys/MesosAnys.java
@@ -0,0 +1,20 @@
+/* Aquest programa demana un mes i un any i et torna
+* un mes anterior i un mes següent.
+*/
+
+public class MesosAnys {
+    public static void main(String[] args) {
+        
+        System.out.println("Mes?");
+        int mes = Integer.parseInt(Entrada.readLine());
+        
+        System.out.println("Any?");
+        int any = Integer.parseInt(Entrada.readLine());
+        
+        if (mes == 1) {
+            System.out.println("el mes" + " " + mes + " " + "no té mes anterior" + " " + "i mes següent" + " " + (mes + 1) + "/" + any);
+        } else {
+            System.out.println("Mes anterior" + " " + (mes - 1) + "/" + any + " " + "i mes següent" + " " + (mes + 1) + "/" + any);
+        }
+    }
+}
