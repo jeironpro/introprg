1ad67a4363bcadc7964f6bfb4eb32d228f9dc7e1
Jeiron <jeironespinal0306@gmail.com>
2024-10-09 13:16:39 +0200

    Completat l'exercici 11_07

---
+++ 11_07_diessetmana/DiaSetmana.java
@@ -0,0 +1,25 @@
+/* Aqui va el comentari de que fas el programa */
+
+public class DiaSetmana {
+    public static void main(String[] args) {
+        int numero = Integer.parseInt(args[0]);
+        
+        if (numero == 1) {
+            System.out.println("Dilluns");
+        } else if (numero == 2) {
+            System.out.println("Dimarts");
+        } else if (numero == 3) {
+            System.out.println("Dimecres");
+        } else if (numero == 4) {
+            System.out.println("Dijous");
+        } else if (numero == 5) {
+            System.out.println("Divendres");
+        } else if (numero == 6) {
+            System.out.println("Dissabte");
+        } else if (numero == 7) {
+            System.out.println("Diumenge");
+        } else {
+            System.out.println("Error");
+        }
+    }
+}
