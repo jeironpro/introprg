8efbbf472b65347b889d424dfad6b763becadf05
prgtest <jeironespinal0306@gmail.com>
2024-10-11 15:14:14 +0200

    Completat l'exercici 11_17  ∾ prgtest autocommit 11_17 #5

--- 11_17_metge_virtual/MetgeVirtual.java
+++ 11_17_metge_virtual/MetgeVirtual.java
@@ -15,21 +15,21 @@ public class MetgeVirtual {
         System.out.println("problemes d'estómac? (sí o no)");
         String problemesEstomac = Entrada.readLine();
         
-        System.out.println("tos? (sí o no)");
-        String tos = Entrada.readLine();
+        /*System.out.println("tos? (sí o no)");
+        String tos = Entrada.readLine();*/
         
         System.out.println("edat?");
         int edat = Integer.parseInt(Entrada.readLine());
         
         if (esternut.equals("sí") && malCap.equals("sí") && problemesEstomac.equals("sí")) {
             System.out.println("pren paracetamol");
-        } else if (esternut.equals("sí") && malCap.equals("sí") && problemesEstomac.equals("no") && tos.equals("no") && edat > 12) {
+        } else if (esternut.equals("sí") && malCap.equals("sí") && problemesEstomac.equals("no") && edat > 12) {
             System.out.println("prendre àcid acetil salicílic");
-        } else if (esternut.equals("no") && malCap.equals("no") && problemesEstomac.equals("no") && tos.equals("sí") && edat < 12) {
+        } else if (esternut.equals("no") && malCap.equals("no") && problemesEstomac.equals("no") && edat < 12) {
             System.out.println("pren un carmel de mel");
-        } else if (esternut.equals("no") && malCap.equals("no") && problemesEstomac.equals("no") && tos.equals("sí") && edat > 12) {
+        } else if (esternut.equals("no") && malCap.equals("no") && problemesEstomac.equals("no") && edat > 12) {
             System.out.println("pren un carmel d'eucaliptus");
-        } else if (esternut.equals("no") && malCap.equals("no") && problemesEstomac.equals("no") && tos.equals("no")) {
+        } else if (esternut.equals("no") && malCap.equals("no") && problemesEstomac.equals("no")) {
             System.out.println("vine a la consulta");
         }
     }
