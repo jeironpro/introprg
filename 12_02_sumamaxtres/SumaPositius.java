c5927addbf3c14aabb146032c06414b237238293
Jeiron <jeironespinal0306@gmail.com>
2024-10-16 09:36:07 +0200

    Completat l'exercici 12_02

---
+++ 12_02_sumamaxtres/SumaPositius.java
@@ -0,0 +1,42 @@
+/* Aquest programa demani tres valors i només suma els positius 
+* ara nómes demani fins introduiex un número negatiu.
+*/
+
+public class SumaPositius {
+    public static void main(String[] args) {
+        
+        // creem la variable on guardarem el resultat de suma
+        int suma = 0;   // inicialment no hem sumat res i per tant és 0
+        
+        // demanem valors
+        
+        // declarem la variable que contindrá els valors llegits
+        int valor;
+        
+        // processem el primer valor
+        System.out.println("Introdueix el primer valor");
+        valor = Integer.parseInt(Entrada.readLine());
+        
+        if (valor >= 0) {
+            suma = suma + valor;
+            
+            // processem el segon valor
+            System.out.println("Introdueix el segon valor");
+            valor = Integer.parseInt(Entrada.readLine());
+        
+            if (valor >= 0) {
+                suma = suma + valor;
+                
+                System.out.println("Introdueix el tercer valor");
+                valor = Integer.parseInt(Entrada.readLine());
+                
+                if (valor >= 0) {
+                    suma = suma + valor;
+                }
+            }
+        }
+        
+        // mostrem el resultat
+        System.out.println("La suma és " + suma);
+    }   
+}
