c4af3d6a07fb8a8292a7ced604277f8e43eb608c
Jeiron <jeironespinal0306@gmail.com>
2024-10-18 17:09:00 +0200

    Completat l'exercici 12_07

---
+++ 12_07_mitjana/Mitjana.java
@@ -0,0 +1,30 @@
+/* Aquest programa demana les notes que has obtingut i les suma.
+* Si per primera vegada introduiexes un número negatiu et dirà que
+* Cap nota vàlida introduïda.  
+*/
+
+public class Mitjana {
+    public static void main(String[] args) {
+        System.out.println("Introdueix un valor");
+        double nota = Double.parseDouble(Entrada.readLine());   
+        double cantidadNota = 0;
+        double sumaNotes = 0;
+        
+        if (nota < 0) {
+            System.out.println("Cap nota vàlida introduïda");
+        }
+        
+        while (nota >= 0 && nota <= 100) {
+            if (nota >= 0 && nota <= 100) {
+                cantidadNota = cantidadNota + 1;
+                sumaNotes = sumaNotes + nota;
+            }
+            System.out.println("Introdueix un valor");
+            nota = Integer.parseInt(Entrada.readLine());            
+        }
+        
+        if (nota > 0) {
+            System.out.println("La mitjana de les notes vàlides és " + (sumaNotes / cantidadNota));    
+        }
+    }
+}
