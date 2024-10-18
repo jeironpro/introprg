7132d357146664df2a4a6ed2c68795571fc322c5
Jeiron <jeironespinal0306@gmail.com>
2024-10-18 14:35:51 +0200

    Completat l'exercici 12_06

---
+++ 12_06_trobamaxim/TrobaMaxim.java
@@ -0,0 +1,20 @@
+/**/
+
+public class TrobaMaxim {
+    public static void main(String[] args) {
+        System.out.println("Introdueix un valor");
+        int valor = Integer.parseInt(Entrada.readLine());
+        int valorMaxim = 0;
+        
+        while (valor >= 0) {
+            int valorCambiat = valor;
+            System.out.println("Introdueix un valor");
+            valor = Integer.parseInt(Entrada.readLine()); 
+            
+            if (valor >= 0 && valorCambiat >= valor) {
+                valorMaxim = valorCambiat;
+            }
+        }
+        System.out.println("El valor màxim és " + valorMaxim);
+    }
+}
