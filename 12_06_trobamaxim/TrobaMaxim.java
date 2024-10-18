11d27eb89f46aae155841d89ef76860bb0bcd203
prgtest <jeironespinal0306@gmail.com>
2024-10-18 16:21:20 +0200

    Completat l'exercici 12_06  ∾ prgtest autocommit 12_06 #29

--- 12_06_trobamaxim/TrobaMaxim.java
+++ 12_06_trobamaxim/TrobaMaxim.java
@@ -12,19 +12,7 @@ public class TrobaMaxim {
             }
             
             System.out.println("Introdueix un valor");
-            valor = Integer.parseInt(Entrada.readLine());  
-            
-           
-            /*
-            if (valor >= 0) {
-                if (valorCambiat >= valor) {
-                    valorMaxim = valorCambiat;            
-                } else {
-                    valorMaxim = valorMaxim;
-                }
-            } else if (valorCambiat >= 0 && valor < 0) {
-                valorMaxim = valorCambiat;
-            } */          
+            valor = Integer.parseInt(Entrada.readLine());            
         }
         System.out.println("El màxim és " + valorMaxim);
     }
