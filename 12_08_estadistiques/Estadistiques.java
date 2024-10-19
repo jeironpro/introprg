9c4ab1e2016f194a31586fbc9227d6554d66c16a
prgtest <jeironespinal0306@gmail.com>
2024-10-19 12:48:39 +0200

    Completat l'exercici 12_08  ∾ prgtest autocommit 12_08 #1

--- 12_08_estadistiques/Estadistiques.java
+++ 12_08_estadistiques/Estadistiques.java
@@ -5,28 +5,32 @@ public class Estadistiques {
         System.out.println("Introdueix un valor");
         int valor = Integer.parseInt(Entrada.readLine());
         int valorMaxim = 0;
-        int valorMinim = valor;
+        int valorMinim = 0;
         double sumaValor = 0;
         double cantidadValor = 0;
         
-        while (valor >= 0) {
-            if (valor >= valorMaxim) {
-                valorMaxim = valor;
-            } else if (valor < valorMinim && valorMinim <= valorMaxim) {
-                valorMinim = valor;
-            } else {
-                valorMinim = valorMinim;
+        if (valor < 0) {
+            System.out.println("Cap valor vàlid introduït");
+        } else {
+            while (valor >= 0) {
+                if (valor >= valorMaxim) {
+                    valorMaxim = valor;
+                } else if (valor < valorMinim && valorMinim <= valorMaxim) {
+                    valorMinim = valor;
+                } else {
+                    valorMinim = valorMinim;
+                }
+                
+                if (valor >= 0 && valor <= 100) {
+                    sumaValor = sumaValor + valor;
+                    cantidadValor = cantidadValor + 1; 
+                }
+                System.out.println("Introdueix un valor");
+                valor = Integer.parseInt(Entrada.readLine());
             }
-            
-            if (valor >= 0 && valor <= 100) {
-                sumaValor = sumaValor + valor;
-                cantidadValor = cantidadValor + 1; 
-            }
-            System.out.println("Introdueix un valor");
-            valor = Integer.parseInt(Entrada.readLine());
+            System.out.println("El mínim és: " + valorMinim);
+            System.out.println("La mitjana és: " + (sumaValor / cantidadValor));
+            System.out.println("El màxim és: " + valorMaxim);        
         }
-        System.out.println("El mínim és: " + valorMinim);
-        System.out.println("La mitjana és: " + (sumaValor / cantidadValor));
-        System.out.println("El màxim és: " + valorMaxim);
     }
 }
