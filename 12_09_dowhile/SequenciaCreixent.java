6cfcf696ef1c551be5b3283fabd7d5d5f9498224
prgtest <jeironespinal0306@gmail.com>
2024-10-19 14:38:35 +0200

    Completat l'exercici 12_09  ∾ prgtest autocommit 12_09 #1

--- 12_09_dowhile/SequenciaCreixent.java
+++ 12_09_dowhile/SequenciaCreixent.java
@@ -2,7 +2,7 @@
 
 public class SequenciaCreixent {
     public static void main(String[] args) {
-        System.out.println("Introdueix un valor");
+        System.out.println("Introdueix un valor:");
         int valorCreixent = Integer.parseInt(Entrada.readLine());
         int valor = 0;
         int valorsCreixents = 0;
@@ -13,7 +13,7 @@ public class SequenciaCreixent {
             }     
             valor = valorCreixent;  
               
-            System.out.println("Introdueix un valor");
+            System.out.println("Introdueix un valor:");
             valorCreixent = Integer.parseInt(Entrada.readLine());               
         } while (valorCreixent > valor);
         
