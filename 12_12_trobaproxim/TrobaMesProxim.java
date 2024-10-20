ecc0101c9f98cb340ef4cf26ef561c77654dbb98
prgtest <jeironespinal0306@gmail.com>
2024-10-20 18:00:06 +0200

    Completa l'exercici 12_12  ∾ prgtest autocommit 12_12 #54

--- 12_12_trobaproxim/TrobaMesProxim.java
+++ 12_12_trobaproxim/TrobaMesProxim.java
@@ -5,7 +5,6 @@ public class TrobaMesProxim {
         System.out.println("Introdueix l'àncora");
         int ancora = Integer.parseInt(Entrada.readLine());
         int valorMesProxim = 0;
-        int valorMaxim = 0;
         
         if (ancora < 0) {
             System.out.println("Àncora no vàlida");
@@ -18,13 +17,10 @@ public class TrobaMesProxim {
             } else {
                 while (valor >= 0) {
                 
-                    if (valor == (ancora - 1)) {
+                    if (valor == (ancora - 1) || valor < ancora) {
                         valorMesProxim = valor;
                     }
                     if (valorMesProxim == 0 && valor > ancora || valor == ancora) {
-                        valorMesProxim = valor;
-                    }
-                    if (valor < ancora && valorMesProxim > ancora) {
                         valorMesProxim = valor;
                     }
                     System.out.println("Introdueix un valor");
