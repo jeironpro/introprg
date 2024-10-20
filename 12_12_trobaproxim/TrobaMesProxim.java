6be11c8405745d2fadf1f0fbc8111d4587b8fbfe
prgtest <jeironespinal0306@gmail.com>
2024-10-20 14:47:52 +0200

    Completa l'exercici 12_12  ∾ prgtest autocommit 12_12 #1

--- 12_12_trobaproxim/TrobaMesProxim.java
+++ 12_12_trobaproxim/TrobaMesProxim.java
@@ -4,24 +4,27 @@ public class TrobaMesProxim {
     public static void main(String[] args) {
         System.out.println("Introdueix l'àncora");
         int ancora = Integer.parseInt(Entrada.readLine());
-        System.out.println("Introdueix un valor");
-        int valor = Integer.parseInt(Entrada.readLine());
         int valorMesProxim = 0;
         
         if (ancora < 0) {
             System.out.println("Àncora no vàlida");
-        } else if (valor < 0) {
-            System.out.println("No s'ha introduït cap valor positiu");
         } else {
-            while (valor >= 0) {
+            System.out.println("Introdueix un valor");
+            int valor = Integer.parseInt(Entrada.readLine());
+            
+            if (valor < 0) {
+                System.out.println("No s'ha introduït cap valor positiu");
+            } else {
+                while (valor >= 0) {
                 
-                if (valor >= valorMesProxim && valor < ancora) {
-                    valorMesProxim = valor;
+                    if (valor >= valorMesProxim && valor < ancora) {
+                        valorMesProxim = valor;
+                    }
+                    System.out.println("Introdueix un valor");
+                    valor = Integer.parseInt(Entrada.readLine());
                 }
-                System.out.println("Introdueix un valor");
-                valor = Integer.parseInt(Entrada.readLine());
+                System.out.println("El valor introduït més pròxim a " + ancora + " és " + valorMesProxim);
             }
-            System.out.println("El valor introduït més pròxim a " + ancora + " és " + valorMesProxim);         
-        }
+        }    
     }
 }
