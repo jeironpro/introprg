33ffd73fde0d7ed2c74a4ec34771c81fdfa4dbac
prgtest <jeironespinal0306@gmail.com>
2024-10-21 11:54:03 +0200

    Completa l'exercici 12_12  ∾ prgtest autocommit 12_12 #74

--- 12_12_trobaproxim/TrobaMesProxim.java
+++ 12_12_trobaproxim/TrobaMesProxim.java
@@ -26,7 +26,7 @@ public class TrobaMesProxim {
                     if (valorMesProxim == 0 && valor > ancora || valor == ancora) {
                         valorMesProxim = valor;
                     }
-                    if (valor > valor && valor < ancora && valor < valorMesProxim) {
+                    if (valor < ancora && valor < valorMesProxim) {
                         valorMesProxim = valor;
                     }
                     System.out.println("Introdueix un valor");
