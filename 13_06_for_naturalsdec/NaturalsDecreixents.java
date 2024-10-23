495850c6b49c98969c49d238a517a02274ae8f38
Jeiron <jeironespinal0306@gmail.com>
2024-10-23 17:45:22 +0200

    Completat l'exercici 13_06

---
+++ 13_06_for_naturalsdec/NaturalsDecreixents.java
@@ -0,0 +1,18 @@
+/* Aquest programa rep un nombre per la línia de comandes i et mostra
+* els números de manera decreixent des del nombre introduït fins 1.
+* Ara amb for.
+*/
+
+public class NaturalsDecreixents {
+    public static void main(String[] args) {
+        int numero = Integer.parseInt(args[0]);
+        
+        if (numero < 1) {
+            System.out.println("Cap valor decreixent entre " + numero + " i 1");
+        } else {
+            for (int i = 0; numero > i; numero--) {
+                System.out.println(numero);
+            }
+        }
+    }
+}
