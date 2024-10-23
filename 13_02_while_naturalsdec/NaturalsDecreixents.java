860e2283d349802df3f85d4783d9bf5aab8a2440
Jeiron <jeironespinal0306@gmail.com>
2024-10-23 15:04:50 +0200

    Completat l'exercici 13_02

---
+++ 13_02_while_naturalsdec/NaturalsDecreixents.java
@@ -0,0 +1,18 @@
+/* Aquest programa rep un nombre per la línia de comandes i et mostra
+* els números des del nombre introduït fins 1.
+*/
+
+public class NaturalsDecreixents {
+    public static void main(String[] args) {
+        int numero = Integer.parseInt(args[0]);
+        
+        if (numero < 1) {
+            System.out.println("Cap valor creixent entre 1 i " + numero);
+        } else {
+            while (numero > 0) {
+                System.out.println(numero);
+                numero--;
+            }
+        }
+    }
+}
