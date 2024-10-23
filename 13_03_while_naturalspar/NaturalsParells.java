9b791085225dda23f8891bf21b40afdbab659318
Jeiron <jeironespinal0306@gmail.com>
2024-10-23 15:18:49 +0200

    Completat l'exercici 13_03

---
+++ 13_03_while_naturalspar/NaturalsParells.java
@@ -0,0 +1,15 @@
+/* Aquest programa rep un nombre per la línia de comandes i et mostra
+* els números de 2 en 2 fins al nombre que li has introduit.
+*/
+
+public class NaturalsParells {
+    public static void main(String[] args) {
+        int numero = Integer.parseInt(args[0]);
+        int contador = 0;
+        
+        while (contador < numero) {
+            contador = contador + 2;
+            System.out.println(contador);
+        }
+    }
+}
