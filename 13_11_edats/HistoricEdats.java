d258e35ff4c27decaaad0e03a9896615e5f7006e
prgtest <jeironespinal0306@gmail.com>
2024-10-24 10:58:10 +0200

    Completat l'exercici 13_11  ∾ prgtest autocommit 13_11 #4

--- 13_11_edats/HistoricEdats.java
+++ 13_11_edats/HistoricEdats.java
@@ -6,42 +6,33 @@ public class HistoricEdats {
     public static void main(String[] args) {
         System.out.println("nom?");
         String nom = Entrada.readLine();
+        System.out.println("edat?");
+        int edat = Integer.parseInt(Entrada.readLine());
+        System.out.println("any actual?");
+        int anyActual = Integer.parseInt(Entrada.readLine());
         
-        if (nom.isBlank()) {
+        
+        
+        if (nom.isBlank() || edat < 0 || anyActual < 1971) {
             System.out.println("Entrada incorrecta");
         } else {
-            System.out.println("edat?");
-            int edat = Integer.parseInt(Entrada.readLine());
-            
-            if (edat < 0) {
-                System.out.println("Entrada incorrecta");
-            } else {
-                System.out.println("any actual?");
-                int anyActual = Integer.parseInt(Entrada.readLine());
+            if (edat > 0) {
+                int anyNaixement = anyActual - edat;
+                System.out.println("El " + anyNaixement + " vau néixer");                  
+                edat = 0;
                 
-                if (anyActual < 1971) {
-                    System.out.println("Entrada incorrecta");
-                } else { 
-                    if (edat > 0) {
-                        int anyNaixement = anyActual - edat;
-                        System.out.println("El " + anyNaixement + " vau néixer");                  
-                        edat = 0;
-                        
-                        for (int i = anyNaixement + 1; i < anyActual; i++) {
-                            edat = edat + 1;
-                            if (edat == 1) {
-                                System.out.println("El " + i + " teníeu " + edat + " any");
-                            } else {
-                                System.out.println("El " + i + " teníeu " + edat + " anys");  
-                            }
-                        }
-                        System.out.println("Adéu " + nom);           
+                for (int i = anyNaixement + 1; i < anyActual; i++) {
+                    edat = edat + 1;
+                    if (edat == 1) {
+                        System.out.println("El " + i + " teníeu " + edat + " any");
                     } else {
-                        System.out.println("Adéu " + nom); 
+                        System.out.println("El " + i + " teníeu " + edat + " anys");  
                     }
                 }
+                System.out.println("Adéu " + nom);           
+            } else {
+                System.out.println("Adéu " + nom); 
             }
-            
         }
     }
 }
