223994838dff837a3c812b88df76710f1b16d93d
prgtest <jeironespinal0306@gmail.com>
2024-10-24 11:03:43 +0200

    Completat l'exercici 13_11  ∾ prgtest autocommit 13_11 #10

--- 13_11_edats/HistoricEdats.java
+++ 13_11_edats/HistoricEdats.java
@@ -16,19 +16,23 @@ public class HistoricEdats {
         if (nom.isBlank() || edat < 0 || anyActual < 1971) {
             System.out.println("Entrada incorrecta");
         } else {
-            int anyNaixement = anyActual - edat;
-            System.out.println("El " + anyNaixement + " vau néixer");                  
-            edat = 0;
-            
-            for (int i = anyNaixement + 1; i < anyActual; i++) {
-                edat = edat + 1;
-                if (edat == 1) {
-                    System.out.println("El " + i + " teníeu " + edat + " any");
-                } else {
-                    System.out.println("El " + i + " teníeu " + edat + " anys");  
+            if (edat > 0) {
+                int anyNaixement = anyActual - edat;
+                System.out.println("El " + anyNaixement + " vau néixer");                  
+                edat = 0;
+                
+                for (int i = anyNaixement + 1; i < anyActual; i++) {
+                    edat = edat + 1;
+                    if (edat == 1) {
+                        System.out.println("El " + i + " teníeu " + edat + " any");
+                    } else {
+                        System.out.println("El " + i + " teníeu " + edat + " anys");  
+                    }
                 }
+                System.out.println("Adéu " + nom);           
+            } else {
+                System.out.println("Adéu " + nom); 
             }
-            System.out.println("Adéu " + nom);
         }
     }
 }
