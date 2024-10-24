67445f822d8adfd75b1a42aee693bce11b2ee6f5
prgtest <jeironespinal0306@gmail.com>
2024-10-24 11:53:37 +0200

    Completat l'exercici 13_11  ∾ prgtest autocommit 13_11 #11

--- 13_11_edats/HistoricEdats.java
+++ 13_11_edats/HistoricEdats.java
@@ -11,28 +11,25 @@ public class HistoricEdats {
         System.out.println("any actual?");
         int anyActual = Integer.parseInt(Entrada.readLine());
         
-        
-        
         if (nom.isBlank() || edat < 0 || anyActual < 1971) {
             System.out.println("Entrada incorrecta");
         } else {
+            int anyNaixement = anyActual - edat;
             if (edat > 0) {
-                int anyNaixement = anyActual - edat;
-                System.out.println("El " + anyNaixement + " vau néixer");                  
-                edat = 0;
-                
-                for (int i = anyNaixement + 1; i < anyActual; i++) {
-                    edat = edat + 1;
-                    if (edat == 1) {
-                        System.out.println("El " + i + " teníeu " + edat + " any");
-                    } else {
-                        System.out.println("El " + i + " teníeu " + edat + " anys");  
-                    }
-                }
-                System.out.println("Adéu " + nom);           
-            } else {
-                System.out.println("Adéu " + nom); 
+                System.out.println("El " + anyNaixement + " vau néixer"); 
             }
+                        
+            edat = 0;
+            for (int i = anyNaixement + 1; i < anyActual; i++) {
+                edat = edat + 1;
+                
+                if (edat == 1) {
+                    System.out.println("El " + i + " teníeu " + edat + " any");
+                } else {
+                    System.out.println("El " + i + " teníeu " + edat + " anys");  
+                }
+            }
+            System.out.println("Adéu " + nom); 
         }
     }
 }
