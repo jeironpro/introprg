bddb899b7c3e0850738aac33b38a442a7ab0a5de
Jeiron <jeironespinal0306@gmail.com>
2024-10-28 14:40:58 +0100

    Completat l'exercici 13_12

---
+++ 13_12_quadrat/QuadratNombres.java
@@ -0,0 +1,22 @@
+/* Aquest programa rep valors enters des de l'1 fins a 9, i et mostra un quadrat de nombres
+* fins al nombre introduït. Si s'introdueix valors menor a 1 o major a 9,
+* et mostrarà un missatge de error.
+ */
+
+public class QuadratNombres {
+    public static void main(String[] args) {
+        System.out.println("Introdueix un valor enter entre 1 i 9");
+        int valor = Integer.parseInt(Entrada.readLine());
+        
+        if (valor < 0 || valor > 9) {
+            System.out.println("Valor no vàlid");
+        } else {
+            for (int i = 1; i <= valor; i++) {
+                for (int j = 1; j <= valor; j++) {
+                    System.out.print(j);
+                }
+                System.out.println();
+            }        
+        }
+    }
+}
