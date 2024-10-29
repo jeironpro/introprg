9e46adde996c7f19e8924a9dbb33a685c9672699
prgtest <jeironespinal0306@gmail.com>
2024-10-29 08:03:27 +0100

    Completa l'exercici 13_14  ∾ prgtest autocommit 13_14 #2

--- 13_14_triangleinvertit/TriangleInvertit.java
+++ 13_14_triangleinvertit/TriangleInvertit.java
@@ -19,11 +19,7 @@ public class TriangleInvertit {
         } else {
             for (int i = valor; i >= 1; i--) {
                 for (int j = 1; j <= i; j++) {
-                    if (j <= 4) {
-                        System.out.print(j + ", ");
-                    } else {
-                        System.out.print(j);
-                    }
+                    System.out.print(j + ", ");
                 }
                 System.out.println();
             }
