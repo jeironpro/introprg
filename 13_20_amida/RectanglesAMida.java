ebe7e026a58e85e6887b1e8512af932ad31efb57
prgtest <jeironespinal0306@gmail.com>
2024-11-04 17:26:37 +0100

    Completat l'exercici 13_20  ∾ prgtest autocommit 13_20 #3

--- 13_20_amida/RectanglesAMida.java
+++ 13_20_amida/RectanglesAMida.java
@@ -14,30 +14,37 @@ public class RectanglesAMida {
         int rectangle = 0;
         int punts = 0;
         int contadorPunts = 0;
+        boolean continuar = true;
         
-        while (!columna.isBlank()) {
-            int valorColumna = Integer.parseInt(columna);
-            if (valorColumna > 0) {
-                System.out.print("  ");
-                for (int j = 0; j < valorColumna; j++) {
-                    System.out.print(j);
-                }
-                for (int i = 0; i < fila; i++) {
-                    System.out.println();
-                    System.out.print(i + " "); 
-                    contadorPunts = fila * valorColumna;
-                    for (int k = 0; k < valorColumna; k++) {
-                        System.out.print("*");
+        while (continuar) {
+            if (!columna.isBlank()) {
+                int valorColumna = Integer.parseInt(columna);
+                if (valorColumna > 0) {
+                    System.out.print("  ");
+                    for (int j = 0; j < valorColumna; j++) {
+                        System.out.print(j);
                     }
+                    for (int i = 0; i < fila; i++) {
+                        System.out.println();
+                        System.out.print(i + " "); 
+                        contadorPunts = fila * valorColumna;
+                        for (int k = 0; k < valorColumna; k++) {
+                            System.out.print("*");
+                        }
+                    }
+                    System.out.println();
+                    fila = valorColumna;
+                    System.out.println(fila + " x ?");
+                    columna = Entrada.readLine();
+                    rectangle++; 
+                    punts += contadorPunts;     
+                } else {
+                    continuar = false;
+                    System.out.println("Rectangles: " + rectangle);
+                    System.out.println("Punts: " + punts);
                 }
-                System.out.println();
-                fila = valorColumna;
-                System.out.println(fila + " x ?");
-                columna = Entrada.readLine();
-                rectangle++; 
-                punts += contadorPunts;     
             } else {
-                columna = "";
+                continuar = false;
                 System.out.println("Rectangles: " + rectangle);
                 System.out.println("Punts: " + punts);
             }
