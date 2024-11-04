a109b183eae36798b4e58ff234f930276c163062
prgtest <jeironespinal0306@gmail.com>
2024-11-04 17:20:48 +0100

    Completat l'exercici 13_20  ∾ prgtest autocommit 13_20 #1

--- 13_20_amida/RectanglesAMida.java
+++ 13_20_amida/RectanglesAMida.java
@@ -36,10 +36,10 @@ public class RectanglesAMida {
                 columna = Entrada.readLine();
                 rectangle++; 
                 punts += contadorPunts;     
-            } else {
-                columna = "";
                 System.out.println("Rectangles: " + rectangle);
                 System.out.println("Punts: " + punts);
+            } else {
+                columna = "";
             }
         }
     }
