9594a911ba8f36175718a90a639ab0439a7e2806
prgtest <jeironespinal0306@gmail.com>
2024-11-11 12:20:17 +0100

    Completat l'exercici 14_02  ∾ prgtest autocommit 14_02 #1

--- 14_02_informestring/InformeString.java
+++ 14_02_informestring/InformeString.java
@@ -16,39 +16,39 @@ public class InformeString {
         int num = Integer.parseInt(Entrada.readLine());
         
         // Obtenir la longitud (nombre de caràcters) del text principal.
-        System.out.println(textPrincipal.length());
+        System.out.println("textPrincipal.length(): " + textPrincipal.length());
         
         // Verificar si el text principal comença per el text secundari.
-        System.out.println(textPrincipal.startsWith(textSecundari));
+        System.out.println("textPrincipal.startsWith(textSecundari): " + textPrincipal.startsWith(textSecundari));
         
         // Verificar si el text principal acaba per el text secundari.
-        System.out.println(textPrincipal.endsWith(textSecundari));
+        System.out.println("textPrincipal.endsWith(textSecundari): " + textPrincipal.endsWith(textSecundari));
         
         // Verificar si el text principal és equal que el text secundari.
-        System.out.println(textPrincipal.equals(textSecundari));
+        System.out.println("textPrincipal.equals(textSecundari): " + textPrincipal.equals(textSecundari));
         
         // Verificar si el text principal és equal que el text secundari ignorant les majúscules.
-        System.out.println(textPrincipal.equalsIgnoreCase(textSecundari));
+        System.out.println("textPrincipal.equalsIgnoreCase(textSecundari): " + textPrincipal.equalsIgnoreCase(textSecundari));
         
         // Verificar si el text principal està buit.
-        System.out.println(textPrincipal.isBlank());
+        System.out.println("textPrincipal.isBlank(): " + textPrincipal.isBlank());
         
         // Verificar si el text principal està buit.
-        System.out.println(textPrincipal.isEmpty());
+        System.out.println("textPrincipal.isEmpty(): " + textPrincipal.isEmpty());
         
         // Verificar quina lletra està a la posició num.
-        System.out.println(textPrincipal.charAt(num));
+        System.out.println("textPrincipal.charAt(num): " + textPrincipal.charAt(num));
         
         // Unir (sumar-li) text principla amb text secundari.
-        System.out.println(textPrincipal.concat(textSecundari));
+        System.out.println("textPrincipal.concat(textSecundari): " + textPrincipal.concat(textSecundari));
         
         // Repeteix text principal el número de vegades indicat per num.
-        System.out.println(textPrincipal.repeat(num));
+        System.out.println("textPrincipal.repeat(num): " + textPrincipal.repeat(num));
         
         // Convertir text principal amb totes les lletres en majúscules.
-        System.out.println(textPrincipal.toUpperCase());
+        System.out.println("textPrincipal.toUpperCase(): " + textPrincipal.toUpperCase());
         
         // Convertir text principal amb totes les lletres en minúscules.
-        System.out.println(textPrincipal.toLowerCase());
+        System.out.println("textPrincipal.toLowerCase(): " + textPrincipal.toLowerCase());
     }
 }
