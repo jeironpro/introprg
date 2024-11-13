9b1d4eb2954ac5ede1545ad1c2f06e7600577d8a
prgtest <jeironespinal0306@gmail.com>
2024-11-13 12:11:51 +0100

    Completat l'exercici 14_03  ∾ prgtest autocommit 14_03 #2

--- 14_03_informecaracter/InformeCaracter.java
+++ 14_03_informecaracter/InformeCaracter.java
@@ -15,34 +15,40 @@ public class InformeCaracter {
         // Obtenir el primer carácter del text
         char caracter = text.charAt(posicio);
         
-        // Obtenir la descripció del caracter
-        System.out.println("Character.getName('" + caracter + "'): " + Character.getName(caracter));
-        
-        // Verificar si es un digit
-        System.out.println("Character.isDigit('" + caracter + "'): " + Character.isDigit(caracter));
-        
-        // Verificar si pots ser l'inici d'un identificador vàlid per Java
-        System.out.println("Character.isJavaIdentifierStart('" + caracter + "'): " + Character.isJavaIdentifierStart(caracter));
-        
-        // Verificar si pots ser part d'un identificador vàlid per Java
-        System.out.println("Character.isJavaIdentifierPart('" + caracter + "'): " + Character.isJavaIdentifierPart(caracter));
-        
-        // Verificar si el caracter és un lletra
-        System.out.println("Character.isLetter('" + caracter + "'): " + Character.isLetter(caracter));
-        
-        // Verificar si és minúscula
-        System.out.println("Character.isLowerCase('" + caracter + "'): " + Character.isLowerCase(caracter));
-        
-        // Verificar si és majúscula
-        System.out.println("Character.isUpperCase('" + caracter + "'): " + Character.isUpperCase(caracter));
-        
-        // Verificar si correspon amb un espai blanc
-        System.out.println("Character.isWhitespace('" + caracter + "'): " + Character.isWhitespace(caracter));
-        
-        // Convertir de majúscula a minúscula
-        System.out.println("Character.toLowerCase('" + caracter + "'): " + Character.toLowerCase(caracter));
-        
-        // Convertir de minúscula a majúscula
-        System.out.println("Character.toUpperCase('" + caracter + "'): " +  Character.toUpperCase(caracter));
+        if (text.isBlank()) {
+            System.out.println("Error");
+        } else if (posicio > text.length()) {
+            System.out.println("Fora de rang");
+        } else {
+            // Obtenir la descripció del caracter
+            System.out.println("Character.getName('" + caracter + "'): " + Character.getName(caracter));
+            
+            // Verificar si es un digit
+            System.out.println("Character.isDigit('" + caracter + "'): " + Character.isDigit(caracter));
+            
+            // Verificar si pots ser l'inici d'un identificador vàlid per Java
+            System.out.println("Character.isJavaIdentifierStart('" + caracter + "'): " + Character.isJavaIdentifierStart(caracter));
+            
+            // Verificar si pots ser part d'un identificador vàlid per Java
+            System.out.println("Character.isJavaIdentifierPart('" + caracter + "'): " + Character.isJavaIdentifierPart(caracter));
+            
+            // Verificar si el caracter és un lletra
+            System.out.println("Character.isLetter('" + caracter + "'): " + Character.isLetter(caracter));
+            
+            // Verificar si és minúscula
+            System.out.println("Character.isLowerCase('" + caracter + "'): " + Character.isLowerCase(caracter));
+            
+            // Verificar si és majúscula
+            System.out.println("Character.isUpperCase('" + caracter + "'): " + Character.isUpperCase(caracter));
+            
+            // Verificar si correspon amb un espai blanc
+            System.out.println("Character.isWhitespace('" + caracter + "'): " + Character.isWhitespace(caracter));
+            
+            // Convertir de majúscula a minúscula
+            System.out.println("Character.toLowerCase('" + caracter + "'): " + Character.toLowerCase(caracter));
+            
+            // Convertir de minúscula a majúscula
+            System.out.println("Character.toUpperCase('" + caracter + "'): " +  Character.toUpperCase(caracter));
+        }
     }
 }
