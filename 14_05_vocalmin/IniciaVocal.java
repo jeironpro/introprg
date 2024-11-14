a8619375f8e2a9c0a7132be09ff3126bae757608
Jeiron <jeironespinal0306@gmail.com>
2024-11-14 15:20:43 +0100

    Completat l'exercici 14_05

---
+++ 14_05_vocalmin/IniciaVocal.java
@@ -0,0 +1,15 @@
+/*  */
+
+public class IniciaVocal {
+    public static void main(String[] args) {
+        System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules");
+        String paraula = Entrada.readLine();
+        
+        while (!paraula.isEmpty()) {
+            System.out.print((paraula.charAt(0) == 'a' || paraula.charAt(0) == 'e' || paraula.charAt(0) == 'i' || paraula.charAt(0) == 'o' || paraula.charAt(0) == 'u' ? "El lloro diu: " + paraula + "\n" : ""));
+            System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules");
+            paraula = Entrada.readLine();
+        }
+        System.out.println("Adéu");
+    }
+}
