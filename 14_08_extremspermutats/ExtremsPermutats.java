c7a71b814757ad36eee440c2cea8c1fe33f15594
prgtest <jeironespinal0306@gmail.com>
2024-11-15 15:06:18 +0100

    Completat l'exercici 14_08  ∾ prgtest autocommit 14_08 #27

--- 14_08_extremspermutats/ExtremsPermutats.java
+++ 14_08_extremspermutats/ExtremsPermutats.java
@@ -20,13 +20,13 @@ public class ExtremsPermutats {
                 if (paraula.length() > 1) {
                     // Verificar si el index 0 és una lletra
                     if (Character.isLetter(paraula.charAt(0)) && Character.isLetter(paraula.charAt(1)) && Character.isLetter(paraula.charAt(paraula.length()-1)) && Character.isLetter(paraula.charAt(paraula.length()-2))) {
+                        // Convertir les lletres en minùscules
+                        char primeraLletra = Character.toLowerCase(paraula.charAt(0));
+                        char segonaLletra = Character.toLowerCase(paraula.charAt(1));
+                        char penultimaLletra = Character.toLowerCase(paraula.charAt(paraula.length()-1));
+                        char ultimaLletra = Character.toLowerCase(paraula.charAt(paraula.length()-2));
                         // Verificar si la primera i la segona lletra de la paraula són iguals a la penúltima i a l'última lletra respectivament sense importar el ordre 
-                        char primeraLletra = Character.toUpperCase(paraula.charAt(0));
-                        char segonaLletra = Character.toUpperCase(paraula.charAt(1));
-                        char penultimaLletra = Character.toUpperCase(paraula.charAt(paraula.length()-1));
-                        char ultimaLletra = Character.toUpperCase(paraula.charAt(paraula.length()-2));
-                        if ((primeraLletra == penultimaLletra || primeraLletra == ultimaLletra)
-                        && (segonaLletra == penultimaLletra || segonaLletra == ultimaLletra)) {
+                        if ((primeraLletra == penultimaLletra || primeraLletra == ultimaLletra) && (segonaLletra == penultimaLletra || segonaLletra == ultimaLletra)) {
                             System.out.println("Repeteix: " + paraula);
                         }
                     }
