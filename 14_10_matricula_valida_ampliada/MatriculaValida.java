09711ad6c8b44e6ae786573afa510e506cf9910e
Jeiron <jeironespinal0306@gmail.com>
2024-11-15 17:22:28 +0100

    Ara si, completat l'exercici 14_10

--- 14_10_matricula_valida_ampliada/MatriculaValida.java
+++ 14_10_matricula_valida_ampliada/MatriculaValida.java
@@ -22,46 +22,69 @@ public class MatriculaValida {
             System.out.println("És una matrícula italiana vàlida");
             } else {
                 System.out.println("No és una matrícula italiana vàlida");
-                if (Character.isLetter(primeraLletra) && primeraLletra >= 'A' && primeraLletra <= 'Z' && primeraLletra != 'I' && primeraLletra != 'O' && primeraLletra != 'Q' && primeraLletra != 'U' && Character.isUpperCase(primeraLletra)) {
-                    System.out.println(primeraLletra + ": Correcte");
+                // Verificar que la primera lletra compleixi totes les condicions per ser vàlida
+                if (Character.isDigit(primeraLletra) || primeraLletra < 'A' || primeraLletra > 'Z') {
+                    System.out.println(primeraLletra + ": Ha de ser una lletra");
+                } else if (Character.isLowerCase(primeraLletra)) {
+                    System.out.println(primeraLletra + ": Ha de ser majúscula");
+                } else if (primeraLletra == 'I' || primeraLletra == 'O' || primeraLletra == 'Q' || primeraLletra == 'U') {
+                    System.out.println(primeraLletra + ": No és una lletra vàlida");
                 } else {
-                    System.out.println(primeraLletra + ": Ha de ser majúscula");                    
+                    System.out.println(primeraLletra + ": Correcte");                    
                 }
                 
-                if (Character.isLetter(segonaLletra) && segonaLletra >= 'A' && segonaLletra <= 'Z' && segonaLletra != 'I' && segonaLletra != 'O' && segonaLletra != 'Q' && segonaLletra != 'U' && Character.isUpperCase(segonaLletra)) {
-                    System.out.println(segonaLletra + ": Correcte");
+                // Verificar que la segona lletra compleixi totes les condicions per ser vàlida
+                if (Character.isDigit(segonaLletra) || segonaLletra < 'A' || segonaLletra > 'Z') {
+                    System.out.println(segonaLletra + ": Ha de ser una lletra");
+                } else if (Character.isLowerCase(segonaLletra)) {
+                    System.out.println(segonaLletra + ": Ha de ser majúscula");
+                } else if (segonaLletra == 'I' || segonaLletra == 'O' || segonaLletra == 'Q' || segonaLletra == 'U') {
+                    System.out.println(segonaLletra + ": No és una lletra vàlida");
                 } else {
-                    System.out.println(segonaLletra + ": Ha de ser una majúscula");                
+                    System.out.println(segonaLletra + ": Correcte");                    
                 }
                 
-                if (Character.isDigit(primerNum)) {
-                    System.out.println(primerNum + ": Correcte");  
-                } else {
+                // Vericar si el primer digit compleixi totes les condicions per ser vàlid
+                if (Character.isLetter(primerNum)) {
                     System.out.println(primerNum + ": Ha de ser un digit");
+                } else {
+                    System.out.println(primerNum + ": Correcte");  
                 }
                 
-                if (Character.isDigit(segonNum)) {
-                    System.out.println(segonNum + ": Correcte");  
-                } else {
+                // Vericar si el segon digit compleixi totes les condicions per ser vàlid
+                if (Character.isLetter(segonNum)) {
                     System.out.println(segonNum + ": Ha de ser un digit");
+                } else {
+                    System.out.println(segonNum + ": Correcte");  
                 }
                 
-                if (Character.isDigit(tercerNum)) {
-                    System.out.println(tercerNum + ": Correcte");  
-                } else {
+                // Vericar si el tercer digit compleixi totes les condicions per ser vàlid
+                if (Character.isLetter(tercerNum)) {
                     System.out.println(tercerNum + ": Ha de ser un digit");
+                } else {
+                    System.out.println(tercerNum + ": Correcte");  
                 }
                 
-                if (Character.isLetter(terceraLletra) && terceraLletra >= 'A' && terceraLletra <= 'Z' && terceraLletra != 'I' && terceraLletra != 'O' && terceraLletra != 'Q' && terceraLletra != 'U' && Character.isUpperCase(terceraLletra)) {
-                    System.out.println(terceraLletra + ": Correcte");
+                // Verificar que la tercera lletra compleixi totes les condicions per ser vàlida
+                if (Character.isDigit(terceraLletra) || terceraLletra < 'A' || terceraLletra > 'Z') {
+                    System.out.println(terceraLletra + ": Ha de ser una lletra");
+                } else if (Character.isLowerCase(terceraLletra)) {
+                    System.out.println(terceraLletra + ": Ha de ser majúscula");
+                } else if (terceraLletra == 'I' || terceraLletra == 'O' || terceraLletra == 'Q' || terceraLletra == 'U') {
+                    System.out.println(terceraLletra + ": No és una lletra vàlida");
                 } else {
-                    System.out.println(terceraLletra + ": Ha de ser majúscula");                    
+                    System.out.println(terceraLletra + ": Correcte");                    
                 }
                 
-                if (Character.isLetter(quartaLletra) && quartaLletra >= 'A' && quartaLletra <= 'Z' && quartaLletra != 'I' && quartaLletra != 'O' && quartaLletra != 'Q' && quartaLletra != 'U' && Character.isUpperCase(quartaLletra)) {
-                    System.out.println(quartaLletra + ": Correcte");
+                // Verificar que la tercera lletra compleixi totes les condicions per ser vàlida
+                if (Character.isDigit(quartaLletra) || quartaLletra < 'A' || quartaLletra > 'Z') {
+                    System.out.println(quartaLletra + ": Ha de ser una lletra");
+                } else if (Character.isLowerCase(quartaLletra)) {
+                    System.out.println(quartaLletra + ": Ha de ser majúscula");
+                } else if (quartaLletra == 'I' || quartaLletra == 'O' || quartaLletra == 'Q' || quartaLletra == 'U') {
+                    System.out.println(quartaLletra + ": No és una lletra vàlida");
                 } else {
-                    System.out.println(quartaLletra + ": Ha de ser una majúscula");                
+                    System.out.println(quartaLletra + ": Correcte");                    
                 }
             }
         } else {
