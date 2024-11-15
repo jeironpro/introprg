e6614d9514f3836a9bee99dbe3c4ebf3557d557a
Jeiron <jeironespinal0306@gmail.com>
2024-11-15 21:06:50 +0100

    Completat l'exercici 14_13

---
+++ 14_13_nomeslletres/NomesLletres.java
@@ -0,0 +1,18 @@
+/* Aquest programa demana un text i et mostra nomes les lletres separats per una coma i un espai */
+
+public class NomesLletres {
+    public static void main(String[] args) {
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        
+         for (int i = 0; i < text.length(); i++) {
+            if (i < text.length()-1) {
+                if (Character.isLetter(text.charAt(i))) {
+                    System.out.print(text.charAt(i) + ", ");                            
+                }
+            } else {
+                System.out.print(text.charAt(i));    
+            }
+        }
+    }
+}
