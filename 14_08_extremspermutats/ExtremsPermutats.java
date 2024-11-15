fdd03e40b08c4a939790aa60ca7cefab79b6b1a3
prgtest <jeironespinal0306@gmail.com>
2024-11-15 11:53:54 +0100

    Completat l'exercici 14_08  ∾ prgtest autocommit 14_08 #17

--- 14_08_extremspermutats/ExtremsPermutats.java
+++ 14_08_extremspermutats/ExtremsPermutats.java
@@ -6,24 +6,27 @@
 public class ExtremsPermutats {
     public static void main(String[] args) {
         System.out.println("Ves introduïnt texts (finalitza amb INTRO sol)");
-        boolean continuar = true;
-        String guardaParaula = "";
-        int cantidadParaula = 0;
+        String paraula = "si";
         
-        while (continuar) {
-            String paraula = Entrada.readLine();
+        while (!paraula.isEmpty()) {
+            paraula = Entrada.readLine();
             
-            if (!paraula.isEmpty() && Character.isLetter(paraula.charAt(0)) && (paraula.charAt(paraula.length()-1) == paraula.charAt(0) || paraula.charAt(paraula.length()-1) == paraula.charAt(1) && paraula.charAt(paraula.length()-2) == paraula.charAt(0) || paraula.charAt(paraula.length()-2) == paraula.charAt(1))) {
-                cantidadParaula += 1;
-                guardaParaula = paraula;
+            // Verificar si la paraula no és buida
+            if (!paraula.isEmpty()) {
+                // Verificar si la paraula té una longitud de més de quatre lletra
+                if (paraula.length() >= 4) {
+                    // Verificar si el index 0 és una lletra
+                    if (Character.isLetter(paraula.charAt(0))) {
+                        // Verificar si la primera i la segona lletra de la paraula són iguals a la penúltima i a l'última lletra respectivament sense importar el ordre 
+                        if ((paraula.charAt(paraula.length()-1) == paraula.charAt(0) || paraula.charAt(paraula.length()-1) == paraula.charAt(1) && paraula.charAt(paraula.length()-2) == paraula.charAt(0) || paraula.charAt(paraula.length()-2) == paraula.charAt(1))) {
+                                System.out.println("Repeteix: " + paraula);
+                        }
+                    }
+                
+                }
+            } else {
+                paraula = "";
             }
-            
-            if (paraula.isEmpty()) {
-                continuar = false;
-            }
-        }
-        for (int i = cantidadParaula; i > 0; i--) {
-            System.out.println("Repeteix: " + guardaParaula);                
         }
         System.out.println("Adéu");
     }
