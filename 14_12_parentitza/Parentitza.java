717da46d84acce9068dff6ef0478dfb70847c08e
Jeiron <jeironespinal0306@gmail.com>
2024-11-15 20:55:57 +0100

    Completat l'exercici 14_12

---
+++ 14_12_parentitza/Parentitza.java
@@ -0,0 +1,20 @@
+/* Aquest programa demana un text i mostra cada lletra en parèntesis */
+
+public class Parentitza {
+    public static void main(String[] args) {
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        
+        for (int i = 0; i < text.length(); i++) {
+            if (i < text.length()-1) {
+                if (Character.isLetter(text.charAt(i))) {
+                    System.out.print("("+ text.charAt(i) +")");                            
+                } else {
+                    System.out.print(text.charAt(i));       
+                } 
+            } else {
+                System.out.print(text.charAt(i));    
+            }
+        }
+    }
+}
