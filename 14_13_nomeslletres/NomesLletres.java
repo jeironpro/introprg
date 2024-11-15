adc391fc46fbd31a4b53f6578e834d72c0f1dcc5
prgtest <jeironespinal0306@gmail.com>
2024-11-15 22:05:49 +0100

    Completat l'exercici 14_13  ∾ prgtest autocommit 14_13 #27

--- 14_13_nomeslletres/NomesLletres.java
+++ 14_13_nomeslletres/NomesLletres.java
@@ -8,10 +8,10 @@ public class NomesLletres {
          for (int i = 0; i < text.length(); i++) {
             if (Character.isLetter(text.charAt(i))) {
                 for (int j = i; j <= i; j++) {
-                    if (j < text.length()-1) {
-                        System.out.print(text.charAt(j) + ", ");                              
-                    } else {
+                    if (j == 1) {
                         System.out.print(text.charAt(j)); 
+                    } else {
+                        System.out.print(", " + text.charAt(j));                              
                     }
                 }
             }
