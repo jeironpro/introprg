c740f2c35f5ef62b040b586cae415fb6fb6074a4
prgtest <jeironespinal0306@gmail.com>
2024-11-17 00:09:26 +0100

    Completat l'exercici 14_17  ∾ prgtest autocommit 14_17 #3

--- 14_17_enterestricte/Enters.java
+++ 14_17_enterestricte/Enters.java
@@ -13,22 +13,17 @@ public class Enters {
             text = Entrada.readLine();
             
             // Verificar si el text és buit
-            if (text.isEmpty()) {
-                // Finalitzar el programa
-                text = "";
-                System.out.println("Adéu");
+            if (!text.isEmpty()) {
+                if (text.charAt(0) == '-' || Character.isDigit(text.charAt(0))) {
+                    System.out.println("És enter");
+                } else {
+                    System.out.println("No és enter");
+                }
             // Del contrari, fer la verificació si és enter o no
             } else {
-                // Si comença per lletra o un espai en blanc i si acaba amb un espai no és un enter
-                if (Character.isLetter(text.charAt(0)) || Character.isWhitespace(text.charAt(0)) || Character.isWhitespace(text.charAt(text.length()-1))) {
-                    System.out.println("No és enter"); 
-                // Del contrari, és un enter                   
-                } else {
-                    int enter = Integer.parseInt(text);
-                    if (enter < 0 || enter > 0) {
-                        System.out.println("És enter");               
-                    }
-                }
+                // Finalitzar el programa
+                text = "";
+                System.out.println("Adéu");             
             }
         }
     }
