48759ae791251759ecdec6b2961e07a347b6efe2
prgtest <jeironespinal0306@gmail.com>
2024-11-17 01:09:34 +0100

    Completat l'exercici 14_17  ∾ prgtest autocommit 14_17 #26

--- 14_17_enterestricte/Enters.java
+++ 14_17_enterestricte/Enters.java
@@ -14,7 +14,9 @@ public class Enters {
             
             // Verificar si el text és buit
             if (!text.isEmpty()) {
-                if (!Character.isWhitespace(text.charAt(0)) && !Character.isWhitespace(text.charAt(text.length()-1))) {
+                if ((text.charAt(0) == '-' || text.charAt(0) == '+') ||
+                (!Character.isWhitespace(text.charAt(0)) && !Character.isWhitespace(text.charAt(text.length()-1)))
+                ) {
                     int enter = Integer.parseInt(text);
                     System.out.println("És enter");                
                 } else {
