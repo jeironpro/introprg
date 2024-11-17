df0f1ac303ebe226a2284d1490957a4165e0cc21
prgtest <jeironespinal0306@gmail.com>
2024-11-17 01:31:59 +0100

    Completat l'exercici 14_17  ∾ prgtest autocommit 14_17 #31

--- 14_17_enterestricte/Enters.java
+++ 14_17_enterestricte/Enters.java
@@ -22,7 +22,7 @@ public class Enters {
                 !Character.isWhitespace(text.charAt(0)) && 
                 !Character.isWhitespace(text.charAt(text.length()-1))) || 
                 (Character.isDigit(text.charAt(0)) && 
-                Character.isDigit(text.charAt(text.length()-1)) && !text.contains(".") || !text.contains("_"))
+                Character.isDigit(text.charAt(text.length()-1)) && !text.contains(".") && !text.contains("_"))
                 ) 
                 {
                     System.out.println("És enter");                
