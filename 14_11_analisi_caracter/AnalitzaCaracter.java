a92fa59ff1208c7408941fed799ab167175abd91
prgtest <jeironespinal0306@gmail.com>
2024-11-17 15:18:40 +0100

    Completat l'exercici 14_11  ∾ prgtest autocommit 14_11 #1

--- 14_11_analisi_caracter/AnalitzaCaracter.java
+++ 14_11_analisi_caracter/AnalitzaCaracter.java
@@ -5,55 +5,60 @@ public class AnalitzaCaracter {
         System.out.println("Text?");
         String text = Entrada.readLine();
         
-        System.out.println("Posició?");
-        int posicio = Integer.parseInt(Entrada.readLine());
-        
-        if (posicio < 0) {
-            posicio = text.length() + Math.abs(posicio);
-            while (posicio >= text.length()-1) {
-                posicio--;
-            }
-            System.out.print("'" + text.charAt(posicio) + "'");
-            if (Character.isLetter(text.charAt(posicio))) {
-                if (Character.isUpperCase(text.charAt(posicio))) {
-                    System.out.println(" és una lletra majúscula");
+        if (!text.isEmpty()) {
+            System.out.println("Posició?");
+            int posicio = Integer.parseInt(Entrada.readLine());
+            
+            if (posicio < 0) {
+                posicio = text.length() + Math.abs(posicio);
+                while (posicio >= text.length()-1) {
+                    posicio--;
+                }
+                System.out.print("'" + text.charAt(posicio) + "'");
+                if (Character.isLetter(text.charAt(posicio))) {
+                    if (Character.isUpperCase(text.charAt(posicio))) {
+                        System.out.println(" és una lletra majúscula");
+                    } else {
+                        System.out.println(" és una lletra minúscula");
+                    }            
+                } else if (Character.isDigit(text.charAt(posicio))) {
+                    System.out.println(" és un digit");
                 } else {
-                    System.out.println(" és una lletra minúscula");
-                }            
-            } else if (Character.isDigit(text.charAt(posicio))) {
-                System.out.println(" és un digit");
-            } else {
-                System.out.println(" és un sìmbol");
-            }
-        } else if (posicio >= text.length()) {
-            while(posicio > text.length()-1) {
-                text = text + text;
-            }
-            System.out.print("'" + text.charAt(posicio) + "'");
-            if (Character.isLetter(text.charAt(posicio))) {
-                if (Character.isUpperCase(text.charAt(posicio))) {
-                    System.out.println(" és una lletra majúscula");
+                    System.out.println(" és un sìmbol");
+                }
+            } else if (posicio >= text.length()) {
+                while(posicio > text.length()-1) {
+                    text = text + text;
+                }
+                System.out.print("'" + text.charAt(posicio) + "'");
+                if (Character.isLetter(text.charAt(posicio))) {
+                    if (Character.isUpperCase(text.charAt(posicio))) {
+                        System.out.println(" és una lletra majúscula");
+                    } else {
+                        System.out.println(" és una lletra minúscula");
+                    }            
+                } else if (Character.isDigit(text.charAt(posicio))) {
+                    System.out.println(" és un digit");
                 } else {
-                    System.out.println(" és una lletra minúscula");
-                }            
-            } else if (Character.isDigit(text.charAt(posicio))) {
-                System.out.println(" és un digit");
+                    System.out.println(" és un sìmbol");
+                }
             } else {
-                System.out.println(" és un sìmbol");
-            }
+                System.out.print("'" + text.charAt(posicio) + "'");
+                if (Character.isLetter(text.charAt(posicio))) {
+                    if (Character.isUpperCase(text.charAt(posicio))) {
+                        System.out.println(" és una lletra majúscula");
+                    } else {
+                        System.out.println(" és una lletra minúscula");
+                    }            
+                } else if (Character.isDigit(text.charAt(posicio))) {
+                    System.out.println(" és un digit");
+                } else {
+                    System.out.println(" és un sìmbol");
+                }
+            }            
         } else {
-            System.out.print("'" + text.charAt(posicio) + "'");
-            if (Character.isLetter(text.charAt(posicio))) {
-                if (Character.isUpperCase(text.charAt(posicio))) {
-                    System.out.println(" és una lletra majúscula");
-                } else {
-                    System.out.println(" és una lletra minúscula");
-                }            
-            } else if (Character.isDigit(text.charAt(posicio))) {
-                System.out.println(" és un digit");
-            } else {
-                System.out.println(" és un sìmbol");
-            }
+            System.out.println("Text buit");
         }
+        
     }
 }
