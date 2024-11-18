3fc2d84a2c29d3d2ecff14e044f0f5074ee265d6
prgtest <jeironespinal0306@gmail.com>
2024-11-18 17:14:41 +0100

    Completat l'exercici 14_11  ∾ prgtest autocommit 14_11 #55

--- 14_11_analisi_caracter/AnalitzaCaracter.java
+++ 14_11_analisi_caracter/AnalitzaCaracter.java
@@ -26,7 +26,7 @@ public class AnalitzaCaracter {
                     caracter += textInvertit.charAt(Math.abs(posicio-1));
                 } else {
                     posicio = posicio % text.length();
-                    caracter += textInvertit.charAt(posicio-1);
+                    caracter += textInvertit.charAt(Math.abs(posicio-1));
                 }
             } else if (posicio < text.length()) {
                 caracter += text.charAt(posicio);                     
