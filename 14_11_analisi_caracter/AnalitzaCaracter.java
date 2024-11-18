942309caf42fc89a7683a735365502ad6ea5f365
prgtest <jeironespinal0306@gmail.com>
2024-11-18 12:32:53 +0100

    Completat l'exercici 14_11  ∾ prgtest autocommit 14_11 #1

--- 14_11_analisi_caracter/AnalitzaCaracter.java
+++ 14_11_analisi_caracter/AnalitzaCaracter.java
@@ -1,9 +1,11 @@
 /**/
 
+
 public class AnalitzaCaracter {
     public static void main(String[] args) {
         System.out.println("Text?");
         String text = Entrada.readLine();
+        String caracter = "";
         
         if (!text.isEmpty()) {
             System.out.println("Posició?");
@@ -11,12 +13,24 @@ public class AnalitzaCaracter {
             
             if (posicio < 0) {
                 posicio = Math.abs(posicio) % text.length();
-                System.out.println(text.charAt(posicio));
+                caracter += text.charAt(posicio);
             } else if (posicio < text.length()) {
-                System.out.println(text.charAt(posicio));                     
+                caracter += text.charAt(posicio);                     
             } else if (posicio > text.length()) {
                 posicio = posicio % text.length();
-                System.out.println(text.charAt(posicio));
+                caracter += text.charAt(posicio);
+            }
+            
+            if (Character.isLetter(caracter.charAt(0))) {
+                if (Character.isUpperCase(caracter.charAt(0))) {
+                    System.out.println("'" + caracter + "'" + " és una lletra majúscula");
+                } else {
+                    System.out.println("'" + caracter + "'" + " és una lletra minúscula");
+                }
+            } else if (Character.isDigit(caracter.charAt(0))) {
+                System.out.println("'" + caracter + "'" + " és un digit");
+            } else {
+                System.out.println("'" + caracter + "'" + " és una altra cosa");
             }
         } else {
             System.out.println("Text buit");
