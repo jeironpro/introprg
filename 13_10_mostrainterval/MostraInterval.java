0bb3f162d8f20e85d0da4f292adbb90a5a770bd9
Jeiron <jeironespinal0306@gmail.com>
2024-11-18 21:39:31 +0100

    Canviant l'estructura

--- 13_10_mostrainterval/MostraInterval.java
+++ 13_10_mostrainterval/MostraInterval.java
@@ -4,19 +4,27 @@
 
 public class MostraInterval {
     public static void main(String[] args) {
+        // Demanar el prime valor
         System.out.println("inici?");
         int inici = Integer.parseInt(Entrada.readLine());
+        // Demanar el segon valor
         System.out.println("final?");
         int finall = Integer.parseInt(Entrada.readLine());
         
-        if (inici > finall) {
-            for (int i = inici; i >= finall; i--) {
+        // Verificar si el valor inici és menor a valor finall
+        if (inici < finall) {
+            // Com es menor fer un for creixent
+            for (int i = inici; i <= finall; i++) {
+                // Mostrar el valor de manera creixent des de valor inici fins a valor finall
                 System.out.println(i);
             }
+        // Del contrari, valor inici és major a valor finall
         } else {
-            for (int j = inici; j <= finall; j++) {
-                System.out.println(j);
-            }        
+            // Fer un for decreixent
+            for (int i = inici; i >= finall; i--) {
+                // Mostrar el valor de manera decreixent des de valor inici fins a valor finall
+                System.out.println(i);
+            }
         }
     }
 }
