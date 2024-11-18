7bb3e81d830be1f711d45d75ecd7ef0e420856e0
prgtest <jeironespinal0306@gmail.com>
2024-11-18 23:30:06 +0100

    Canviant l'estructura  ∾ prgtest autocommit 14_09 #24

--- 14_09_matricula_italiana/MatriculaValida.java
+++ 14_09_matricula_italiana/MatriculaValida.java
@@ -5,28 +5,28 @@ public class MatriculaValida {
         System.out.println("Introduïu una matrícula");
         String matricula = Entrada.readLine();
         
-        if (matricula.length() == 7) {
-            if (Character.isUpperCase(matricula.charAt(0))) {
-                if (matricula.charAt(0) >= 'A' && matricula.charAt(0) <= 'Z' && matricula.charAt(0) != 'I' &&matricula.charAt(0) != 'O' && matricula.charAt(0) != 'Q' && matricula.charAt(0) != 'U') {
-                    if (Character.isUpperCase(matricula.charAt(1))) {
-                        if (matricula.charAt(1) >= 'A' && matricula.charAt(1) <= 'Z' && matricula.charAt(1) != 'I' && matricula.charAt(1) != 'O' && matricula.charAt(1) != 'Q' && matricula.charAt(1) != 'U') {
-                            if (Character.isDigit(matricula.charAt(2)) && Character.isDigit(matricula.charAt(3)) && Character.isDigit(matricula.charAt(4))) {
-                                if (Character.isUpperCase(matricula.charAt(5))) {
-                                    if (matricula.charAt(5) >= 'A' && matricula.charAt(5) <= 'Z' && matricula.charAt(5) != 'I' && matricula.charAt(5) != 'O' && matricula.charAt(5) != 'Q' && matricula.charAt(5) != 'U') {
-                                        if (Character.isUpperCase(matricula.charAt(6))) {
-                                            if (matricula.charAt(6) >= 'A' && matricula.charAt(6) <= 'Z' && matricula.charAt(6) != 'I' && matricula.charAt(6) != 'O' && matricula.charAt(6) != 'Q' && matricula.charAt(6) != 'U') {        
-                                                System.out.println("És una matrícula italiana vàlida");
-                                            } else {
-                                                System.out.println("No és una matrícula italiana vàlida");
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+        // Puc fer if anitats però hauria de posar un else a cada if per dir que no és una matrícula vàlida
+        // He intentat fer-ho amb un bucle for però no va funcionar
+        // Es que aquesta estructura no està bé
+        if (matricula.length() == 7 && 
+            Character.isUpperCase(matricula.charAt(0)) && 
+            matricula.charAt(0) >= 'A' && matricula.charAt(0) <= 'Z' && 
+            matricula.charAt(0) != 'I' &&matricula.charAt(0) != 'O' && matricula.charAt(0) != 'Q' && matricula.charAt(0) != 'U' && 
+            Character.isUpperCase(matricula.charAt(1)) && 
+            matricula.charAt(1) >= 'A' && matricula.charAt(1) <= 'Z' && 
+            matricula.charAt(1) != 'I' && matricula.charAt(1) != 'O' && matricula.charAt(1) != 'Q' && matricula.charAt(1) != 'U' && 
+            Character.isDigit(matricula.charAt(2)) && 
+            Character.isDigit(matricula.charAt(3)) && 
+            Character.isDigit(matricula.charAt(4)) && 
+            Character.isUpperCase(matricula.charAt(5)) && 
+            matricula.charAt(5) >= 'A' && matricula.charAt(5) <= 'Z' && 
+            matricula.charAt(5) != 'I' && matricula.charAt(5) != 'O' && 
+            matricula.charAt(5) != 'Q' && matricula.charAt(5) != 'U' && 
+            Character.isUpperCase(matricula.charAt(6)) && 
+            matricula.charAt(6) >= 'A' && matricula.charAt(6) <= 'Z' && 
+            matricula.charAt(6) != 'I' && matricula.charAt(6) != 'O' && 
+            matricula.charAt(6) != 'Q' && matricula.charAt(6) != 'U') {        
+            System.out.println("És una matrícula italiana vàlida");
         } else {
             System.out.println("No és una matrícula italiana vàlida");
         }
