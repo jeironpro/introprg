2c42a38ef6865cf40aa699c4d9220482fc159075
prgtest <jeironespinal0306@gmail.com>
2024-11-18 21:54:38 +0100

    Canviant l'estructura  ∾ prgtest autocommit 14_05 #2

--- 14_05_vocalmin/IniciaVocal.java
+++ 14_05_vocalmin/IniciaVocal.java
@@ -14,7 +14,6 @@ public class IniciaVocal {
             if (paraula.charAt(0) == 'a' || paraula.charAt(0) == 'e' || paraula.charAt(0) == 'i' || paraula.charAt(0) == 'o' || paraula.charAt(0) == 'u') {
                 // Repetir la paraula
                 System.out.println("El lloro diu: " + paraula);
-            // Del contrari, no és vocal
             }
             // Demanar paraula a dins del bucle
             System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules");
