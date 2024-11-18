be431e8f6f0096b5fdca6be37a4b1fed0e8a3e03
Jeiron <jeironespinal0306@gmail.com>
2024-11-18 20:30:51 +0100

    Simplificant el codi

--- 12_04_quantsparells/QuantsParells.java
+++ 12_04_quantsparells/QuantsParells.java
@@ -4,20 +4,25 @@
 
 public class QuantsParells {
     public static void main(String[] args) {
+        // Declarar e inicialitzar la variable valor amb 0
         int valor = 0;
+        // Declarar e inicialitzar la variable valorParell amb 0
+        // Aquesta variable emmagatzema la quantitat de vegades que el bucle troba un numero parell
         int valorParell = 0;
         
-        
+        // El bucle s'executarà mentra el valor sigui positiu
         while (valor >= 0) {
+            // Demanar el valor
             System.out.println("Introdueix un valor");
             valor = Integer.parseInt(Entrada.readLine());
             
-            if (valor >= 0) {
-                if (valor % 2 == 0) {
-                    valorParell = valorParell + 1;
-                }
+            // Verificar si el valor és parell
+            if (valor % 2 == 0) {
+                // Sumar un cada vegades que trobe un valor parell
+                valorParell = valorParell + 1;
             }
         }
+        // Mostrar la quantitat de números parell trobats
         System.out.println("Nombre de parells introduïts: " + valorParell);
     }
 }
