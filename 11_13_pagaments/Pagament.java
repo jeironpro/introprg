fccaaf068f756353fce9c00d1d714b69559ecd15
Jeiron <jeironespinal0306@gmail.com>
2024-11-18 19:47:26 +0100

    Simplificant el codi

--- 11_13_pagaments/Pagament.java
+++ 11_13_pagaments/Pagament.java
@@ -6,16 +6,14 @@ public class Pagament {
     public static void main(String[] args) {
         System.out.println("Preu?");
         int preu = Integer.parseInt(Entrada.readLine());
+        
         System.out.println("Paga?");
         int paga = Integer.parseInt(Entrada.readLine());
-        int resultat;
         
         if (preu < paga) {
-            resultat = paga - preu;
-            System.out.println("Sobren " + resultat + "€");
+            System.out.println("Sobren " + (paga - preu) + "€");
         } else if (preu > paga) {
-            resultat = preu - paga;
-            System.out.println("Falten " + resultat + "€");
+            System.out.println("Falten " + (preu - paga) + "€");
         } else {
             System.out.println("No sobra ni falta res");
         }
