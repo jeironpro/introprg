23f9b8c570f1aee1d89c81d04108880cd2a5fd8e
prgtest <jeironespinal0306@gmail.com>
2024-11-18 23:06:32 +0100

    Canviant l'estructura  ∾ prgtest autocommit 14_09 #8

--- 14_09_matricula_italiana/MatriculaValida.java
+++ 14_09_matricula_italiana/MatriculaValida.java
@@ -10,13 +10,9 @@ public class MatriculaValida {
         
         if (matricula.length() == 7) {
             for (int i = 0; i < matricula.length(); i++) {
-                if (i >= 0 && i <= 1) {
+                if (i >= 0 && i <= 1 && i >= 5 && i <= 6) {
                     if (Character.isUpperCase(matricula.charAt(i)) && matricula.charAt(i) >= 'A' && matricula.charAt(i) <= 'Z' && matricula.charAt(i) != 'I' && matricula.charAt(i) != 'O' && matricula.charAt(i) != 'Q' && matricula.charAt(i) != 'U') {
-                        if (i >= 5 && i <= 6) {
-                            if (Character.isUpperCase(matricula.charAt(i)) && matricula.charAt(i) >= 'A' && matricula.charAt(i) <= 'Z' && matricula.charAt(i) != 'I' && matricula.charAt(i) != 'O' && matricula.charAt(i) != 'Q' && matricula.charAt(i) != 'U') {
-                                primerPatro = true;
-                            }
-                        }
+                        primerPatro = true;
                     }
                 }
                 if (i >= 2 && i <=4) {
@@ -26,7 +22,7 @@ public class MatriculaValida {
                 }
                 
             }        
-            if (primerPatro == true && segonPatro == true) {
+            if (primerPatro == true && segonPatro == true && tercerPatro == true) {
                 System.out.println("És una matrícula italiana vàlida"); 
             } else {
                 System.out.println("No és una matrícula italiana vàlida");
