86f6b2c9d5a4d2463082301d101bc03c6afe3b28
prgtest <jeironespinal0306@gmail.com>
2024-11-18 23:15:16 +0100

    Canviant l'estructura  ∾ prgtest autocommit 14_09 #18

--- 14_09_matricula_italiana/MatriculaValida.java
+++ 14_09_matricula_italiana/MatriculaValida.java
@@ -10,17 +10,17 @@ public class MatriculaValida {
         
         if (matricula.length() == 7) {
             for (int i = 0; i < matricula.length(); i++) {
-                if (i == 0 && i == 1) {
+                if (i >= 0) {
                     if (Character.isUpperCase(matricula.charAt(i)) && matricula.charAt(i) >= 'A' && matricula.charAt(i) <= 'Z' && matricula.charAt(i) != 'I' && matricula.charAt(i) != 'O' && matricula.charAt(i) != 'Q' && matricula.charAt(i) != 'U') {
                         primerPatro = true;
                     }                
                 }
-                if (i >= 2 && i <= 4) {
+                if (i >= 4) {
                     if (Character.isDigit(matricula.charAt(i))) {
                         segonPatro = true;
                     }
                 }
-                if (i >= 5 && i <= 6) {
+                if (i >= 5) {
                     if (Character.isUpperCase(matricula.charAt(i)) && matricula.charAt(i) >= 'A' && matricula.charAt(i) <= 'Z' && matricula.charAt(i) != 'I' && matricula.charAt(i) != 'O' && matricula.charAt(i) != 'Q' && matricula.charAt(i) != 'U') {
                         tercerPatro = true;
                     }                
