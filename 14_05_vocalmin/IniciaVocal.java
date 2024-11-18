3725a8aafbbecd85da3b46410db7b5f44e13c534
Jeiron <jeironespinal0306@gmail.com>
2024-11-18 21:52:21 +0100

    Canviant l'estructura

--- 14_05_vocalmin/IniciaVocal.java
+++ 14_05_vocalmin/IniciaVocal.java
@@ -4,14 +4,24 @@
 
 public class IniciaVocal {
     public static void main(String[] args) {
+        // Demanar paraula
         System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules");
         String paraula = Entrada.readLine();
         
+        // El bucle s'executarà mentre la paraula no està buida
         while (!paraula.isEmpty()) {
-            System.out.print((paraula.charAt(0) == 'a' || paraula.charAt(0) == 'e' || paraula.charAt(0) == 'i' || paraula.charAt(0) == 'o' || paraula.charAt(0) == 'u' ? "El lloro diu: " + paraula + "\n" : ""));
-            System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules");
-            paraula = Entrada.readLine();
+            // Veficar si la paraula comença amb vocal
+            if (paraula.charAt(0) == 'a' || paraula.charAt(0) == 'e' || paraula.charAt(0) == 'i' || paraula.charAt(0) == 'o' || paraula.charAt(0) == 'u') {
+                // Repetir la paraula
+                System.out.println("El lloro diu: " + paraula);
+            // Del contrari, no és vocal
+            } else {
+                // Demanar paraula a dins del bucle
+                System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules");
+                paraula = Entrada.readLine();            
+            }
         }
+        // Quan surti del bucle, mostrar aquest missatge
         System.out.println("Adéu");
     }
 }
