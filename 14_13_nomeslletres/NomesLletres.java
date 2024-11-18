2812c709c4823a7df1755fc220692adbf70aa398
prgtest <jeironespinal0306@gmail.com>
2024-11-18 23:47:33 +0100

    Canviant l'estructura  ∾ prgtest autocommit 14_13 #2

--- 14_13_nomeslletres/NomesLletres.java
+++ 14_13_nomeslletres/NomesLletres.java
@@ -2,16 +2,30 @@
 
 public class NomesLletres {
     public static void main(String[] args) {
+        // Demanar el text
         System.out.println("Text?");
         String text = Entrada.readLine();
+        // Declarar e inicialitzar la variable nouText buida
+        String nouText = "";
         
+        // Fer un for per iterar tots el caràcter del text
         for (int i = 0; i < text.length(); i++) {
+            // Verificar si els caràcters són lletres
             if (Character.isLetter(text.charAt(i))) {
-                if (i < text.length()-1) {
-                    System.out.print(text.charAt(i) + ", ");
-                } else {
-                    System.out.print(text.charAt(i-1));
-                }
+                // Com són lletres, guardar-lo en la variable nouText
+                nouText += text.charAt(i);
+            }
+        }
+        // Fer un for per iterar tots els caràcters del nou text.
+        for (int j = 0; j < nouText.length(); j++) {
+            // Verificar si j és menor a la longitud del text - 1
+            if (j < nouText.length()-1) {
+                // Com és així mostrar tots el caracter separat amb coma en aquesta longitud
+                System.out.print(nouText.charAt(j) + ", ");
+            // Del contrari, es igual a la longitud
+            } else {
+                // Mostrar el caràcter final
+                System.out.print(nouText.charAt(j));
             }
         }
     }
