67b7054061d0449127d224abfc76eb58d409ece8
Jeiron <jeironespinal0306@gmail.com>
2024-11-18 19:44:59 +0100

    Simplificant el codi

--- 11_12_calculadora_senzilla/CalculadoraSenzilla.java
+++ 11_12_calculadora_senzilla/CalculadoraSenzilla.java
@@ -7,15 +7,15 @@ public class CalculadoraSenzilla {
     public static void main(String[] args) {
         System.out.println("Operand1?");
         double operandUno = Double.parseDouble(Entrada.readLine());
+        
         System.out.println("Operador?");
         char operador = Entrada.readLine().charAt(0);
+        
         System.out.println("Operand2?");
         double operandDos = Double.parseDouble(Entrada.readLine());
-        double resultat; 
         
         if (operador == '+') {
-            resultat = operandUno + operandDos;
-            System.out.println(operandUno + " + " + operandDos + " = " + resultat);
+            System.out.println(operandUno + " + " + operandDos + " = " + (operandUno + operandDos));
         } else if (operador == '-') {
             System.out.println(operandUno + " - " + operandDos + " = " + (operandUno - operandDos));
         } else if (operador == '*') {
