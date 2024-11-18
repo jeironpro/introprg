f94187baf6104e0dfb3a2db8b7f80a8d8a6ae46f
prgtest <jeironespinal0306@gmail.com>
2024-11-18 23:05:16 +0100

    Canviant l'estructura  ∾ prgtest autocommit 14_09 #7

--- 14_09_matricula_italiana/MatriculaValida.java
+++ 14_09_matricula_italiana/MatriculaValida.java
@@ -6,25 +6,31 @@ public class MatriculaValida {
         String matricula = Entrada.readLine();
         boolean primerPatro = false;
         boolean segonPatro = false;
+        boolean tercerPatro = false;
         
         if (matricula.length() == 7) {
             for (int i = 0; i < matricula.length(); i++) {
-                if (i == 0 && i == 1 && i == 5 && i == 6) {
+                if (i >= 0 && i <= 1) {
                     if (Character.isUpperCase(matricula.charAt(i)) && matricula.charAt(i) >= 'A' && matricula.charAt(i) <= 'Z' && matricula.charAt(i) != 'I' && matricula.charAt(i) != 'O' && matricula.charAt(i) != 'Q' && matricula.charAt(i) != 'U') {
-                        primerPatro = true;
+                        if (i >= 5 && i <= 6) {
+                            if (Character.isUpperCase(matricula.charAt(i)) && matricula.charAt(i) >= 'A' && matricula.charAt(i) <= 'Z' && matricula.charAt(i) != 'I' && matricula.charAt(i) != 'O' && matricula.charAt(i) != 'Q' && matricula.charAt(i) != 'U') {
+                                primerPatro = true;
+                            }
+                        }
                     }
                 }
-                if (i == 2 && i == 3 && i == 4) {
+                if (i >= 2 && i <=4) {
                     if (Character.isDigit(matricula.charAt(i))) {
                         segonPatro = true;
                     }
                 }
+                
             }        
-        }
-        if (primerPatro == true && segonPatro == true) {
-            System.out.println("És una matrícula italiana vàlida"); 
-        } else {
-            System.out.println("No és una matrícula italiana vàlida");
+            if (primerPatro == true && segonPatro == true) {
+                System.out.println("És una matrícula italiana vàlida"); 
+            } else {
+                System.out.println("No és una matrícula italiana vàlida");
+            }
         }
     }
 }
