9cd1ec1bb0bff0432af5d0879253368850dec5f7
Jeiron <jeironespinal0306@gmail.com>
2024-11-18 19:20:53 +0100

    Simplificant el codi

--- 11_04_ternari/ParellSenar.java
+++ 11_04_ternari/ParellSenar.java
@@ -6,12 +6,13 @@
 
 public class ParellSenar {
     public static void main(String[] args) {
+        // Obtenir el argumento 0 (primer) de la línia de comanda
         int numero = Integer.parseInt(args[0]);
         
-        String parell = "és parell";
-        String senar = "és senar";
-        String resultat = (numero % 2 == 0) ? parell : senar;
+        // Operador ternari que verifica si un nombre és parell o senar
+        String resultat = (numero % 2 == 0) ? "El número " + numero + " és parell" : "El número " + numero + " és senar";
         
-        System.out.println("El número " + numero + " " + resultat);
+        // Mostrar resultat
+        System.out.println(resultat);
     }
 }
