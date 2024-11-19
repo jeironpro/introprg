5430cb870dbd010c0e098ab2c74f1c1ee988b68f
prgtest <jeironespinal0306@gmail.com>
2024-11-19 12:18:54 +0100

    Eliminant l'ús de bucle  ∾ prgtest autocommit 14_11 #4

--- 14_11_analisi_caracter/AnalitzaCaracter.java
+++ 14_11_analisi_caracter/AnalitzaCaracter.java
@@ -25,7 +25,7 @@ public class AnalitzaCaracter {
             // Verificar si la posició es negativa
             if (posicio < 0) {
                 // Convertir a positiva
-                posicio = Math.abs(posicio) % text.length();
+                posicio = text.length() - posicio;
                 // Obtenir el residu de posicio entre la longitud del text 
                 posicio = posicio % text.length();
                 
