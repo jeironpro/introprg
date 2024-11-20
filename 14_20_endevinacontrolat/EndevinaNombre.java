2b6c4b0ad7e680a9314e0da702e64589c3c3f7f9
prgtest <jeironespinal0306@gmail.com>
2024-11-20 17:20:36 +0100

    Completat l'exercici 14_20  ∾ prgtest autocommit 14_20 #20

--- 14_20_endevinacontrolat/EndevinaNombre.java
+++ 14_20_endevinacontrolat/EndevinaNombre.java
@@ -12,19 +12,31 @@
 
 public class EndevinaNombre {
     public static void main(String[] args) {
+        // Demanar nombre
         System.out.println("Nombre?");
         String cadena = Entrada.readLine();
+        // Prendre el primer argument de la línia de comanda
         int valorEndevinar = Integer.parseInt(args[0]);
+        // Declarar e inicialitzar la variable valor amb 0
         int valor = 0;
+        // Declarar e incialitzar enter com false
         boolean enter = false;
-           
+        
+        // Utilitzant un bucle infinit que acaba amb break
         while (true) {
+            // Verificar que la cadena no està buida
             if (!cadena.isEmpty()) {
+                // Fer un for per iterar tots els caràcter de la cadena
                 for (int i = 0; i < cadena.length(); i++) {
+                    // Verificar si tots el caràcter són dígits
                     if (Character.isDigit(cadena.charAt(i))) {
+                        // Com són dígits convertir a int
                         valor = Integer.parseInt(cadena);
+                        // i enter és true
                         enter = true;
+                    // Del contrari
                     } else {
+                        // enter és false
                         enter = false;
                     }
                 }
@@ -36,23 +48,34 @@ public class EndevinaNombre {
                         enter = true;
                     }                            
                 }
-                
+                // Verificar si enter és false
                 if (!enter) {
                     System.out.println("Només nombres");
+                // Verificar si el valor està fora del rang
                 } else if (valor <= 0 || valor > 100) {
-                    System.out.println("Fora de rang");                    
+                    // Mostrar aquest missatge
+                    System.out.println("Fora de rang"); 
+                // Verificar si el valor és major al valor a endevinar                   
                 } else if (valor > valorEndevinar) {
+                    // Mostrar aquest missatge
                     System.out.println("Massa gran");
+                // Verificar si el valor és menor al valor a endevinar
                 } else if (valor < valorEndevinar) {
+                    // Mostrar aquest missatge
                     System.out.println("Massa petit");
                 } else {
+                    // Mostrar aquest missatge
                     System.out.println("Encertat!");
+                    // Finalitzar el programa
                     break;
                 }
             } else {
+                // Mostrar aquest missatge
                 System.out.println("Cancel·lat!");
+                // Finalitzar el programa
                 break;            
             }
+            // Tornar a demanar la cadena a dins del bucle
             System.out.println("Nombre?");
             cadena = Entrada.readLine();
         }
