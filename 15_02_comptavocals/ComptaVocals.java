ff7e4be0f3921af5f7b767fd12772bafa11783d8
prgtest <jeironespinal0306@gmail.com>
2024-11-21 14:43:40 +0100

    Completat lèxercici 15_02  ∾ prgtest autocommit 15_02 #1

--- 15_02_comptavocals/ComptaVocals.java
+++ 15_02_comptavocals/ComptaVocals.java
@@ -10,9 +10,23 @@ public class ComptaVocals {
         int contador = 0;
         
         // Fer un for per iterar tots els caràcter del text
-        for (int i = 0; i < text.length(); i++) {
+        for (int i = 0; i <= text.length(); i++) {
             // Verificar si alguns dels caràcters coincideixen amb les vocals
-            if (text.charAt(i) == 'a' || text.charAt(i) == 'à' || text.charAt(i) == 'e' || text.charAt(i) == 'è' || text.charAt(i) == 'é' || text.charAt(i) == 'i' || text.charAt(i) == 'í' || text.charAt(i) == 'ï' || text.charAt(i) == 'o' || text.charAt(i) == 'ó' || text.charAt(i) == 'ò' || text.charAt(i) == 'u' || text.charAt(i) == 'ú' || text.charAt(i) == 'ü') {
+            if (
+            text.charAt(i) == 'a' || 
+            text.charAt(i) == 'à' || 
+            text.charAt(i) == 'e' || 
+            text.charAt(i) == 'è' || 
+            text.charAt(i) == 'é' || 
+            text.charAt(i) == 'i' || 
+            text.charAt(i) == 'í' || 
+            text.charAt(i) == 'ï' || 
+            text.charAt(i) == 'o' || 
+            text.charAt(i) == 'ó' || 
+            text.charAt(i) == 'ò' || 
+            text.charAt(i) == 'u' || 
+            text.charAt(i) == 'ú' || 
+            text.charAt(i) == 'ü') {
                 // Si algun coincideix, sumar-li 1 a contador
                contador += 1;
             }
