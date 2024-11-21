4969807cd86488d4db10c3a8bda305f721f0c545
prgtest <jeironespinal0306@gmail.com>
2024-11-21 14:50:29 +0100

    Completat lèxercici 15_02  ∾ prgtest autocommit 15_02 #2

--- 15_02_comptavocals/ComptaVocals.java
+++ 15_02_comptavocals/ComptaVocals.java
@@ -8,10 +8,11 @@ public class ComptaVocals {
         String text = Entrada.readLine();
         // Declarar e inicialitzar la variable contador amb 0
         int contador = 0;
+        String vocals = "aàeéèiíïoóòuúü";
         
         // Fer un for per iterar tots els caràcter del text
-        for (int i = 0; i <= text.length(); i++) {
-            // Verificar si alguns dels caràcters coincideixen amb les vocals
+        for (int i = 0; i < text.length(); i++) {
+            /*// Verificar si alguns dels caràcters coincideixen amb les vocals
             if (
             text.charAt(i) == 'a' || 
             text.charAt(i) == 'à' || 
@@ -29,6 +30,11 @@ public class ComptaVocals {
             text.charAt(i) == 'ü') {
                 // Si algun coincideix, sumar-li 1 a contador
                contador += 1;
+            }*/
+            for (int j = 0; j < vocals.length(); j++) {
+                if (text.charAt(i) == vocals.charAt(j)) {
+                    contador += 1;
+                }
             }
         }
         // Mostrar la quantitat de vocals hi ha al text
