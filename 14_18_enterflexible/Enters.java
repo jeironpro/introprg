d863807e798af1a1682d6c2cb715417409f48eb3
prgtest <jeironespinal0306@gmail.com>
2024-11-22 22:53:21 +0100

    Completat l'exercici 14_11  ∾ prgtest autocommit 14_18 #19

--- 14_18_enterflexible/Enters.java
+++ 14_18_enterflexible/Enters.java
@@ -28,6 +28,8 @@ public class Enters {
                 }
                 if ((text.charAt(0) == '-' || text.charAt(0) == '+') && !Character.isDigit(text.charAt(1))) {
                     enter = false;
+                } else {
+                    enter = true;
                 }
                 
                 if (Character.isWhitespace(text.charAt(0)) && !Character.isDigit(text.charAt(1))) {
