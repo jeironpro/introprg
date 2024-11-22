5e616ca182fbeeeced9f2e1e00c2a3d41af3b4f9
prgtest <jeironespinal0306@gmail.com>
2024-11-22 10:51:32 +0100

    Completat l'exercici 15_08  ∾ prgtest autocommit 15_08 #3

--- 15_08_codifica/CodificaBasic.java
+++ 15_08_codifica/CodificaBasic.java
@@ -6,19 +6,28 @@
 
 public class CodificaBasic {
     public static void main(String[] args) {
+        // Demanar el text
         System.out.println("Text?");
         String text = Entrada.readLine();
+        // Declarar e inicialitzar la variable nouText buida
         String nouText = "";
         
+        // Fer un for per iterar els caràcters del text
         for (int i = 0; i < text.length(); i++) {
+            // Verificar si el caràcter està en el rang des de l'a fins a l'y
             if (text.charAt(i) >= 'a' && text.charAt(i) < 'z') {
                 nouText += (char)(text.charAt(i) + 1);
+            // Verificar si es l'z
             } else if (text.charAt(i) == 'z') {
+                // Guardar l'a en el nouText
                 nouText += 'a';
+            // Del contrari
             } else {
+                // Com no es una lletra des l'a fins a l'y, guarda-ho igual
                 nouText += text.charAt(i);
             }
         }
+        // Mostrar el nouText
         System.out.println(nouText);
     }
 }
