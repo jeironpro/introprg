167a926bd808d96368c6095e4147c9249aad2ce2
prgtest <jeironespinal0306@gmail.com>
2024-11-22 10:19:58 +0100

    Completat l'exercici 15_03  ∾ prgtest autocommit 15_03 #2

--- 15_03_informetext/InformeText.java
+++ 15_03_informetext/InformeText.java
@@ -36,57 +36,59 @@ public class InformeText {
         // Vocals en minúscules (incloent les català)
         String vocalsMinuscules = "aàeéèiíïoóòuúü";
         
-        // Fer un for per iterar tots els caràcters del text
-        for (int i = 0; i < text.length(); i++) {
-            // Verificar majúscules o minúscules
-            if (Character.isLetter(text.charAt(i))) {
-                if (Character.isUpperCase(text.charAt(i))) {
-                    contadorMajuscula += 1;
-                } else {
-                    contadorMinuscula += 1;
+        if (!text.isEmpty()) {
+            // Fer un for per iterar tots els caràcters del text
+            for (int i = 0; i < text.length(); i++) {
+                // Verificar majúscules o minúscules
+                if (Character.isLetter(text.charAt(i))) {
+                    if (Character.isUpperCase(text.charAt(i))) {
+                        contadorMajuscula += 1;
+                    } else {
+                        contadorMinuscula += 1;
+                    }
+                }          
+                // Fer un for per iterar les vocals en majúscules
+                for (int k = 0; k < vocalsMajuscules.length(); k++) {
+                    if (text.charAt(i) == vocalsMajuscules.charAt(k)) {
+                        contadorVocalMajuscula += 1;                 
+                    }
                 }
-            }          
-            // Fer un for per iterar les vocals en majúscules
-            for (int k = 0; k < vocalsMajuscules.length(); k++) {
-                if (text.charAt(i) == vocalsMajuscules.charAt(k)) {
-                    contadorVocalMajuscula += 1;                 
+                // Fer un for per iterar les vocals en minúscules
+                for (int j = 0; j < vocalsMinuscules.length(); j++) {
+                    if (text.charAt(i) == vocalsMinuscules.charAt(j)) {
+                        contadorVocalMinuscula += 1;
+                    }                    
+                }
+                // Verificar si és dígit
+                if (Character.isDigit(text.charAt(i))) {
+                    contadorDigit += 1;
+                }
+                // Verificar si són altres caràcters
+                if (!Character.isLetter(text.charAt(i)) && !Character.isDigit(text.charAt(i))) {
+                    contadorAltresCaracter += 1;
                 }
             }
-            // Fer un for per iterar les vocals en minúscules
-            for (int j = 0; j < vocalsMinuscules.length(); j++) {
-                if (text.charAt(i) == vocalsMinuscules.charAt(j)) {
-                    contadorVocalMinuscula += 1;
-                }                    
-            }
-            // Verificar si és dígit
-            if (Character.isDigit(text.charAt(i))) {
-                contadorDigit += 1;
-            }
-            // Verificar si són altres caràcters
-            if (!Character.isLetter(text.charAt(i)) && !Character.isDigit(text.charAt(i))) {
-                contadorAltresCaracter += 1;
-            }
+            // Mostrar informe
+            System.out.println("Informe");
+            System.out.println("=======");
+            // Mostrar la quantitat de les lletres majúscules i el seu percentage
+            System.out.printf("lletres en majúscules: %d (%.2f%%)%n", contadorMajuscula, 100.0 * contadorMajuscula / totalCaracter);
+            // Mostrar la quantitat de les lletres minúscules i el seu percentage
+            System.out.printf("lletres en minúscules: %d (%.2f%%)%n", contadorMinuscula, 100.0 * contadorMinuscula / totalCaracter);
+            // Mostrar la quantitat de totes les lletres i el seu percentage
+            System.out.printf("total lletres: %d (%.2f%%)%n", (contadorMajuscula + contadorMinuscula), 100.0 * (contadorMajuscula + contadorMinuscula) / totalCaracter);
+            // Mostrar la quantitat de les vocals majúscules i el seu percentage
+            System.out.printf("vocals en majúscules: %d (%.2f%%)%n", contadorVocalMajuscula, 100.0 * contadorVocalMajuscula / totalCaracter);
+            // Mostrar la quantitat de les vocals minúscules i el seu percentage
+            System.out.printf("vocals en minúscules: %d (%.2f%%)%n", contadorVocalMinuscula, 100.0 * contadorVocalMinuscula / totalCaracter);
+            // Mostrar la quantitat de totes les vocals i el seu percentage
+            System.out.printf("total vocals: %d (%.2f%%)%n", (contadorVocalMajuscula + contadorVocalMinuscula), 100.0 * (contadorVocalMajuscula + contadorVocalMinuscula) / totalCaracter);
+            // Mostrar la quantitat de tots els dígits i el seu percentage
+            System.out.printf("digits: %d (%.2f%%)%n", contadorDigit, 100.0 * contadorDigit / totalCaracter);
+            // Mostrar la quantitat de altres caràcters i el seu percentage
+            System.out.printf("altres caràcters: %d (%.2f%%)%n", contadorAltresCaracter, 100.0 * contadorAltresCaracter / totalCaracter);
+            // Mostrar la longitud del text
+            System.out.println("total caràcters: " + totalCaracter);
         }
-        // Mostrar informe
-        System.out.println("Informe");
-        System.out.println("=======");
-        // Mostrar la quantitat de les lletres majúscules i el seu percentage
-        System.out.printf("lletres en majúscules: %d (%.2f%%)%n", contadorMajuscula, 100.0 * contadorMajuscula / totalCaracter);
-        // Mostrar la quantitat de les lletres minúscules i el seu percentage
-        System.out.printf("lletres en minúscules: %d (%.2f%%)%n", contadorMinuscula, 100.0 * contadorMinuscula / totalCaracter);
-        // Mostrar la quantitat de totes les lletres i el seu percentage
-        System.out.printf("total lletres: %d (%.2f%%)%n", (contadorMajuscula + contadorMinuscula), 100.0 * (contadorMajuscula + contadorMinuscula) / totalCaracter);
-        // Mostrar la quantitat de les vocals majúscules i el seu percentage
-        System.out.printf("vocals en majúscules: %d (%.2f%%)%n", contadorVocalMajuscula, 100.0 * contadorVocalMajuscula / totalCaracter);
-        // Mostrar la quantitat de les vocals minúscules i el seu percentage
-        System.out.printf("vocals en minúscules: %d (%.2f%%)%n", contadorVocalMinuscula, 100.0 * contadorVocalMinuscula / totalCaracter);
-        // Mostrar la quantitat de totes les vocals i el seu percentage
-        System.out.printf("total vocals: %d (%.2f%%)%n", (contadorVocalMajuscula + contadorVocalMinuscula), 100.0 * (contadorVocalMajuscula + contadorVocalMinuscula) / totalCaracter);
-        // Mostrar la quantitat de tots els dígits i el seu percentage
-        System.out.printf("digits: %d (%.2f%%)%n", contadorDigit, 100.0 * contadorDigit / totalCaracter);
-        // Mostrar la quantitat de altres caràcters i el seu percentage
-        System.out.printf("altres caràcters: %d (%.2f%%)%n", contadorAltresCaracter, 100.0 * contadorAltresCaracter / totalCaracter);
-        // Mostrar la longitud del text
-        System.out.println("total caràcters: " + totalCaracter);
     }
 }
