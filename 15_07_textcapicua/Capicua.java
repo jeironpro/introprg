ae24bb6dd42b9971c06963c8ee6a2c9880222bb9
Jeiron <jeironespinal0306@gmail.com>
2024-11-22 10:27:54 +0100

    Completat l'exercici 15_07

---
+++ 15_07_textcapicua/Capicua.java
@@ -0,0 +1,22 @@
+/* Aquest programa demana un text i et diu si es capicua o no
+* Els textos capicua són els que es pot llegir al dret i a l'inrevès
+* i són iguals. */
+
+public class Capicua {
+    public static void main (String[] args) {
+        System.out.println("Text");
+        String text = Entrada.readLine();
+        text = text.toLowerCase();
+        String textInvertit = "";
+        
+        for (int i = text.length()-1; i >= 0; i--) {
+            textInvertit += text.charAt(i);            
+        }
+        
+        if (text.equals(textInvertit)) {
+            System.out.println("És capicua");                    
+        } else {
+            System.out.println("No és capicua");                    
+        }
+    }   
+}
