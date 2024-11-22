b90b6e31b467cffc0f377fc1432d9706806912a1
prgtest <jeironespinal0306@gmail.com>
2024-11-22 20:54:19 +0100

    Completat l'exercici 15_04  ∾ prgtest autocommit 15_04 #5

--- 15_04_majusculitza/MajusculitzaInicials.java
+++ 15_04_majusculitza/MajusculitzaInicials.java
@@ -24,7 +24,7 @@ public class MajusculitzaInicials {
             } 
             
             if (!Character.isLetter(text.charAt(i)) || !Character.isDigit(text.charAt(i))) {
-                esMajuscula = false;
+                esMajuscula = true;
             }
             
             
