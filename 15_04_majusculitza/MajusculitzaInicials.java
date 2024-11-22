f382abaafabab563dc30bd952091069e5b365966
Jeiron <jeironespinal0306@gmail.com>
2024-11-22 20:50:31 +0100

    Completat l'exercici 15_04

--- 15_04_majusculitza/MajusculitzaInicials.java
+++ 15_04_majusculitza/MajusculitzaInicials.java
@@ -18,7 +18,7 @@ public class MajusculitzaInicials {
             // Assignarle false a esMajuscula per reiniciar el boolean en la iteració
             esMajuscula = false; 
             // Verificar si es un espai en blanc o es el signe -
-            if (Character.isWhitespace(text.charAt(i)) || text.charAt(i) == '-') {
+            if (Character.isWhitespace(text.charAt(i)) || text.charAt(i) == '-' || !Character.isLetter(text.charAt(i)) || !Character.isDigit(text.charAt(i))) {
                 // Assignarle true a esMajuscula
                 esMajuscula = true;
             }
