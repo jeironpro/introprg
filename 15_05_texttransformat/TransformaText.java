3a4b01d9234ecb82429f97b80e9ddf4995df5d12
prgtest <jeironespinal0306@gmail.com>
2024-11-22 17:08:48 +0100

    Completat l'exercici 15_05  ∾ prgtest autocommit 15_05 #9

--- 15_05_texttransformat/TransformaText.java
+++ 15_05_texttransformat/TransformaText.java
@@ -10,23 +10,36 @@ public class TransformaText {
         System.out.println("Text?");
         String text = Entrada.readLine();
         String nouText = "";
-        String vocals = "aàeèéiíïoòóuúü";
+        // String vocals = "aàeèéiíïoòóuúü";
         
         for (int i = 0; i < text.length(); i++) {
-            for (int j = 0; j < vocals.length(); j++) {
-                if (Character.toLowerCase(text.charAt(i)) == vocals.charAt(j)) {
-                    nouText += Character.toLowerCase(text.charAt(i));
-                } else {
-                    nouText += Character.toUpperCase(text.charAt(i));                
-                }
-            }
-            
-            if (Character.isDigit(text.charAt(i))) {
+            if (
+            Character.toLowerCase(text.charAt(i)) == 'a' ||
+            Character.toLowerCase(text.charAt(i)) == 'à' ||
+            Character.toLowerCase(text.charAt(i)) == 'e' || 
+            Character.toLowerCase(text.charAt(i)) == 'è' ||
+            Character.toLowerCase(text.charAt(i)) == 'é' || 
+            Character.toLowerCase(text.charAt(i)) == 'i' ||
+            Character.toLowerCase(text.charAt(i)) == 'í' ||
+            Character.toLowerCase(text.charAt(i)) == 'ï' || 
+            Character.toLowerCase(text.charAt(i)) == 'o' ||
+            Character.toLowerCase(text.charAt(i)) == 'ó' ||
+            Character.toLowerCase(text.charAt(i)) == 'ò' || 
+            Character.toLowerCase(text.charAt(i)) == 'u' ||
+            Character.toLowerCase(text.charAt(i)) == 'ú' || 
+            Character.toLowerCase(text.charAt(i)) == 'ü'
+            ) {
+                nouText += Character.toLowerCase(text.charAt(i));
+            } else if (Character.isDigit(text.charAt(i))) {
                 if (Character.isDigit(text.charAt(i+1))) {
-                    nouText += "(" + text.charAt(i) + text.charAt(i+1)+ ")";
+                    nouText += "(" + text.charAt(i) + "" + text.charAt(i+1) + ")";
                 }
             } else if (Character.isWhitespace(text.charAt(i))) {
-                nouText += text.charAt(i);
+                if (Character.isLetter(text.charAt(i+1))) {
+                    nouText += text.charAt(i);
+                }
+            } else if (Character.isLetter(text.charAt(i))) {
+                nouText += Character.toUpperCase(text.charAt(i));            
             }
         }
         System.out.println(nouText);
