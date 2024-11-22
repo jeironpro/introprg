ee29c63b36d7f9d7647dbd32d14a6e2adca63383
prgtest <jeironespinal0306@gmail.com>
2024-11-22 22:46:04 +0100

    Completat l'exercici 14_11  ∾ prgtest autocommit 14_18 #10

--- 14_18_enterflexible/Enters.java
+++ 14_18_enterflexible/Enters.java
@@ -25,6 +25,10 @@ public class Enters {
                     if (!Character.isDigit(text.charAt(i)) && !Character.isWhitespace(text.charAt(i))) {
                         enter = false;
                     }
+                    
+                    if ((text.charAt(0) != '-' || text.charAt(0) != '+') && !Character.isDigit(text.charAt(i))) {
+                        enter = false;
+                    }
                 }
                 
                 
