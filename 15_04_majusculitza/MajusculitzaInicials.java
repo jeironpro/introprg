b2438860480b8715c86d54a096658b71dbb986cf
prgtest <jeironespinal0306@gmail.com>
2024-11-22 21:25:06 +0100

    Completat l'exercici 15_04  ∾ prgtest autocommit 15_04 #7

--- 15_04_majusculitza/MajusculitzaInicials.java
+++ 15_04_majusculitza/MajusculitzaInicials.java
@@ -10,35 +10,37 @@ public class MajusculitzaInicials {
         // Crear un nou String que guarde el text transformat
         String nouText = "";
         // El primer caràcter del text sempre serà majúscula
-        nouText += Character.toUpperCase(text.charAt(0));
         // Declarar el boolean esMajuscula en false
         boolean esMajuscula = false;
-       
-        for (int i = 0; i < text.length()-1; i++) {
-            // Assignarle false a esMajuscula per reiniciar el boolean en la iteració
-            esMajuscula = false; 
-            // Verificar si es un espai en blanc o es el signe -
-            if (Character.isWhitespace(text.charAt(i)) || text.charAt(i) == '-') {
-                // Assignarle true a esMajuscula
-                esMajuscula = true;
-            } 
-            
-            if (!Character.isLetter(text.charAt(i)) && !Character.isDigit(text.charAt(i))) {
-                esMajuscula = true;
+        
+        if (!text.isEmpty()) {
+            nouText += Character.toUpperCase(text.charAt(0));
+            for (int i = 0; i < text.length()-1; i++) {
+                // Assignarle false a esMajuscula per reiniciar el boolean en la iteració
+                esMajuscula = false; 
+                // Verificar si es un espai en blanc o es el signe -
+                if (Character.isWhitespace(text.charAt(i)) || text.charAt(i) == '-') {
+                    // Assignarle true a esMajuscula
+                    esMajuscula = true;
+                } 
+                
+                if (!Character.isLetter(text.charAt(i)) && !Character.isDigit(text.charAt(i))) {
+                    esMajuscula = true;
+                }
+                
+                
+                // Verificar si esMajuscula és true
+                if (esMajuscula) {
+                    // Guardar el caràcter en majúscula
+                    nouText += Character.toUpperCase(text.charAt(i+1));
+                // Del contrari, quan esMajuscula sigui false
+                } else {
+                    // Guardar el caràcter en minúscula
+                    nouText += Character.toLowerCase(text.charAt(i+1));
+                }
             }
-            
-            
-            // Verificar si esMajuscula és true
-            if (esMajuscula) {
-                // Guardar el caràcter en majúscula
-                nouText += Character.toUpperCase(text.charAt(i+1));
-            // Del contrari, quan esMajuscula sigui false
-            } else {
-                // Guardar el caràcter en minúscula
-                nouText += Character.toLowerCase(text.charAt(i+1));
-            }
+            // Mostrar el text transformat
+            System.out.println(nouText);        
         }
-        // Mostrar el text transformat
-        System.out.println(nouText);
     }
 }
