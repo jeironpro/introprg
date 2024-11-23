dc8b9eb32659ed6878ce9ad1639f818de1b208d4
prgtest <jeironespinal0306@gmail.com>
2024-11-23 11:21:24 +0100

    Canviant l'ús de l'utilitat  ∾ prgtest autocommit 15_07 #13

--- 15_07_textcapicua/Capicua.java
+++ 15_07_textcapicua/Capicua.java
@@ -7,24 +7,21 @@ public class Capicua {
         // Demanar el text
         System.out.println("Text?");
         String text = Entrada.readLine();
-        String textIntroduit = "";
-        // Declarar e inicialitzar la variable textInvertit buida
-        String textInvertit = "";
+        boolean esCapicua = false;
+        
         
         // Fer un for per iterar tots els caràcters del text
-        for (int i = 0; i < text.length(); i++) {
-            // Guardar tots el caràcter del text en la variable textIntroduit
-            textIntroduit += Character.toLowerCase(text.charAt(i));     
+        for (int i = text.length()-1; i >= 0; i--) {    
+            for (int j = 0; j < text.length(); j++) {
+                if (text.charAt(j) != text.charAt(i)) {
+                    esCapicua = false;
+                }    
+            }
         }
-        
         // Fer un for per iterar tots els caràcters del text
-        for (int i = text.length()-1; i >= 0; i--) {
-            // Guardar tots el caràcter del text en la variable textInvertit
-            textInvertit += Character.toLowerCase(text.charAt(i));     
-        }
         
         // Verificar si el text i textInvertit no estem buit i que sigui iguals
-        if (!text.isEmpty() && textIntroduit == textInvertit) {
+        if (!text.isEmpty() && esCapicua) {
             // Mostrar aquest missatge           
             System.out.println("És capicua");                    
         // Del contrari
