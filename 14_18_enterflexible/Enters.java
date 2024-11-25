fd1846688d646a7860350c088f4b0bec546fe611
prgtest <jeironespinal0306@gmail.com>
2024-11-25 21:07:12 +0100

    Completat l'exercici 14_11  ∾ prgtest autocommit 14_18 #27

--- 14_18_enterflexible/Enters.java
+++ 14_18_enterflexible/Enters.java
@@ -33,17 +33,7 @@ public class Enters {
                     }
                 }
                 
-                if ((text.charAt(0) == '-' || text.charAt(0) == '+') && Character.isDigit(text.charAt(1))) {
-                    enter = true;
-                } else if ((text.charAt(0) == '-' || text.charAt(0) == '+') && (text.charAt(1) == '-' || text.charAt(1) == '+')) {
-                    enter = false;
-                } else if (Character.isWhitespace(text.charAt(0))) {
-                    if (text.charAt(1) == '+' || text.charAt(1) == '-') {
-                        enter = true;
-                    }
-                } else if ((text.charAt(0) == '+' || text.charAt(0) == '-') && Character.isWhitespace(text.charAt(1))) {
-                    enter = false;
-                }
+                
                 
                 if (enter) {
                     System.out.println("És enter");
