7dbfc322e2b4cab179dea05fde0108a2ea5270f5
Jeiron <jeironespinal0306@gmail.com>
2024-11-28 12:03:58 +0100

    Completat l'exercici 15_12

---
+++ 15_12_duhast/TriangleParaules.java
@@ -0,0 +1,18 @@
+/**/
+
+public class TriangleParaules {
+    public static void main(String[] args) {
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        String nouText = text + " ";
+        
+        for (int i = 0; i < nouText.length(); i++) {
+            if (Character.isWhitespace(nouText.charAt(i))) {
+                for (int j = 0; j <= i; j++) {
+                    System.out.print(nouText.charAt(j));
+                }
+                System.out.println();            
+            }
+        }
+    }
+}
