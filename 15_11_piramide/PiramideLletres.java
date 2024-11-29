c98729cf91536d26bd9fd468dc5a3d44ad9fd86b
prgtest <jeironespinal0306@gmail.com>
2024-11-29 22:27:45 +0100

    Completat l'exercici 15_11  ∾ prgtest autocommit 15_11 #1

--- 15_11_piramide/PiramideLletres.java
+++ 15_11_piramide/PiramideLletres.java
@@ -9,11 +9,12 @@ public class PiramideLletres {
         
         // Fer un for per iterar tots els caràcters del text
         for(int i = 0; i < text.length(); i++) {
-            
+            // Fer un for per mostrar els punts creixent
             for (int j = i; j < text.length()-1; j++) {
                 System.out.print(".");
                 System.out.print(".");
             }
+            // Fer un for per mostrar els caràcters creixent, els punts entre caràcters i el guió al final de l'últim caràcter
             for (int k = 0; k <= i; k++) {
                 System.out.print(text.charAt(k));
                 if (k < i) {
@@ -23,12 +24,14 @@ public class PiramideLletres {
                     System.out.print("-");
                 }
             }
+            // Fer un for per mostrar els caràcters decreixent i els punts entre caràcters
             for (int k = i; k >= 0; k--) {
                 System.out.print(text.charAt(k));
                 if (k > 0) {
                     System.out.print(".");
                 }
             }
+            // Fer un for per mostrar els punts decreixent
             for (int j = i; j < text.length()-1; j++) {
                 System.out.print(".");
                 System.out.print(".");
