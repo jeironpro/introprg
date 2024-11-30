b5d0f05432c9fbf9b66e77aa3240dcc6f6c67835
Jeiron <jeironespinal0306@gmail.com>
2024-11-30 17:02:44 +0100

    Completat l'exercici 15_14

---
+++ 15_14_paraulesreves/ParaulesReves.java
@@ -0,0 +1,26 @@
+/* Aquest programa demana un text i et mostra cada paraula del text de manera
+* invertida */
+
+public class ParaulesReves {
+    public static void main(String[] args) {
+         // Demanar el text
+        System.out.println("Text?");
+        // Llegir el text
+        String text = Entrada.readLine();
+        String paraula = "";
+        
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isLetter(text.charAt(i))) {
+                paraula += text.charAt(i);
+            } else if (!Character.isLetter(text.charAt(i))) {
+                for (int j = paraula.length()-1; j >= 0; j--) {
+                    System.out.print(paraula.charAt(j));
+                }
+                paraula = "";
+            }
+            if (!Character.isLetter(text.charAt(i)) || Character.isWhitespace(text.charAt(i))) {
+                System.out.print(text.charAt(i));            
+            }
+        }
+    }
+}
