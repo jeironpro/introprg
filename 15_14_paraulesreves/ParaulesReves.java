4ff3289623aa394e3dc46d2b429211c24d25a19f
prgtest <jeironespinal0306@gmail.com>
2024-11-30 17:31:49 +0100

    Completat l'exercici 15_14  ∾ prgtest autocommit 15_14 #3

--- 15_14_paraulesreves/ParaulesReves.java
+++ 15_14_paraulesreves/ParaulesReves.java
@@ -7,13 +7,20 @@ public class ParaulesReves {
         System.out.println("Text?");
         // Llegir el text
         String text = Entrada.readLine();
+        // Afegirle un espai al final del text
+        // Per poder mostrar els texts (paraules) que no tinguin espai al final
         text = text + " ";
+        // Declarar e inicialitzar el String paraula buit
+        // Per almacenar cada paraula del text
         String paraula = "";
         
+        // Fer un for per iterar cada caràcter del text
         for (int i = 0; i < text.length(); i++) {
+            // Verificar si els caràcters és una lletra
             if (Character.isLetter(text.charAt(i))) {
+                // Guardar les lletres a paraula
                 paraula += text.charAt(i);
-            } else if (!Character.isLetter(text.charAt(i))) {
+            } else {
                 for (int j = paraula.length()-1; j >= 0; j--) {
                     System.out.print(paraula.charAt(j));
                 }
