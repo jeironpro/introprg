f6d0421b938becfb23024a3aceaf8127d43ab8af
prgtest <jeironespinal0306@gmail.com>
2024-12-02 16:06:38 +0100

    Completat l'exercici 14_18  ∾ prgtest autocommit 14_18 #2

--- 14_18_enterflexible/Enters.java
+++ 14_18_enterflexible/Enters.java
@@ -7,51 +7,53 @@ public class Enters {
         System.out.println("Introdueix texts (enter sol per finalitzar)");
         String text = "true";
         String nouText = "";
-        boolean esEnter = false;
                  
         while(!text.isEmpty()) {
             text = Entrada.readLine();
+            boolean esEnter = false;
             
-            for (int i = 0; i < text.length(); i++) {
-                char c = text.charAt(i);
-                if (Character.isDigit(c)) {
-                    esEnter = true;
-                }
-                if (!Character.isWhitespace(c) && (!Character.isLetter(c) || c == '-' || c == '+' || c == '.' || c == '_')) {
-                    nouText += c;
-                }
-            }
-            if (!nouText.isEmpty()) {
-                // Verificar si el caràcter 0 és un signe de - o +
-                if (nouText.charAt(0) == '-' || nouText.charAt(0) == '+') {
-                    // Verificar si el caràcter 1 és un dígit
-                    if (Character.isDigit(nouText.charAt(1))) {
-                        // Com el caràcter 0 es un signe i el caràcter 1 és un dígit, enter es true
+            if (!text.isEmpty()) {
+                for (int i = 0; i < text.length(); i++) {
+                    char c = text.charAt(i);
+                    if (Character.isDigit(c)) {
                         esEnter = true;
-                    } else {
-                        esEnter = false;
-                    }                            
-                }            
-            }
-            
-            for (int i = 0; i < nouText.length(); i++) {
-                char c = nouText.charAt(i);
-                
-                if (c == '.' || c == '_') {
-                    if (i + 1 < text.length() && Character.isDigit(nouText.charAt(i+1))) {
-                        esEnter = true;
-                    } else {
-                        esEnter = false;
-                        break;
+                    }
+                    if (!Character.isWhitespace(c) && (!Character.isLetter(c) || c == '-' || c == '+' || c == '.' || c == '_')) {
+                        nouText += c;
                     }
                 }
-            }
-            nouText = "";
-            
-            if (esEnter) {
-                System.out.println("És enter");
-            } else {
-                System.out.println("No és enter");
+                if (!nouText.isEmpty()) {
+                    // Verificar si el caràcter 0 és un signe de - o +
+                    if (nouText.charAt(0) == '-' || nouText.charAt(0) == '+') {
+                        // Verificar si el caràcter 1 és un dígit
+                        if (Character.isDigit(nouText.charAt(1))) {
+                            // Com el caràcter 0 es un signe i el caràcter 1 és un dígit, enter es true
+                            esEnter = true;
+                        } else {
+                            esEnter = false;
+                        }                            
+                    }            
+                }
+                
+                for (int i = 0; i < nouText.length(); i++) {
+                    char c = nouText.charAt(i);
+                    
+                    if (c == '.' || c == '_') {
+                        if (i + 1 < text.length() && Character.isDigit(nouText.charAt(i+1))) {
+                            esEnter = true;
+                        } else {
+                            esEnter = false;
+                            break;
+                        }
+                    }
+                }
+                nouText = "";
+                
+                if (esEnter) {
+                    System.out.println("És enter");
+                } else {
+                    System.out.println("No és enter");
+                }
             }
         }
         System.out.println("Adéu");
