1cde2e8ee8f0de1a6aa6fc0925bdb3e161d1ea5a
prgtest <jeironespinal0306@gmail.com>
2024-12-02 19:33:14 +0100

    Completat l'exercici 14_18  ∾ prgtest autocommit 14_18 #51

--- 14_18_enterflexible/Enters.java
+++ 14_18_enterflexible/Enters.java
@@ -20,24 +20,26 @@ public class Enters {
                     }
                 }
                 
-                for (int i = 0; i < nouText.length(); i++) {
-                    char c = nouText.charAt(i);      
-                    if (!Character.isDigit(c)) {
-                        esEnter = false;
-                    }           
-                    if (c == '.' || c == '_') {
-                        if (i - 1 >= 0 && Character.isDigit(nouText.charAt(i-1)) && i + 1 < nouText.length() && Character.isDigit(nouText.charAt(i+1))) {
-                            esEnter = true;;
-                        } 
-                    }   
-                } 
-                if (nouText.charAt(0) == '_' || nouText.charAt(0) == '.') {
-                    esEnter = false;                          
-                }
-                if (nouText.charAt(0) == '-' || nouText.charAt(0) == '+') {
-                    if (Character.isDigit(nouText.charAt(1))) {
-                        esEnter = true;
-                    }                            
+                if (!nouText.isEmpty()) {
+                    for (int i = 0; i < nouText.length(); i++) {
+                        char c = nouText.charAt(i);      
+                        if (!Character.isDigit(c)) {
+                            esEnter = false;
+                        }           
+                        if (c == '.' || c == '_') {
+                            if (i - 1 >= 0 && Character.isDigit(nouText.charAt(i-1)) && i + 1 < nouText.length() && Character.isDigit(nouText.charAt(i+1))) {
+                                esEnter = true;;
+                            } 
+                        }   
+                    } 
+                    if (nouText.charAt(0) == '_' || nouText.charAt(0) == '.') {
+                        esEnter = false;                          
+                    }
+                    if (nouText.charAt(0) == '-' || nouText.charAt(0) == '+') {
+                        if (Character.isDigit(nouText.charAt(1))) {
+                            esEnter = true;
+                        }                            
+                    }
                 }
                 nouText = "";
                 if (esEnter) {
