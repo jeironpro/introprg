ab6ecbc0c66316139166269877c02743796fde6f
prgtest <jeironespinal0306@gmail.com>
2024-12-04 22:50:57 +0100

    Completat l'exercici 14_18  ∾ prgtest autocommit 14_18 #112

--- 14_18_enterflexible/Enters.java
+++ 14_18_enterflexible/Enters.java
@@ -21,22 +21,26 @@ public class Enters {
             text = Entrada.readLine();
             
             // Verificar si el text no està buit
-            if (!text.isEmpty()) {
+            if (!text.isEmpty()) { 
+                 // Com el text no està buit, enter és true           
                 enter = true;
+                
                 // Fer un for per iterar tots els caràcters del text
                 for (int i = 0; i < text.length(); i++) {
                     // Declarar e inicialitzar el char c amb cada caràcter del text
                     char c = text.charAt(i);
-                    // Verificar si els caràcters no és dígit    
+                    
+                    // Verificar si el caràcter és una lletra
                     if (Character.isLetter(c)) {
                         // enter serà false
                         enter = false;
-                    }
-                    // Verificar si el caràcter és un dígit, o un simbol com -, +, . o _
+                    }             
+                      
+                    // Verificar si el caràcter no és un dígit, o un simbol com -, +, . o _
                     if (Character.isDigit(c) || c == '-' || c == '+' || c == '.' || c == '_') {
                         // Guardar aquests caràcters a nouText
                         nouText += c;
-                    } 
+                    }
                 }
                 
                 // Verificar si el nouText no està buit
@@ -44,24 +48,29 @@ public class Enters {
                     // Fer un for per iterar tots els caràcters del nouText
                     for (int i = 0; i < nouText.length(); i++) {
                         // Declarar e inicialitzar el char c amb cada caràcter del nouText
-                        char c = nouText.charAt(i);
-                        if (i == 0 && (c == '.' || c == '_')) {
+                        char c = nouText.charAt(i);  
+                        
+                        // Verificar si els caràcters no és dígit    
+                        if (!Character.isDigit(c)) {
+                            // enter serà false
                             enter = false;
-                        }
-                        if (i == nouText.length()-1 && (c == '.' || c == '_')) {
-                            enter = false;
-                        } 
-                        if (i >= 1 && (c == '.' || c == '_')) {
-                            // Verificar si els caràcter és un punt o un guió baix
-                            if (i > 0 && i < nouText.length()) {
-                                // Verificar si el caràcter en la posició anterior i següent al punt o guió és un dígit
-                                if (!Character.isDigit(nouText.charAt(i-1)) && !Character.isDigit(nouText.charAt(i+1))) {
-                                    // enter serà true
-                                    enter = false;
-                                } 
-                            }                                                 
-                        } 
+                        }           
+                        
+                        // Verificar si els caràcter és un punt o un guion baix
+                        if (c == '.' || c == '_') {
+                            // Verificar si el caràcter en la posició anterior i següent al punt o guió és un dígit
+                            if (i-1 >= 0 && Character.isDigit(nouText.charAt(i-1)) && i+1 < nouText.length() && Character.isDigit(nouText.charAt(i+1))) {
+                                // enter serà true
+                                enter = true;;
+                            } 
+                        }   
+                    } 
+                    // Verificar que el caràcter en la posició 0 no sigui un punt o un guió 
+                    if (nouText.charAt(0) == '.' || nouText.charAt(0) == '_') {
+                        // enter serà false
+                        enter = false;                          
                     }
+                    
                     // Verificar que el caràcter en la posició 0 és un signe de menys o més 
                     if (nouText.charAt(0) == '-' || nouText.charAt(0) == '+') {
                         // enter serà true
