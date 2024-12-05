9de1bec61850e2ef4e83c4d66ad4fbef99e4f183
prgtest <jeironespinal0306@gmail.com>
2024-12-06 00:16:28 +0100

    Completat l'exercici 15_16  ∾ prgtest autocommit 15_16 #48

--- 15_16_password/Password.java
+++ 15_16_password/Password.java
@@ -47,6 +47,7 @@ public class Password {
                 if (Character.isWhitespace(c)) {
                     // conteEspai serà true
                     conteEspai = true;
+                    break;
                 }
                 // Verificar si la contrasenya conté dígit
                 if (Character.isDigit(c)) {
