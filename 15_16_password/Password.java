439dde50099d3876e3deb38f5b275e71ebfe4346
prgtest <jeironespinal0306@gmail.com>
2024-12-05 22:24:40 +0100

    Completat l'exercici 15_16  ∾ prgtest autocommit 15_16 #3

--- 15_16_password/Password.java
+++ 15_16_password/Password.java
@@ -9,8 +9,6 @@ public class Password {
         if (contrasenya.isEmpty()) {
             System.out.println("El password ha de tenir entre 8 i 16 caràcters.");
         } else {
-            // Demanar la contrasenya
-            System.out.println("Contrasenya?");
             // Declarar e inicialitzar el boolean conteDigit en false
             boolean conteDigit = false;
             // Declarar e inicialitzar el boolean conteMajuscula en false
