30015cddca9df1e3ffcb42d7b996122a29f47bad
prgtest <jeironespinal0306@gmail.com>
2024-12-07 13:51:18 +0100

    Completat l'exercici 15_16  ∾ prgtest autocommit 15_16 #135

--- 15_16_password/Password.java
+++ 15_16_password/Password.java
@@ -110,7 +110,7 @@ public class Password {
                 }
             }
             // Verificar si la contrasenya conté tres dígits consecutius
-            if (comptadorDigitConsecutius >= 3) {
+            if (comptadorDigitConsecutius >= 4) {
                 // tresDigitConsecutius serà true
                 tresDigitConsecutius = true;
             }
