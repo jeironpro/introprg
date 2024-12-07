51d947637cb8f0d3ad884228b07e87180f267872
prgtest <jeironespinal0306@gmail.com>
2024-12-07 19:06:08 +0100

    Completat l'exercici 15_16  ∾ prgtest autocommit 15_16 #144

--- 15_16_password/Password.java
+++ 15_16_password/Password.java
@@ -130,10 +130,10 @@ public class Password {
                 System.out.println("El password no pot contenir espais en blanc.");
             } else if (caracterRepetit) {
                 System.out.println("El password no pot contenir caràcters repetits.");
-            } else if (quatresSeguit) {
-                System.out.println("El password no pot contenir més de 4 caràcters seguits del mateix tipus.");
             } else if (tresDigitConsecutius) {
                 System.out.println("El password no pot contenir més de 3 números consecutius."); 
+            } else if (quatresSeguit) {
+                System.out.println("El password no pot contenir més de 4 caràcters seguits del mateix tipus.");
             }
         // Del contrari
         } else {
