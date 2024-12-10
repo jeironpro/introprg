9e8a391b10b24bd17d053444b58ace3d93aa5645
prgtest <jeironespinal0306@gmail.com>
2024-12-10 16:09:16 +0100

    Completat l'exercici 16_13  ∾ prgtest autocommit 16_13 #4

--- 16_13_nomeslletres/NomesLletres.java
+++ 16_13_nomeslletres/NomesLletres.java
@@ -10,21 +10,30 @@ public class NomesLletres {
     }
     
     public static void filtraLletres(String text) {
+        // Declarar e inicialitzar la variable nouText buida
+        String nouText = "";
+        
         // Fer un for per iterar tots el caràcter del text
         for (int i = 0; i < text.length(); i++) {
-            // Declarar e inicialitzar el char c amb els caràcters del text
-            char c = text.charAt(i);
             // Verificar si els caràcters són lletres
-            if (Character.isLetter(c)) {
-                // Verificar si i és igual a 0
-                if (i > 0) {
-                    // Mostrar una coma abans del caràcter
-                    System.out.print(", " + c);
-                // Del contrari
-                } else {
-                    // Mostrar el caràcter sense coma
-                    System.out.print(c);
-                }
+            if (Character.isLetter(text.charAt(i))) {
+                // Com són lletres, guardar-lo en la variable nouText
+                nouText += text.charAt(i);
+            }
+        }
+        
+        // Fer un for per iterar tots els caràcters del nou text.
+        for (int i = 0; i < nouText.length(); i++) {
+            // Declarar e inicialitzar el char c amb els caràcters del nouText
+            char c = text.charAt(i);
+            // Verificar si i és menor a la longitud del text - 1
+            if (i < nouText.length()-1) {
+                // Mostrar els caràcters separat amb coma en aquesta longitud
+                System.out.print(c+ ", ");
+            // Del contrari, es igual a la longitud
+            } else {
+                // Mostrar el caràcter final sense coma
+                System.out.print(c);
             }
         }
     }
