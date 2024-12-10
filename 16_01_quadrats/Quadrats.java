7726a4b6a6301ad5d700e21f49bc711b4a2e5ce1
Jeiron <jeironespinal0306@gmail.com>
2024-12-10 08:07:00 +0100

    Completat l'exercici 16_01

---
+++ 16_01_quadrats/Quadrats.java
@@ -0,0 +1,26 @@
+/* Programa que dibuixa un quadrat de 5 línies i 5 columnes amb "X" */
+public class Quadrats {
+    public static void main(String[] args) {
+        // Demanar la quantitat
+        System.out.println("Quants?");
+        // Llegir la quantitat
+        int quantitat = Integer.parseInt(Entrada.readLine());
+        
+        // Fer la quantitat de quadrat como s'indique
+        for (int i = 0; i < quantitat; i++) {
+            // Fer les línies
+            for (int linia = 1; linia <= 5; linia++) {
+                // Fer les columnes
+                for (int columna = 1; columna <= 5; columna++) {
+                    // Mostrar les X
+                    System.out.print(" X");
+                }
+                // Línia que separa cada línia de X's
+                System.out.println();
+            }        
+            // Línia que separa els quadrats
+            System.out.println();
+        }
+    }
+
+}
