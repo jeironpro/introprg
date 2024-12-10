f707adc996145e4f33257b486dfc85ddb6fa9208
prgtest <jeironespinal0306@gmail.com>
2024-12-10 14:37:50 +0100

    Completat l'exercici 16_05  ∾ prgtest autocommit 16_05 #8

--- 16_05_quadrat_parametritzat/Quadrat.java
+++ 16_05_quadrat_parametritzat/Quadrat.java
@@ -1,20 +1,15 @@
 /* Programa que rep un nombre per la linia de comanda i dibuixa un quadrat */
 public class Quadrat {
     public static void main(String[] args) {
-        // Agafar el argument 0 
-        int num = Integer.parseInt(args[0]);
-        
-        if (num >= 1) {
-            // Fer les línies
-            for (int linia = 1; linia <= num; linia++) {
-                // Fer les columnes
-                for (int columna = 1; columna <= 5; columna++) {
-                    // Mostrar les X
-                    System.out.print(" X");
-                }
-                // Salt de línia que separa cada línia de X's dibuixada
-                System.out.println();
+        // Fer les línies
+        for (int linia = 1; linia <= Integer.parseInt(args[0]); linia++) {
+            // Fer les columnes
+            for (int columna = 1; columna <= 5; columna++) {
+                // Mostrar les X
+                System.out.print(" X");
             }
+            // Salt de línia que separa cada línia de X's dibuixada
+            System.out.println();
         }
     }
 }
