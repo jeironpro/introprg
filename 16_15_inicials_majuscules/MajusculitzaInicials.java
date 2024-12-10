a309efdcbcdbf8f6a5efab60603d5f0523b70e83
prgtest <jeironespinal0306@gmail.com>
2024-12-10 16:28:40 +0100

    Completat l'exercici 16_15  ∾ prgtest autocommit 16_15 #2

--- 16_15_inicials_majuscules/MajusculitzaInicials.java
+++ 16_15_inicials_majuscules/MajusculitzaInicials.java
@@ -10,20 +10,23 @@ public class MajusculitzaInicials {
     }
     
     public static void majusculitzaInicials(String text) {
-        // Mostrar el primer caràcter en majúscula
-        System.out.print(Character.toUpperCase(text.charAt(0)));
-        
-        // For per iterar tots els caràcters del text
-        for (int i = 1; i < text.length(); i++) {
-            // Verificar si el caràcter abans a i és un espai en blanc o un guió
-            if ((!Character.isLetter(text.charAt(i-1)) && !Character.isDigit(text.charAt(i-1))) || Character.isWhitespace(text.charAt(i-1)) || text.charAt(i-1) == '-') {
-                // Mostrar el caràcter en majúscula
-                System.out.print(Character.toUpperCase(text.charAt(i)));
-            // Del contrari
-            } else {
-                // Mostrar el caràcter en minuscula
-                System.out.print(Character.toLowerCase(text.charAt(i)));
-            }
-        }    
+        // Verificar si el text no està buit
+        if (!text.isEmpty()) {
+            // Mostrar el primer caràcter en majúscula
+            System.out.print(Character.toUpperCase(text.charAt(0)));
+            
+            // For per iterar tots els caràcters del text
+            for (int i = 1; i < text.length(); i++) {
+                // Verificar si el caràcter abans a i és un espai en blanc o un guió
+                if ((!Character.isLetter(text.charAt(i-1)) && !Character.isDigit(text.charAt(i-1))) || Character.isWhitespace(text.charAt(i-1)) || text.charAt(i-1) == '-') {
+                    // Mostrar el caràcter en majúscula
+                    System.out.print(Character.toUpperCase(text.charAt(i)));
+                // Del contrari
+                } else {
+                    // Mostrar el caràcter en minuscula
+                    System.out.print(Character.toLowerCase(text.charAt(i)));
+                }
+            }    
+        }
     }
 }
