66021b4f92f08d75fc714afea3f7d7ef5942a461
prgtest <jeironespinal0306@gmail.com>
2024-12-10 16:05:31 +0100

    Completat l'exercici 16_13  ∾ prgtest autocommit 16_13 #3

--- 16_13_nomeslletres/NomesLletres.java
+++ 16_13_nomeslletres/NomesLletres.java
@@ -17,13 +17,13 @@ public class NomesLletres {
             // Verificar si els caràcters són lletres
             if (Character.isLetter(c)) {
                 // Verificar si i és igual a 0
-                if (i == 0) {
-                    // Mostrar el caràcter sense coma
-                    System.out.print(c);
-                // Del contrari
-                } else {
+                if (i > 0) {
                     // Mostrar una coma abans del caràcter
                     System.out.print(", " + c);
+                // Del contrari
+                } else {
+                    // Mostrar el caràcter sense coma
+                    System.out.print(c);
                 }
             }
         }
