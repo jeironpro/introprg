cff8acb44d54f384bdbe0c057f711b2c0850416e
Jeiron <jeironespinal0306@gmail.com>
2024-12-10 16:22:41 +0100

    Completat l'exercici 16_15

---
+++ 16_15_inicials_majuscules/MajusculitzaInicials.java
@@ -0,0 +1,29 @@
+/* Programa que demana un text i et mostra el text transformat a capitalize (el primer caràcter de cada paraula en majúscula). */
+public class MajusculitzaInicials {
+    public static void main(String[] args) {
+        // Demanar el text
+        System.out.println("Text?");
+        // Llegir el text
+        String text = Entrada.readLine();
+        // Passar-li el valor del text al mòdul
+        majusculitzaInicials(text);
+    }
+    
+    public static void majusculitzaInicials(String text) {
+        // Mostrar el primer caràcter en majúscula
+        System.out.print(Character.toUpperCase(text.charAt(0)));
+        
+        // For per iterar tots els caràcters del text
+        for (int i = 1; i < text.length(); i++) {
+            // Verificar si el caràcter abans a i és un espai en blanc o un guió
+            if (Character.isWhitespace(text.charAt(i-1)) || text.charAt(i-1) == '-') {
+                // Mostrar el caràcter en majúscula
+                System.out.print(Character.toUpperCase(text.charAt(i)));
+            // Del contrari
+            } else {
+                // Mostrar el caràcter en minuscula
+                System.out.print(Character.toLowerCase(text.charAt(i)));
+            }
+        }    
+    }
+}
