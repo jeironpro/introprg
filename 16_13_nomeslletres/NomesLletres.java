6eb1a2f6cb6d832e11bf59f69879b8f643a97402
Jeiron <jeironespinal0306@gmail.com>
2024-12-10 15:57:08 +0100

    Completat l'exercici 16_13

---
+++ 16_13_nomeslletres/NomesLletres.java
@@ -0,0 +1,31 @@
+/* Programa que demana un text i et mostra nomes les lletres separats per una coma i un espai */
+public class NomesLletres {
+    public static void main(String[] args) {
+        // Demanar el text
+        System.out.println("Text?");
+        // Llegir el text
+        String text = Entrada.readLine();
+        // Passar-li el valor del text al mòdul
+        filtraLletres(text);
+    }
+    
+    public static void filtraLletres(String text) {
+        // Fer un for per iterar tots el caràcter del text
+        for (int i = 0; i < text.length(); i++) {
+            // Declarar e inicialitzar el char c amb els caràcters del text
+            char c = text.charAt(i);
+            // Verificar si els caràcters són lletres
+            if (Character.isLetter(c)) {
+                // Verificar si i és igual a 0
+                if (i == 0) {
+                    // Mostrar el caràcter sense coma
+                    System.out.print(c);
+                // Del contrari
+                } else {
+                    // Mostrar una coma abans del caràcter
+                    System.out.print(", " + c);
+                }
+            }
+        }
+    }
+}
