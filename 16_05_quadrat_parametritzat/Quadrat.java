a0dce7afe48105018bf50739065370f3c48c7998
prgtest <jeironespinal0306@gmail.com>
2024-12-10 14:48:36 +0100

    Completat l'exercici 16_05  ∾ prgtest autocommit 16_05 #12

--- 16_05_quadrat_parametritzat/Quadrat.java
+++ 16_05_quadrat_parametritzat/Quadrat.java
@@ -3,16 +3,23 @@ public class Quadrat {
     public static void main(String[] args) {
         // Agafar el argument 0 
         int num = Integer.parseInt(args[0]);
-        
+        dibuixaQuadrat(num);
+    }
+    
+     public static void dibuixaQuadrat(int num) {
         // Fer les línies
         for (int linia = 1; linia <= num; linia++) {
-            // Fer les columnes
-            for (int columna = 1; columna <= num; columna++) {
-                // Mostrar les X
-                System.out.print(" X");
-            }
+            dibuixaLinia();
             // Salt de línia que separa cada línia de X's dibuixada
             System.out.println();
+        }
+    }
+    
+    public static void dibuixaLinia() {
+        // Fer les columnes
+        for (int columna = 1; columna <= 5; columna++) {
+            // Mostrar les X
+            System.out.print(" X");
         }
     }
 }
