d9a39b229d60c3d9a83992bf6c3f17f9953fe81c
prgtest <jeironespinal0306@gmail.com>
2024-12-13 11:57:11 +0100

    Completat l'exercici 15_16  ∾ prgtest autocommit 15_16 #12

--- 15_16_password/Password.java
+++ 15_16_password/Password.java
@@ -85,11 +85,6 @@ public class Password {
                 }
                 // Verificar que la quantitat de majúscules es major a la quantitat de minúscules
                 majusculesSuperior = comptadorMajuscula >= comptadorMinuscula;
-                
-                // Verificar si la contrasenya conté 4 caràcters seguits del mateix tipus
-                if (comptadorMajuscula >= 4 || comptadorMinuscula >= 4  || comptadorSimbol >= 4) {
-                    quatresSeguit = true;
-                }
             }
             // Declarar e inicialitzar el int comptadorDigitConsecutius en 0
             int comptadorDigitConsecutius = 0;
@@ -117,6 +112,11 @@ public class Password {
                         quatresSeguit = true;
                     }
                 }
+            }
+            
+            // Verificar si la contrasenya conté 4 caràcters seguits del mateix tipus
+            if (comptadorDigitConsecutius < 3 && (comptadorMajuscula >= 4 || comptadorMinuscula >= 4  || comptadorSimbol >= 4)) {
+                quatresSeguit = true;
             }
             
             if (!conteDigit) {
