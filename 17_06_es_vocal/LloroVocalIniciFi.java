fe5e4c4e2c763681e4c18755c63852f91eaef4fe
prgtest <jeironespinal0306@gmail.com>
2024-12-21 17:53:11 +0100

    Completat l'exercici 17_06  ∾ prgtest autocommit 17_06 #1

--- 17_06_es_vocal/LloroVocalIniciFi.java
+++ 17_06_es_vocal/LloroVocalIniciFi.java
@@ -10,19 +10,32 @@ public class LloroVocalIniciFi {
             System.out.println("El lloro demana paraula amb vocal a l'inici o/i final");
             // Llegir la paraula
             String paraula = Entrada.readLine();
+            // Declarar e inicialitzar el boolean iniVocal en false
             boolean iniVocal = false;
+            // Declarar e inicialitzar el boolean fiVocal en false
             boolean fiVocal = false;
+            
+            // Si la paraula no està buida
             if (!paraula.isEmpty()) {
-                char pc = paraula.charAt(0);
-                char uc = paraula.charAt(paraula.length()-1);
+                // Agafar el primer caràcter de la paraula
+                char primerCaracter = paraula.charAt(0);
+                // Agafar l'últim caràcter de la paraula
+                char ultimCaracter = paraula.charAt(paraula.length()-1);
                 
-                iniVocal = UtilString.esVocal(pc);     
-                fiVocal = UtilString.esVocal(uc);   
+                // Assignar-li la funció esVocal que es troba en el fitxer UtilString al boolean iniVocal 
+                // i passar-li com argument el primer caràcter de la paraula
+                iniVocal = UtilString.esVocal(primerCaracter);  
+                // Assignar-li la funció esVocal que es troba en el fitxer UtilString al boolean fiVocal 
+                // i passar-li com argument l'últim caràcter de la paraula   
+                fiVocal = UtilString.esVocal(ultimCaracter);   
                 
+                // Si la paraula incia o acaba en vocal, repetir la paraula
                 if (iniVocal || fiVocal) {
                     System.out.println("El lloro diu: " + paraula);
                 }  
+            // Del contrari
             } else {
+                // Demanar confirmació per finalitzar
                 System.out.println("El lloro demana confirmació per finalitzar");
                 if (UtilitatsConfirmacio.respostaABoolean(Entrada.readLine())) {
                     // Aturar el bucle
