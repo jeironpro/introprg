116171405ca199f1d2dd1516313e35d5b5dbe9c6
prgtest <jeironespinal0306@gmail.com>
2025-01-10 12:31:09 +0100

    Completat l'exercici 17_09  ∾ prgtest autocommit 17_09 #2

--- 17_09_mostra_interval_string/MostraInterval.java
+++ 17_09_mostra_interval_string/MostraInterval.java
@@ -1,16 +1,23 @@
-/**/
+/* Aquest programa demana un text, un nombre de inici i un altre final
+* et mostrarà les lletres que estan al rang del valor inici i valor final
+* pots ser de manera creixent i decreixent amb móduls de un altre fitxer.
+*/
 
 public class MostraInterval {
     public static void main(String[] args) {
+        // Demanar el text
         System.out.println("text?");
+        // Llegir el text
         String text = Entrada.readLine();
-        
+        // Demanar el número de inici
         System.out.println("inici?");
+        // Llegir el número de inici
         int ini = Integer.parseInt(Entrada.readLine());
-        
+        // Demanar el número final
         System.out.println("final?");
+        // Llegir el número final
         int fi = Integer.parseInt(Entrada.readLine());
-        
+        // Mostrar el resultat de la funció intervalString(li passem el text, número de inici i número final)
         System.out.println(UtilString.intervalString(text, ini, fi));
     }
 }
