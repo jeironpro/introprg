242712677a66bb142c3bb8f6f22ce5c3394121fd
prgtest <jeironespinal0306@gmail.com>
2025-01-10 11:26:26 +0100

    Completat l'exercici 17_10  ∾ prgtest autocommit 17_10 #3

--- 17_10_es_enter/UtilString.java
+++ 17_10_es_enter/UtilString.java
@@ -109,7 +109,7 @@ public class UtilString {
     
     public static boolean esEnter(String text) {
         // Si el text no está buit
-        if (!text.isEmpty()) {
+        if (!text.isBlank()) {
             // Eliminar els espais que apareixen davant i darrere del text
             text = text.trim();
             // Declarar e inicialitzar el char signe amb el carácter en la posició 0
