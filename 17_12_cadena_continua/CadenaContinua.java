d71ee772512ade15d0adf9d31bd79c1becef8b39
Jeiron <jeironespinal0306@gmail.com>
2025-01-10 12:41:07 +0100

    Completat l'exercici 17_12

--- 17_12_cadena_continua/CadenaContinua.java
+++ 17_12_cadena_continua/CadenaContinua.java
@@ -6,20 +6,23 @@ public class CadenaContinua {
         System.out.println("Text?");
         // Llegir el text
         String text = Entrada.readLine();
-        // Demanar el nombre
-        System.out.println("Nombre?");
-        // Llegir el nombre
-        String nombre = Entrada.readLine();
-        // Si la funció esEnter (li passem el nombre) que està en el fixter UtilString, retorna true 
-        if (UtilString.esEnter(nombre)) {
-            // Convertir el nombre a enter
-            int nombreConfirmat = Integer.parseInt(nombre);
-            // Mostrar el String resultant de la funció cadenaContinua(li passem el text i el nombreConfirmat) que está en el fitxer UtilString
-            System.out.println(UtilString.cadenaContinua(text, nombreConfirmat));
-        // Del contrari
-        } else {
-            // Mostrar aquest missatge
-            System.out.println("error");
+        // Si el text no está buit
+        if (!text.isEmpty()) {
+            // Demanar el nombre
+            System.out.println("Nombre?");
+            // Llegir el nombre
+            String nombre = Entrada.readLine();
+            // Si la funció esEnter (li passem el nombre) que està en el fixter UtilString, retorna true 
+            if (UtilString.esEnter(nombre)) {
+                // Convertir el nombre a enter
+                int nombreConfirmat = Integer.parseInt(nombre);
+                // Mostrar el String resultant de la funció cadenaContinua(li passem el text i el nombreConfirmat) que está en el fitxer UtilString
+                System.out.println(UtilString.cadenaContinua(text, nombreConfirmat));
+            // Del contrari
+            } else {
+                // Mostrar aquest missatge
+                System.out.println("error");
+            }        
         }
     }
 }
