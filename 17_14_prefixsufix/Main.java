c7ff78e9fa031a396ff52d9d0df14c46eece5c11
prgtest <jeironespinal0306@gmail.com>
2025-01-13 23:14:50 +0100

    Completat l'exercici 17_14  ∾ prgtest autocommit 17_14 #1

--- 17_14_prefixsufix/Main.java
+++ 17_14_prefixsufix/Main.java
@@ -1,8 +1,4 @@
-/* 
-* Programa que demana un text principal i un subtext i si desitja que el programa vaig actuar 
-* de manera estricta o flexible, on si la seva resposta és positiva és estricte i negativa és flexible.
-* Quan rep les dades demanats, mostra si el subtext introduït és el començament o la terminació del text.
-*/
+/* Aquest programa comprova el funcionament de les funció UtilSTring.esPrefix() i esSufix() */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Text principal?");
