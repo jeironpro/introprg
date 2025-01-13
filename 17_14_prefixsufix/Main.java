6e518a383f141b3415a42dfc49433ecf49132036
Jeiron <jeironespinal0306@gmail.com>
2025-01-13 23:12:47 +0100

    Completat l'exercici 17_14

--- 17_14_prefixsufix/Main.java
+++ 17_14_prefixsufix/Main.java
@@ -1,4 +1,8 @@
-/* Aquest programa comprova el funcionament de les funció UtilSTring.esPrefix() i esSufix() */
+/* 
+* Programa que demana un text principal i un subtext i si desitja que el programa vaig actuar 
+* de manera estricta o flexible, on si la seva resposta és positiva és estricte i negativa és flexible.
+* Quan rep les dades demanats, mostra si el subtext introduït és el començament o la terminació del text.
+*/
 public class Main {
     public static void main(String[] args) {
         System.out.println("Text principal?");
