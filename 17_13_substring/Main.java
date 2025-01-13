3b9c443484583563e00cc0c70e202450989357cb
Jeiron <jeironespinal0306@gmail.com>
2025-01-13 23:25:31 +0100

    Agregant la capçalera al Main

--- 17_13_substring/Main.java
+++ 17_13_substring/Main.java
@@ -1,5 +1,8 @@
-/* Aquest programa es utilitza per provar la funció esSubstring */
-
+/* 
+* Programa que demana un text principal i un subtext i si desitja que el programa vaig actuar 
+* de manera estricta o flexible, on si la seva resposta és positiva és estricte i negativa és flexible.
+* Quan rep les dades demanats, mostra si el subtext introduït és part del text de manera seqüencial.
+*/
 public class Main {
     public static void main(String[] args) {
         System.out.println("Text principal?");
