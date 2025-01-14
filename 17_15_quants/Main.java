6dc0aa8df07d7a27dda95fafbdf8f4c5d04d4a76
Jeiron <jeironespinal0306@gmail.com>
2025-01-14 10:33:01 +0100

    Completat l'exercici 17_15

--- 17_15_quants/Main.java
+++ 17_15_quants/Main.java
@@ -1,14 +1,18 @@
- /* Aquest programa comprova el funcionament de la funció UtilSTring.quants() */
- public class Main {
-     public static void main(String[] args) {
-         System.out.println("Text principal?");
-         String principal = Entrada.readLine();
-         System.out.println("Subtext?");
-         String subtext = Entrada.readLine();
-         System.out.println("Estricte?");
-         boolean estricte = UtilitatsConfirmacio.respostaABoolean(Entrada.readLine());
-         System.out.printf("quants(\"%s\", \"%s\", %b) -> %d%n",
-                           principal, subtext, estricte,
-                           UtilString.quants(principal, subtext, estricte));
-     }
- }
+/* 
+* Programa que demana un text principal i un subtext i si desitja que el programa vaig actuar 
+* de manera estricta o flexible, on si la seva resposta és positiva és estricte i negativa és flexible.
+* Quan rep les dades demanats, mostra quantes vegades es repeteix el subtext en el text.
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Text principal?");
+        String principal = Entrada.readLine();
+        System.out.println("Subtext?");
+        String subtext = Entrada.readLine();
+        System.out.println("Estricte?");
+        boolean estricte = UtilitatsConfirmacio.respostaABoolean(Entrada.readLine());
+        System.out.printf("quants(\"%s\", \"%s\", %b) -> %d%n",
+                principal, subtext, estricte,
+                UtilString.quants(principal, subtext, estricte));
+    }
+}
