6407863bf8b896384d542c46135b5c95873753a3
prgtest <jeironespinal0306@gmail.com>
2025-01-15 21:26:12 +0100

    Prova de l'exercici 17_16  ∾ prgtest autocommit 17_16 #4

--- 17_16_progressio/Progressio.java
+++ 17_16_progressio/Progressio.java
@@ -3,6 +3,8 @@
 public class Progressio {
     public static void main(String[] args) {
         System.out.println("Introduïu text. Enter per finalitzar.");
+        System.out.println("Estricte?");
+        boolean estricte = UtilitatsConfirmacio.respostaABoolean(Entrada.readLine());
         
         while (true) {
             String text = Entrada.readLine();
@@ -19,16 +21,15 @@ public class Progressio {
                 if (!Character.isWhitespace(c)) {
                     paraula += c;
                 } else {
-                    String palabraFiltrada = UtilString.filtraAlfabetCatala(UtilString.filtraVocalCatala(paraula));
-                    if (palabraFiltrada.length() >= 3) {
+                    if (paraula.length() >= 3) {
                         paraulaValida = true;
-                        if (UtilString.esCreixent(palabraFiltrada)) {
+                        if (UtilString.esCreixent(paraula, estricte)) {
                             System.out.println("\"" + paraula + "\"" + " és creixent");
-                        } else if (UtilString.esDecreixent(palabraFiltrada)) {
+                        } else if (UtilString.esDecreixent(paraula, estricte)) {
                             System.out.println("\"" + paraula + "\"" + " és decreixent");
-                        } else if (UtilString.esCreixiDecri(palabraFiltrada)) {
+                        } else if (UtilString.esCreixiDecri(paraula, estricte)) {
                             System.out.println("\"" + paraula + "\"" + " és creixidecri");
-                        } else if (UtilString.esDecriCreixi(palabraFiltrada)) {
+                        } else if (UtilString.esDecriCreixi(paraula, estricte)) {
                             System.out.println("\"" + paraula + "\"" + " és decricreixi");
                         } else {
                             System.out.println("\"" + paraula + "\"" + " és normaleta");
