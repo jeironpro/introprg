5319c26bfb0d178d3ed62597b3f9e81642de446b
Jeiron <jeironespinal0306@gmail.com>
2025-01-16 10:31:51 +0100

    Completat l'exercici 18_07

---
+++ 18_07_enters_entre_comes_v3/EntersEntreComes.java
@@ -0,0 +1,42 @@
+/* 
+* Programa que demana els valors a introduir en un array en enters, 
+* els valors en cada posició de l'index per emmagatzemar-ho en el array
+* i mostra aquest valor separat per coma.
+*/
+
+public class EntersEntreComes {
+    public static void main(String[] args) {
+        // Demanar la quantitat de valors del array
+        System.out.println("Quants valors?");
+        // Llegir la quantitat de valors
+        int valors = Integer.parseInt(Entrada.readLine());
+        // Declarar e inicialitzar el array de enters numeros amb el valor introduït per l'usuari
+        int[] numeros = new int[valors];
+        // Declarar e inicialitzar el int comptador en 0 
+        int comptador = 0;
+        // Declarar e incialitzar el int numValor en 1
+        int numValor = 1;
+        // Fer un while que s'execute mentre comptador sigui menor que la longitud del array numeros
+        while (comptador < numeros.length) {
+            // Demanar el(s) valor(s)
+            System.out.println("Valor " + numValor + "?");
+            // Llegir el(s) valor(s)
+            int valor = Integer.parseInt(Entrada.readLine());
+            // Guardar el valor en l'index corresponent
+            numeros[comptador] = valor;
+            // Augmentar en 1 comptador
+            comptador++;
+            // Augmentar en 1 numValor
+            numValor++;
+        }
+        // Mostrar el valor en l'index 0
+        System.out.print(numeros[0]);
+        // Fer un for per iterar el array a partir de la posició 1
+        for (int i = 1; i < numeros.length; i++) {
+            // Mostrar els valor de l'array en la posició de i com una coma abans
+            System.out.print(", " + numeros[i]);
+        }
+        // Fer un salt de línia
+        System.out.println();
+    }
+}
