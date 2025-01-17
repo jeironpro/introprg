a192a854d65d8ca7cbb549653c8362d716f4bb6c
prgtest <jeironespinal0306@gmail.com>
2025-01-17 11:40:30 +0100

    Intent de completar aquest exercici (17_16)  ∾ prgtest autocommit 17_16 #13

--- 17_16_progressio/Progressio.java
+++ 17_16_progressio/Progressio.java
@@ -36,6 +36,7 @@ public class Progressio {
                         }
                     }                
                     paraula = "";
+                    paraulaNormal = "";
                 }
             }
             if (!paraulaValida) {
