2e87592d5ef73c98c2de89398705f7e294094e82
Jeiron <jeironespinal0306@gmail.com>
2025-01-18 14:11:02 +0100

    Completat l'exercici 18_09

---
+++ 18_09_arguments/Arguments.java
@@ -0,0 +1,18 @@
+/* Programa que verifica si el valor introduït per línia de comanda és enter o no */
+
+public class Arguments {
+    public static void main(String[] args) {
+        // Fer un for per iterar la longitud de arguments introduït
+        for (int i = 0; i < args.length; i++) {
+            // Si el argument en la posició de i és enter
+            if (UtilString.esEnter(args[i])) {
+                // Mostrar aquest missatge
+                System.out.println("[" + i + "] " + "\"" + args[i] + "\":" + " és enter");
+            // Del contrari
+            } else {
+                // Mostrar aquest missatge
+                System.out.println("[" + i + "] " + "\"" + args[i] + "\":" + " no és enter");
+            }
+        }
+    }
+}
