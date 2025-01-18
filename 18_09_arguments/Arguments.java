9bbefbeed011f597de84a2029dcd7bd245215686
prgtest <jeironespinal0306@gmail.com>
2025-01-18 14:18:53 +0100

    Completat l'exercici 18_09  ∾ prgtest autocommit 18_09 #1

--- 18_09_arguments/Arguments.java
+++ 18_09_arguments/Arguments.java
@@ -4,8 +4,9 @@ public class Arguments {
     public static void main(String[] args) {
         // Fer un for per iterar la longitud de arguments introduït
         for (int i = 0; i < args.length; i++) {
+            int valorEnter = Integer.parseInt(args[i]);
             // Si el argument en la posició de i és enter
-            if (UtilString.esEnter(args[i])) {
+            if (valorEnter < 0 || valorEnter > 0) {
                 // Mostrar aquest missatge
                 System.out.println("[" + i + "] " + "\"" + args[i] + "\":" + " és enter");
             // Del contrari
