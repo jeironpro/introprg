32ef4ca88bef0687588a5bec90386db2f282ae0a
prgtest <jeironespinal0306@gmail.com>
2025-01-18 14:05:36 +0100

    Completat l'exercici 18_08  ∾ prgtest autocommit 18_08 #1

--- 18_08_enters_entre_comes_v4/EntersEntreComes.java
+++ 18_08_enters_entre_comes_v4/EntersEntreComes.java
@@ -11,39 +11,42 @@ public class EntersEntreComes {
         System.out.println("Quants?");
         // Llegir la quantitat de valors
         int valorsCorrect = UtilString.valorEnter(Entrada.readLine());
-        // Demanar el separador
-        System.out.println("Separador?");
-        // Llegir el separador
-        String separador = Entrada.readLine();
-        // Declarar el char separadorValid
-        char separadorValid = UtilString.separadorArray(separador);
-        // Declarar e inicialitzar el int comptador en 0 
-        int comptador = 0;
-        // Declarar e incialitzar el int numValor en 1
-        int numValor = 1;
-        // Si valor és major a 0
+        // Si el valor és major a 0
         if (valorsCorrect > 0) {
-            // Declarar e inicialitzar el array de enters numeros amb el valor introduït per l'usuari
-            int[] numeros = new int[valorsCorrect];        
-            // Fer un while que s'execute mentre comptador sigui menor que la longitud del array numeros
-            while (comptador < numeros.length) {
-                // Demanar el(s) valor(s)
-                System.out.println("Valor " + numValor + "?");
-                // Llegir el(s) valor(s)
-                int valorCorrect = UtilString.valorEnter(Entrada.readLine());
-                // Guardar el valor en l'index corresponent
-                numeros[comptador] = valorCorrect;                
-                // Augmentar en 1 comptador
-                comptador++;
-                // Augmentar en 1 numValor
-                numValor++;
+            // Demanar el separador
+            System.out.println("Separador?");
+            // Llegir el separador
+            String separador = Entrada.readLine();
+            // Declarar el char separadorValid
+            char separadorValid = UtilString.separadorArray(separador);
+            // Declarar e inicialitzar el int comptador en 0 
+            int comptador = 0;
+            // Declarar e incialitzar el int numValor en 1
+            int numValor = 1;
+            // Si valor és major a 0
+            if (valorsCorrect > 0) {
+                // Declarar e inicialitzar el array de enters numeros amb el valor introduït per l'usuari
+                int[] numeros = new int[valorsCorrect];        
+                // Fer un while que s'execute mentre comptador sigui menor que la longitud del array numeros
+                while (comptador < numeros.length) {
+                    // Demanar el(s) valor(s)
+                    System.out.println("Valor " + numValor + "?");
+                    // Llegir el(s) valor(s)
+                    int valorCorrect = UtilString.valorEnter(Entrada.readLine());
+                    // Guardar el valor en l'index corresponent
+                    numeros[comptador] = valorCorrect;                
+                    // Augmentar en 1 comptador
+                    comptador++;
+                    // Augmentar en 1 numValor
+                    numValor++;
+                }
+                // Mostrar el valor separat des de la funció entreComes
+                System.out.println(UtilString.entreComes(numeros, separadorValid));
+            // Del contrari
+            } else {
+                // Mostrar aquest missatge
+                System.out.println("Res a fer");
             }
-            // Mostrar el valor separat des de la funció entreComes
-            System.out.println(UtilString.entreComes(numeros, separadorValid));
-        // Del contrari
-        } else {
-            // Mostrar aquest missatge
-            System.out.println("Res a fer");
         }
     }
 }
