fe899337e69c760fdb7fc9b381ddf4f537116c9e
Jeiron <jeironespinal0306@gmail.com>
2025-01-20 22:34:28 +0100

    Completat l'exercici 18_14

---
+++ 18_14_tresenratlla_taulell/TresEnRatlla.java
@@ -0,0 +1,85 @@
+/*
+* Programa que mostra la representació d'una partida del joc tic tac toe 
+* amb combinacions establertes i permetent fer un movement al jugador X
+*/
+
+public class TresEnRatlla {
+    public static void main(String[] args){
+        // declaració del taulell
+        char[][] taulell = new char[3][3];
+        // inicialització de la fila 0
+        taulell[0][0] = '·';
+        taulell[0][1] = 'O';
+        taulell[0][2] = 'O';
+        
+        // inicialització de la fila 1
+        taulell[1][0] = '·';
+        taulell[1][1] = 'X';
+        taulell[1][2] = '·';
+        
+        // inicialització de la fila 2
+        taulell[2][0] = '·';
+        taulell[2][1] = '·';
+        taulell[2][2] = 'X';
+        
+        // mostra la posició inicial del taulell
+        System.out.println("La posició inicial del taulell:");
+        mostraFila(taulell[0]);     // mostra la línia 0
+        mostraFila(taulell[1]);     // mostra la línia 1
+        mostraFila(taulell[2]);     // mostra la línia 2
+        
+        // demana coordenades del moviment del jugador X
+        System.out.println("Fila del següent moviment?");
+        int fila = Integer.parseInt(Entrada.readLine());
+        
+        // marquem el nou moviment
+        // Si fila està a dins del rang
+        if (fila >= 0 && fila <= 2) {
+            // Demanar la columna
+            System.out.println("Columna del següent moviment?");
+            // Llegir la columna
+            int columna = Integer.parseInt(Entrada.readLine());
+            // Si fila està a dins del rang
+            if (columna >= 0 && columna <= 2) {
+                // Si la casella està desocupat
+                if (taulell[fila][columna] == '·') {
+                    // Marca l'X en la posició corresponent
+                    taulell[fila][columna] = 'X';  
+                // Del contrari              
+                } else {
+                    // Mostrar aquest missatge d'error
+                    System.out.printf("La casella [%d, %d] està ocupat per '%c'%n", fila, columna, taulell[fila][columna]);
+                    // Finalitzar el programa
+                    return;
+                }
+            // Del contrari
+            } else {
+                // Mostrar aquest missatge d'error
+                System.out.printf("Columna %d està fora de rang%n", columna);
+                // Finalitzar el programa
+                return;
+            }
+        // Del contrari
+        } else {
+            // Mostrar aquest missatge d'error
+            System.out.printf("Fila %d està fora de rang%n", fila);
+            // Finalitzar el programa
+            return;
+        }
+        // tornem a mostrar el taulell
+        System.out.println("La posició final del taulell:");
+        mostraFila(taulell[0]);     // mostra la línia 0
+        mostraFila(taulell[1]);     // mostra la línia 1
+        mostraFila(taulell[2]);     // mostra la línia 2
+    }
+    // mostra el contingut de la fila per sortida estàndard
+    public static void mostraFila(char[] fila) {
+        // For per mostrar les files de l'array
+        for (int col = 0; col < fila.length; col++) { 
+            // Mostra les files
+            System.out.print(fila[col]);
+        }
+        // Fer un salt de línia després de cada iteració de col
+        System.out.println();
+    }
+}
