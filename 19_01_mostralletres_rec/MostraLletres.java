8f9b82f15202468a73fb066c368c780017da53f9
Jeiron <jeironespinal0306@gmail.com>
2025-01-24 12:24:32 +0100

    Completat l'exercici 19_01

---
+++ 19_01_mostralletres_rec/MostraLletres.java
@@ -0,0 +1,35 @@
+/* Programa que demana un text i et mostra nomes les lletres amb funció recursiva */
+
+public class MostraLletres {
+    public static void main(String[] args){
+        // Demanar el text
+        System.out.println("Text?");
+        // Llegir el text
+        String text = Entrada.readLine();
+        // Cridar el mòdul i passar-li
+        mostraLletres(text);
+        System.out.println();   // un salt de línia final
+    }
+
+    // mostra només les lletres de text
+    public static void mostraLletres(String text) {
+        // Si el text està buit
+        if (text.isEmpty()) {      // cas base
+            // Retornar
+            return;   
+        }
+
+        // hi ha al menys un caràcter. Comptem quantes lletres hi queden
+        // considerem el primer caràcter
+        char primer = text.charAt(0);
+        // Si el caràcter és una lletra
+        if (Character.isLetter(primer)) {
+            // Mostrar el caràcter
+            System.out.println(primer);
+        }
+        
+        // considerem les lletres que conté la resta del text
+        String restaText = text.substring(1);  // resta del text
+        mostraLletres(restaText);      // crida recursiva
+    }
+}
