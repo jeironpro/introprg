5e7de5faec6a10e0e2a824851d596f397411da65
Jeiron <jeironespinal0306@gmail.com>
2025-01-27 10:17:26 +0100

    Completat l'exercici 18_21

---
+++ 18_21_formestaula/Formes.java
@@ -0,0 +1,166 @@
+/*
+* programa que rep per línia de comanda especificacions de taules i les dibuixa.
+*
+* Les especificacions permesa són:
+*   - \: primera diagonal
+*   - |: vertical al mig
+*   - -: horitzontal al mig
+*   - +: quarts
+*   - /: segona diagonal
+*   - x: creu
+*   - =: pas de vianants
+*   - ||: zebra
+*   - ++: taulell d'escacs
+*   - ?: taula buida
+*
+* Exemple per cada especificació:
+*   - 5x6\ -> primera diagonal
+*   - 4x4| -> vertical al mig
+*   - 7x5- -> horizontal al mig
+*   - 5x5+ -> quarts
+*   - 7x7/ -> segona diagonal
+*   - 3x5x -> creu
+*   - 7x4= -> pas de vianants
+*   - 4x7|| -> zebra
+*   - 8x8++ -> taulell d'escacs
+*   - 2x3?: taula buida
+*
+* Si s'introduiex cap de les especificacions anteriors, mostrar un missatge d'error.
+*/
+
+public class Formes {
+    public static void main(String[] args){
+        // Fer un for per iterar els arguments
+        for (int i = 0; i < args.length; i++) {
+            // Mostrar cada argument introduït
+            System.out.println(args[i]);
+            // Declarar e inicialitzar el int files amb la funcio obteFiles i passar-li el argument en la posició de i
+            int files = obteFiles(args[i]);
+            // Declarar e inicialitzar el int columnes amb la funcio obteColumnes i passar-li el argument en la posició de i
+            int columnes = obteColumnes(args[i]);
+            
+            // Si les files o columnes són menor a 0
+            if (files < 1 || columnes < 1) {
+                // Mostrar aquest missatge d'error
+                System.out.println("Especificació no vàlida");
+            // Del contrari
+            } else {
+                // Declarar e inicialitzar l'array bidemensional a dimension obtingudes de files x columnes
+                boolean[][] taula = new boolean[files][columnes];
+                
+                // Declarar e inicialitzar el String especificacio buit
+                String especificacio = "";
+                
+                // Agafar l'últim caràcter de l'argument en posició de i
+                char ultimCaracter = args[i].charAt(args[i].length()-1);
+                // Agafar el penultim caràcter de l'argument en posició de i
+                char penultimCaracter = args[i].charAt(args[i].length()-2);
+                
+                // Si l'últim caràcter i el penultim caracàter són aquest caràcter '|'
+                if (ultimCaracter == '|' && penultimCaracter == '|') {
+                    // Guardar els caràcters en especificacio
+                    especificacio += ultimCaracter;
+                    especificacio += penultimCaracter;
+                // Del contrari, si l'últim caràcter i el penultim caracàter són aquest caràcter '+'
+                } else if (ultimCaracter == '+' && penultimCaracter == '+') {
+                    // Guardar els caràcters en especificacio
+                    especificacio += ultimCaracter;
+                    especificacio += penultimCaracter;
+                // Del contrari
+                } else {
+                    // Guardar l'últim caràcter en especificacio
+                    especificacio += ultimCaracter;
+                }
+                
+                // Fer un switch per especificacio
+                switch (especificacio) {
+                    // Caràcter per cridar inicialitzaPrimeraDiagonal
+                    case "\\": UtilTaula.inicialitzaPrimeraDiagonal(taula);
+                        break;
+                    // Caràcter per cridar inicialitzaVerticalMig
+                    case "|": UtilTaula.inicialitzaVerticalMig(taula);
+                        break;
+                    // Caràcter per cridar inicialitzaHoritzontalMig
+                    case "-": UtilTaula.inicialitzaHoritzontalMig(taula);
+                        break;
+                    // Caràcter per cridar inicialitzaQuarts
+                    case "+": UtilTaula.inicialitzaQuarts(taula);
+                        break;
+                    // Caràcter per cridar inicialitzaSegonaDiagonal
+                    case "/": UtilTaula.inicialitzaSegonaDiagonal(taula);
+                        break;
+                    // Caràcter per cridar inicialitzaCreu
+                    case "x": UtilTaula.inicialitzaCreu(taula);
+                        break; 
+                    // Caràcter per cridar inicialitzaPasVianants
+                    case "=": UtilTaula.inicialitzaPasVianants(taula);
+                        break;
+                    // Caràcter per cridar inicialitzaZebra
+                    case "||": UtilTaula.inicialitzaZebra(taula);
+                        break;
+                    // Caràcter per cridar inicialitzaEscacs
+                    case "++": UtilTaula.inicialitzaEscacs(taula);
+                        break;
+                    // Per defecte cridar inicialitzaFalse
+                    default: UtilTaula.inicialitzaFalse(taula);
+                }
+                // Declarar e inicialitzar el String resultat amb la funció taulaToString
+                String resultat = UtilTaula.taulaToString(taula, 'X', '·');
+                // Mostrar la taula cridat a especificacio convertida a String
+                System.out.println(resultat);
+            }
+        }
+    }
+
+    public static int obteFiles(String especificacio) {
+        /* Aquesta funció espera l'especificació de la forma d'una taula.
+         * En cas que l'especificació sigui correcta, retornarà l'enter
+         * corresponent als primers dígits. Per exemple, si especificacio
+         * és "12x5", retornarà 12.
+         * Altrament, retornarà el valor -1.  */
+         
+        String files = "";   
+        for (int i = 0; i < especificacio.length(); i++) {
+            char c = especificacio.charAt(i);
+            if (Character.isDigit(c)) {          
+                files += c;
+            } else if (!files.isEmpty()) {
+                if (Integer.parseInt(files) > 0 && Integer.parseInt(files) <= 99) {
+                    return Integer.parseInt(files);            
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static int obteColumnes(String especificacio) {
+        /* Aquesta funció espera l'especificació de la forma d'una taula.
+         * En cas que l'especificació sigui correcta, retornarà l'enter
+         * corresponent als segons dígits. Per exemple, si especificacio
+         * és "12x5", retornarà 5.
+         * Altrament, retornarà el valor -1.  */
+        
+        String columnes = "";
+        for (int i = 0; i < especificacio.length(); i++) {
+            char c = especificacio.charAt(i);
+            if (c == 'x') {
+                for (int j = i+1; j < especificacio.length(); j++) {
+                    char ch = especificacio.charAt(j);
+                    if (Character.isDigit(ch)) {
+                        columnes += ch;
+                    }
+                }
+            }
+            if (columnes.length() > 2) {
+                columnes = columnes.substring(0, 2);
+                break;
+            }
+        }
+        if (!columnes.isEmpty()) {
+            if (Integer.parseInt(columnes) > 0 && Integer.parseInt(columnes) <= 99) {
+                return Integer.parseInt(columnes);            
+            }
+        }
+        return -1;        
+    }
+}
