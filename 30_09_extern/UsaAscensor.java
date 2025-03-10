310b534146d54447aa4beceda718e607718c29e5
prgtest <jeironespinal0306@gmail.com>
2025-03-10 12:26:20 +0100

    Completat l'exercici 30_09  ∾ prgtest autocommit 30_09 #1

--- 30_09_extern/UsaAscensor.java
+++ 30_09_extern/UsaAscensor.java
@@ -8,6 +8,6 @@ public class UsaAscensor {
         Ascensor ascensor = new Ascensor();
         
         // Mostrar el pis de l'ascensor creat des d'aquí
-        System.out.println("L' ascensor creat des de fora està al pis " + ascensor.pis);
+        System.out.println("L'ascensor creat des de fora està al pis " + ascensor.pis);
     }
 }
