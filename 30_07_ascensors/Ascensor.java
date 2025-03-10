8227278ac2c0bdf9032d59a5976623338b26123e
jeironEC <jeironespinal0306@gmail.com>
2025-03-10 12:12:21 +0100

    Completat l'exercici 30_07

---
+++ 30_07_ascensors/Ascensor.java
@@ -0,0 +1,36 @@
+/* 
+ * Programa que crear un array de ascensor i instancia a cadascun de els, 
+ * a més li assigna el nombre de l'ascensor al pis de cadascun de els.
+ */
+
+public class Ascensor {
+    // Propietats
+    // pis
+    int pis = 0;
+
+    public static Ascensor[] creaAscensorsEnEscala(int quants) {
+        // Crear l'array de ascensor
+        Ascensor[] ascensors = new Ascensor[quants];
+        // Iterar tots els ascensors
+        for (int i = 0; i < ascensors.length; i++) {
+            // Crear una instancia (objecte) per cada ascensor
+            ascensors[i] = new Ascensor();
+            // Assignar-li el valor de i a cada pis de cada ascensor
+            ascensors[i].pis = i;
+        }
+        // Retornar l'ascensor
+        return ascensors;
+    }
+
+    public static void main(String[] args){
+        // Agafar el primer argument de la línea de comanda
+        int quants = Integer.parseInt(args[0]);
+        // Cridar al módul que crea l'array d'ascensors
+        Ascensor[] ascensors = creaAscensorsEnEscala(quants);
+        // Iterar tots els ascensors
+        for (int i = 0; i < ascensors.length; i++) {
+            // Mostrar cada ascensor i en qui pis es troba
+            System.out.printf("Ascensor %d al pis %d%n", i, ascensors[i].pis);
+        }
+    }
+}
