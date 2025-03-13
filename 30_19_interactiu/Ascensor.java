464a22ed83d0202dc5aee2f2af2feef6040f28ea
prgtest <jeironespinal0306@gmail.com>
2025-03-13 22:14:15 +0100

    Completat l'exercici 30_19  ∾ prgtest autocommit 30_19 #4

--- 30_19_interactiu/Ascensor.java
+++ 30_19_interactiu/Ascensor.java
@@ -135,7 +135,7 @@ public class Ascensor {
     
     // Mètode que canviar de pis a l'ascensor segon el moviment
     public int seguentPis() {
-        if (!esAturat() && pis <= 10) {
+        if (esPujant() && pis <= 10) {
             pis = pis +1;
             
             if (pis > 10) {
@@ -144,7 +144,7 @@ public class Ascensor {
             }
         }
 
-        if (!esAturat() && pis >= -1) {
+        if (esBaixant() && pis >= -1) {
             pis = pis -1;
             
             if (pis < -1) {
