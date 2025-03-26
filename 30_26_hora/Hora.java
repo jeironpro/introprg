32447388d58642ec6cdb71957bc13a195c3f3659
jeironEC <jeironespinal0306@gmail.com>
2025-03-26 23:28:38 +0100

    Completat l'exercici 30_26

--- 30_26_hora/Hora.java
+++ 30_26_hora/Hora.java
@@ -238,7 +238,7 @@ public class Hora {
 
     public static void main(String[] args) {
         Hora hora1 = new Hora();
-        Hora hora2 = new Hora(0, 59, 59);
+        Hora hora2 = new Hora(0, 0, 2);
 
         System.out.printf("Inicialment hora1: %s %s hora2: %s%n",
                             hora1,
