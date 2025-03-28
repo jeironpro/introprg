310911c8ca2602ffed3f8d5b1c61fafe746c18bb
jeironEC <jeironespinal0306@gmail.com>
2025-03-28 11:57:34 +0100

    Aquesta és una prova

---
+++ 30_34_garfield/Gat.java
@@ -0,0 +1,120 @@
+/*
+ *
+ */
+
+class Gat {
+    // Propietats de la classe
+    // Propietat privat nom
+    private final String nom;
+    // Propietat privat vides
+    private int vides;
+    // Propietat privat posicio
+    private String posicio;
+    
+    public Gat(String nom) {
+        if (nom != null && !nom.isEmpty()) {
+            this.nom = nom;        
+        } else {
+            this.nom = "anònim"; 
+        }
+        this.setVides(9);
+        this.setPosicio("estirat");
+    }
+    
+    public Gat(String nom, int vides) {
+        if (nom != null && !nom.isEmpty()) {
+            this.nom = nom;        
+        } else {
+            this.nom = "anònim"; 
+        }
+        this.setVides(vides);
+    }
+    
+    public Gat(String nom, int vides, String posicio) {
+        if (nom != null && !nom.isEmpty()) {
+            this.nom = nom;        
+        } else {
+            this.nom = "anònim"; 
+        }
+        this.setVides(vides);
+        this.setPosicio(posicio);
+    }
+    
+    public String getNom() { 
+        return this.nom;
+    }
+    
+    // Mètode que permet accedeix a la propietat vides des de fora
+    public int getVides() {
+        return this.vides;
+    }
+    
+    // Mètode que permet modificar a la propietat vides des de fora
+    public void setVides(int vides) {
+        if (vides >= 0 && vides <= 9) {
+            this.vides = vides;
+        }
+    }
+    
+    // Mètode que permet accedeix a la propietat posició des de fora
+    public String getPosicio() {
+        return this.posicio;
+    }
+    
+    // Mètode que permet modificar a la propietat posicio des de fora
+    public void setPosicio(String posicio) {
+        this.posicio = posicio;
+    }
+    
+    // Mètode que verifica si renat és dret (retorna true o false)
+    public boolean esDret() {
+        if (this.posicio.equals("dret")) {
+            return true;
+        }
+        return false;
+    }
+    
+    // Mètode que verifica si renat és assegut (retorna true o false)
+    public boolean esAssegut() {
+        if (this.posicio.equals("assegut")) {
+            return true;
+        }
+        return false;
+    }
+    
+    // Mètode que verifica si renat és estirat (retorna true o false)
+    public boolean esEstirat() {
+        if (this.posicio.equals("estirat")) {
+            return true;
+        }
+        return false;
+    }
+    
+    // Mètode que verifica si el renat està assegut, si ho està mostra un missatge, sino ho està li assigna aquesta posició i mostra un missatge de que ho a fet.
+    public String aixecat() {
+        if (esDret()) {
+            return "passo de fer res";     
+        }
+        this.setPosicio("dret");
+        return "ja m'aixeco";
+    }
+    
+    // Mètode que verifica si el renat està dret, si ho està mostra un missatge, sino ho està li assigna aquesta posició i mostra un missatge de que ho a fet.
+    public String seu() {
+        if (esAssegut()) {
+            return "passo de fer res";  
+        }
+        this.setPosicio("assegut");
+        return "ja m'assec";
+    }
+    
+    // Mètode que verifica si el renat està estirat, si ho està mostra un missatge, sino ho està li assigna aquesta posició i mostra un missatge de que ho a fet.
+    public String estirat() {
+        if (esEstirat()) {
+            return "passo de fer res";  
+        }
+        this.setPosicio("estirat");
+        return "ja m'estiro";
+    }
+    
+}
---
+++ 30_34_garfield/GatRenat.java
@@ -0,0 +1,58 @@
+/*
+ *
+ */
+ 
+class GatRenat extends Gat {
+    public GatRenat() {
+        super("Renat", 7, "estirat");
+    }
+    
+    /* Constructor amb un paràmetre (vides), crida al constructor amb paràmetres
+     * i li passa l'argument que reb per vides i el valor per defecte de posicio.
+     */
+    public GatRenat(int vides) {
+        super("Renat", vides, "estirat");
+    }
+    
+    /* Constructor amb un paràmetre (posicio), crida al constructor amb paràmetres
+     * i li passa el valor per defecte de vides i l'argument que rep per posicio.
+     */
+    public GatRenat(String posicio) {
+        super("Renat", 7, posicio);
+    }
+    
+    /* Constructor amb paràmetres, fa servir this per referir-se als mètodes 
+     * accessors de la classe, per modificar les propietats. 
+     */
+    public GatRenat(int vides, String posicio) {
+        super("Renat", vides, posicio);
+    }
+    
+    // Mètode que permet modificar a la propietat vides des de fora
+    @Override
+    public void setVides(int vides) {
+        if (vides >= 0 && vides <= 7) {
+            super.setVides(vides);
+        }
+    }
+    
+    // Mètode que permet modificar a la propietat posicio des de fora
+    @Override
+    public void setPosicio(String posicio) {
+        String[] posicions = new String[] {"dret", "assegut", "estirat"};
+        
+        for (int i = 0; i < posicions.length; i++) {
+            if (posicio.equals(posicions[i])) {
+                super.setPosicio(posicio);
+            }
+        }
+    }
+    
+    /*public static void main(String[] args) {
+        GatRenat renat = new GatRenat();
+        
+        System.out.println(renat.getNom()); 
+        System.out.println(renat.getVides()); 
+        System.out.println(renat.getPosicio()); 
+    }*/
+}
