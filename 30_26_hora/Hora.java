0677114af5c6cea4c331be8e730c8e04a70860e4
jeironEC <jeironespinal0306@gmail.com>
2025-03-28 19:56:35 +0100

    Completat l'exercici 30_26

--- 30_26_hora/Hora.java
+++ 30_26_hora/Hora.java
@@ -1,13 +1,13 @@
 /*
- * Programa que conté una classe i tres propietats privades,
- * dos constructor, un constructor per defecte i un altre que
- * rep parámetres, els mètodes accessors per cada propietat, dos
- * mètode que permetren augmentar i disminuir el valor de un propietat
- * en especific, i dos versió mès dels mateixos mètodes que rebren
- * paràmetres, a més un mètode que compara dues instàncies de la classe. 
- * També sobreescriu amb l'anotació @override el mètode toString per 
- * representar la conversió de l'object a un String de manera personalitzada 
- * i no com ho faria java per defecte.
+ * Programa que conté:
+ * 3 propietats privades.
+ * 2 constructor, un constructor per defecte i un altre que rep parámetres i té restrincions. 
+ * els mètodes accessors per cada propietat.
+ * 2 mètode que permetren augmentar i disminuir el valor de la propietat segons.
+ * 2 versió dels mètodes anterior que rebren per paràmetre els segons a incrementar o disminuir a la propietat segons, aquests dos mètode tenen un altre que fa el seu treball.
+ * 1 mètode que compara dues hores (instàncies) de la classe. 
+ * També sobreescriu amb l'anotació @override el mètode toString per representar la conversió del valor de l'object de manera personalitzada i no com ho faria java per defecte.
+ * 1 mètode que composa un operador segons el valor que retorna el mètode que compara les dues hores.
  */
 
 public class Hora {
@@ -18,18 +18,23 @@ public class Hora {
     
     // Constructor per defecte
     public Hora() {
-        // Settear la hora per defecte
         this(0, 0, 0);
     }
     
-    // Constructor amb paràmetres
+    // Constructor amb paràmetres i restrincions
     public Hora(int hores, int minuts, int segons) {
-        // Settear les hores que rep
-        this.setHores(hores);
-        // Settear els minuts que rep
-        this.setMinuts(minuts);
-        // Settear els segons que rep
-        this.setSegons(segons);
+        if (hores >= 0 && hores <= 23) {
+            if (minuts >= 0 && minuts <= 59) {
+                if (segons >= 0 && segons <= 59) {
+                    // Settear les hores que rep
+                    this.setHores(hores);
+                    // Settear els minuts que rep
+                    this.setMinuts(minuts);
+                    // Settear els segons que rep
+                    this.setSegons(segons);
+                }
+            }
+        }
     }
     
     // Mètode accessor getter de la propietat hores
@@ -52,178 +57,137 @@ public class Hora {
     
     // Mètode accessor setter de la propietat hores
     public void setHores(int hores) {
-        // Si les hores que rep és entre 0 i 23
-        if (hores >= 0 && hores <= 23) {
-            // Setter les hores
-            this.hores = hores;
-        }
+        // Setter les hores
+        this.hores = hores;
     }
     
     // Mètode accessor setter de la propietat minuts
     public void setMinuts(int minuts) {
-        // Si els minuts que rep és entre 0 i 59
-        if (minuts >= 0 && minuts <= 59) {
-            // Settear els minuts
-            this.minuts = minuts;
-        }
+        // Settear els minuts
+        this.minuts = minuts;
     }
     
     // Mètode accessor setter de la propietat segons
     public void setSegons(int segons) {
-        // Si els segons que rep és entre 0 i 59
-        if (segons >= 0 && segons <= 59) {
-            // Settear els segons
-            this.segons = segons;
-        }
+        // Settear els segons
+        this.segons = segons;
     }
     
     // Mètode que permet incrementar 1 al valor de la propietat segons
     public void incrementa() {
-        // Si els segons actual són 59
         if (this.segons == 59) {
-            // Si els minuts són 59
             if (this.minuts == 59) {
-                // Si les hores són 59
                 if (this.hores == 23) {
-                    // Settear la hora a 23
                     this.setHores(0);
-                // Del contrari
                 } else {
-                    // Sumar-li 1 a les hores
                     this.hores++;
                 }
-                // Setter els minuts a 0
                 this.setMinuts(0);
-            // Del contrari
             } else {
-                // Restar-li 1 als minuts
                 this.minuts++;
             }
-            // Setter els segons a 0
             this.setSegons(0);
-        // Del contrari
         } else {
-            // Restar-li 1 als segons
             this.setSegons(this.segons + 1);         
         }
     }
     
     // Mètode que permet decrementar 1 al valor de la propietat segons
     public void decrementa() {
-        // Si els segons actual són 0
         if (this.segons == 0) {
-            // Si els minuts són 0
             if (this.minuts == 0) {
-                // Si les hores són 0
                 if (this.hores == 0) {
-                    // Settear la hora a 23
                     this.setHores(23);
-                // Del contrari
                 } else {
-                    // Restar-li 1 a les hores
                     this.hores--;
                 }
-                // Setter els minuts a 59
                 this.setMinuts(59);
-            // Del contrari
             } else {
-                // Restar-li 1 als minuts
                 this.minuts--;
             }
-            // Setter els segons a 59
             this.setSegons(59);
-        // Del contrari
         } else {
-            // Restar-li 1 als segons
             this.setSegons(this.segons - 1);         
         }
     }
     
     // Mètode que permet incrementar el segons que rep a la propietat segons
-    public void incrementa(int segon) {
-        // Cridar el procediment que fa el treball
-        incredecre(segon, "incrementa");
+    public void incrementa(int segons) {
+        if (segons == 1) {
+            incrementa();
+        } else {
+            // Cridar el procediment que fa el treball
+            incredecre(segons, "incrementa");
+        }
     }
     
     // Mètode que permet decrementar el segons que rep a la propietat segons
-    public void decrementa(int segon) {
-        // Cridar el procediment que fa el treball
-        incredecre(segon, "decrementa");
+    public void decrementa(int segons) {
+        if (segons == 1) {
+            decrementa();
+        } else {
+            // Cridar el procediment que fa el treball
+            incredecre(segons, "decrementa");
+        }
     }
     
-    public void incredecre(int segon, String accio) {
-        // Formula per convertir la hora a segons
-        int segonsEnHoraActual = (hores * 3600) + (minuts * 60) + segons;
+    // Mètode que fa el treball de els mètodes incrementa(int) i decrementa(int)
+    public void incredecre(int segons, String accio) {
+        int segonsEnHoraActual = this.hores * 3600 + this.minuts * 60 + this.segons;
         
         if (accio.equals("decrementa")) {
-            // Restar els segons a decrementar
-            segonsEnHoraActual -= segon; 
+            segonsEnHoraActual -= segons; 
         }
         
         if (accio.equals("incrementa")) {
-            // Sumar els segons a aumentar
-            segonsEnHoraActual += segon;
+            segonsEnHoraActual += segons;
+        }
+
+        if (segonsEnHoraActual < 0) {
+            while (segonsEnHoraActual < 0) {
+                segonsEnHoraActual += 86400;
+            }
+        } else if (segonsEnHoraActual >= 86400) {
+            segonsEnHoraActual -= 86400;
         }
         
-        // A partir dels segons restant/acumulat construeix la hora
-        // Calcular quantes hores hi ha en els segons
-        int horesSet = segonsEnHoraActual / 3600;
-        // Calcular el residuo de les hores
-        // Settear la hora
-        this.setHores(horesSet);
-        
-        // Calcular els minuts a partir del residuo de les hores
-        int minutsSet = (segonsEnHoraActual % 3600) / 60;
-        // Settear els minuts
-        this.setMinuts(minutsSet);
-        
-        // Calcular el residuo del segons
-        int segonsSet = (segonsEnHoraActual % 3600) % 60;
-        // Settear els segons
-        this.setSegons(segonsSet);
+        this.setHores(segonsEnHoraActual / 3600);
+        this.setMinuts((segonsEnHoraActual % 3600) / 60);
+        this.setSegons((segonsEnHoraActual % 3600) % 60);
     }
     
     // Mètode que compara dues hores (instàncies)
     public int compareTo(Hora hora) {
-        // Obtenir les hores de la instància
-        int hora1 = this.getHores();
-        // Obtenir els minuts de la instància
-        int minut1 = this.getMinuts();
-        // Obtenir els segons de la instància
-        int segon1 = this.getSegons();
+        int hores1 = this.getHores();
+        int minuts1 = this.getMinuts();
+        int segons1 = this.getSegons();
         
-        // Obtenir les hores de la instància rebuda
-        int hora2 = hora.hores;
-        // Obtenir els minuts de la instància rebuda
-        int minut2 = hora.minuts;
-        // Obtenir els segons de la instància rebuda
-        int segon2 = hora.segons;
+        int hores2 = hora.hores;
+        int minuts2 = hora.minuts;
+        int segons2 = hora.segons;
         
-        // Verificar si la hora completa de la instancia és menor a la de la instància rebuda
-        if (segon1 < segon2 || minut1 < minut2 || hora1 < hora2) {
-            // Retornar -1
+        if (segons1 < segons2 || minuts1 < minuts2 || hores1 < hores2) {
             return -1;
         }
         
-        // Verificar si la hora completa de la instancia és major a la de la instància rebuda
-        if (segon1 > segon2 || minut1 > minut2 || hora1 > hora2) {
-            // Retornar 1
+        if (segons1 > segons2 || minuts1 > minuts2 || hores1 > hores2) {
             return 1;
         }
         
-        // Si es cap de les condicins anteriors, són iguals 
-        // Retornar 0
         return 0;
     }
     
-    // Anotació
     @Override
-    // Mètode convertidor a String
+    /* Mètode sobreescrit per personalitza el valor que mostra per pantalla la
+     * instancia (objecte).
+     */
     public String toString() {
-        // Retornar un String personalitzat
         return String.format("%d:%02d:%02d", hores, minuts, segons);
     }
     
+    /* Mètode que composa un operador de aquest (<, >, ==) segons el valor que
+     * retorna el mètode compareTo.
+     */
     private static String composaOperadorComparacio(Hora hora1, Hora hora2) {
         int comparacio = hora1.compareTo(hora2);
 
@@ -247,8 +211,8 @@ public class Hora {
 
         System.out.println("Incrementem 1 segon a la primera i decrementem 1 segon a la segona");
 
-        //hora1.incrementa();
-        hora2.decrementa(3600);
+        hora1.incrementa();
+        hora2.decrementa();
 
         System.out.printf("Finalment hora1: %s %s hora2: %s%n", 
                             hora1,
