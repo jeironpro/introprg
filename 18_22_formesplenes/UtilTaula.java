eb136285ae83ca5051f9ab4863d5597f7aaa8f5f
prgtest <jeironespinal0306@gmail.com>
2025-04-13 00:23:37 +0200

    Intent de completar aquest exercici  ∾ prgtest autocommit 18_22 #10

--- 18_22_formesplenes/UtilTaula.java
+++ 18_22_formesplenes/UtilTaula.java
@@ -513,9 +513,14 @@ public class UtilTaula {
     }
     
     public static void inicialitzaCreuOPle(boolean[][] taula) {
-        final int N_FILES = taula.length;
-        final int N_COLS = taula[0].length;
-        final float M_COLS = Math.round(taula[0].length/2.00)-1;
+        int N_FILES = taula.length;
+        int N_COLS = taula[0].length;
+        int M_FILES = (taula.length/2);
+        int M_COLS = (taula[0].length/2);
+        
+        if (N_FILES > N_COLS) {
+            N_FILES = N_COLS;
+        }
         
         for (int fila = 0; fila < N_FILES; fila++) {
             for (int col = 0; col < N_COLS; col++) {
