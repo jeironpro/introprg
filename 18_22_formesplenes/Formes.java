e3c69340f7499928a31b3b1f8cc154ffecc4da2b
prgtest <jeironespinal0306@gmail.com>
2025-04-13 14:14:32 +0200

    Canvis en UtilTaula  ∾ prgtest autocommit 18_22 #1

--- 18_22_formesplenes/Formes.java
+++ 18_22_formesplenes/Formes.java
@@ -8,106 +8,18 @@ public class Formes {
             System.out.println(args[i]);
             int files = obteFiles(args[i]);
             int columnes = obteColumnes(args[i]);
+            String forma = obteForma(args[i]);
             
             if (files < 1 || columnes < 1) {
                 System.out.println("Especificació no vàlida");
             } else {
                 boolean[][] taula = new boolean[files][columnes];
-                String especificacioTmp = "";
-                String especificacio = "";
-                String especificacions = args[i];
-                
-                for (int j = especificacions.length()-1; j >= 0; j--) {
-                    char c = especificacions.charAt(j);
-                    if (!Character.isDigit(c)) {
-                        especificacioTmp += c;
-                    } else {
-                        break;
-                    }
-                }
-                
-                for (int j = especificacioTmp.length()-1; j >= 0; j--) {
-                    char c = especificacioTmp.charAt(j);
-                    if (!Character.isDigit(c)) {
-                        especificacio += c;
-                    }
-                }
-                
-                switch (especificacio) {
-                    case "\\": UtilTaula.inicialitzaPrimeraDiagonal(taula);
-                        break;
-                    case "|": UtilTaula.inicialitzaVerticalMig(taula);
-                        break;
-                    case "-": UtilTaula.inicialitzaHoritzontalMig(taula);
-                        break;
-                    case "+": UtilTaula.inicialitzaQuarts(taula);
-                        break;
-                    case "/": UtilTaula.inicialitzaSegonaDiagonal(taula);
-                        break;
-                    case "x": UtilTaula.inicialitzaCreu(taula);
-                        break; 
-                    case "=": UtilTaula.inicialitzaPasVianants(taula);
-                        break;
-                    case "||": UtilTaula.inicialitzaZebra(taula);
-                        break;
-                    case "++": UtilTaula.inicialitzaEscacs(taula);
-                        break;
-                    case "*\\": UtilTaula.inicialitzaPrimeraDiagonalPrimerPle(taula);
-                        break;
-                    case "\\*": UtilTaula.inicialitzaPrimeraDiagonalSegonPle(taula);
-                        break;
-                    case "*/": UtilTaula.inicialitzaSegonaDiagonalPrimerPle(taula);
-                        break;
-                    case "/*": UtilTaula.inicialitzaSegonaDiagonalSegonPle(taula);
-                        break;
-                    case "*|": UtilTaula.inicialitzaVerticalMigPrimerPle(taula);
-                        break;
-                    case "|*": UtilTaula.inicialitzaVerticalMigSegonPle(taula);
-                        break;
-                    case "*-": UtilTaula.inicialitzaHoritzontalMigPrimerPle(taula);
-                        break;
-                    case "-*": UtilTaula.inicialitzaHoritzontalMigSegonPle(taula);
-                        break;
-                    case "*+": UtilTaula.inicialitzaQuartsNOPle(taula);
-                        break;
-                    case "**+": UtilTaula.inicialitzaQuartsSOPle(taula);
-                        break;
-                    case "+*": UtilTaula.inicialitzaQuartsNEPle(taula);
-                        break;
-                    case "+**": UtilTaula.inicialitzaQuartsSEPle(taula);
-                        break;
-                    case "*+**": UtilTaula.inicialitzaQuartsNOSEPlens(taula);
-                        break;
-                    case "**+*": UtilTaula.inicialitzaQuartsSONEPlens(taula);
-                        break;
-                    case "*x": UtilTaula.inicialitzaCreuNPle(taula);
-                        break;
-                    case "**x": UtilTaula.inicialitzaCreuOPle(taula);
-                        break;
-                    case "x*": UtilTaula.inicialitzaCreuSPle(taula);
-                        break;
-                    case "x**": UtilTaula.inicialitzaCreuEPle(taula);
-                        break;
-                    case "*x*": UtilTaula.inicialitzaCreuNSPlens(taula);
-                        break;
-                    case "**x**": UtilTaula.inicialitzaCreuOEPlens(taula);
-                        break;
-                    default: UtilTaula.inicialitzaFalse(taula);
-                }
-                String resultat = UtilTaula.taulaToString(taula, 'X', '·');
-                System.out.println(resultat);
+                mostraForma(taula, forma);
             }
         }
     }
 
     public static int obteFiles(String especificacio) {
-        /* Aquesta funció espera l'especificació de la forma d'una taula.
-         * En cas que l'especificació sigui correcta, retornarà l'enter
-         * corresponent als primers dígits. Per exemple, si especificacio
-         * és "12x5", retornarà 12.
-         * Altrament, retornarà el valor -1.  
-         */
-         
         String files = "";   
         for (int i = 0; i < especificacio.length(); i++) {
             char c = especificacio.charAt(i);
@@ -118,21 +30,15 @@ public class Formes {
             }
         }
         if (!files.isEmpty()) {
-            if (Integer.parseInt(files) >= 1 && Integer.parseInt(files) <= 99) {
-                return Integer.parseInt(files);            
+            int fila = Integer.parseInt(files);
+            if (fila >= 1 && fila <= 99) {
+                return fila;            
             }
         }
         return -1;
     }
 
     public static int obteColumnes(String especificacio) {
-        /* Aquesta funció espera l'especificació de la forma d'una taula.
-         * En cas que l'especificació sigui correcta, retornarà l'enter
-         * corresponent als segons dígits. Per exemple, si especificacio
-         * és "12x5", retornarà 5.
-         * Altrament, retornarà el valor -1.  
-         */
-        
         String columnes = "";
         for (int i = 0; i < especificacio.length(); i++) {
             char c = especificacio.charAt(i);
@@ -150,10 +56,99 @@ public class Formes {
             }
         }
         if (!columnes.isEmpty()) {
-            if (Integer.parseInt(columnes) >= 1 && Integer.parseInt(columnes) <= 99) {
-                return Integer.parseInt(columnes);            
+            int col = Integer.parseInt(columnes);
+            if (col >= 1 && col <= 99) {
+                return col;           
             }
         }
         return -1;        
+    }
+    
+    public static String obteForma(String especificacio) {
+        String formaTmp = "";
+        String forma = "";
+        
+        for (int i = especificacio.length()-1; i >= 0; i--) {
+            char c = especificacio.charAt(i);
+            if (!Character.isDigit(c)) {
+                formaTmp += c;
+            } else {
+                break;
+            }
+        }
+        
+        for (int i = formaTmp.length()-1; i >= 0; i--) {
+            char c = formaTmp.charAt(i);
+            if (!Character.isDigit(c)) {
+                forma += c;
+            }
+        }
+        return forma;
+    }
+    
+    public static void mostraForma(boolean[][] taula, String forma) {
+        switch (forma) {
+            case "\\": UtilTaula.inicialitzaPrimeraDiagonal(taula);
+                break;
+            case "|": UtilTaula.inicialitzaVerticalMig(taula);
+                break;
+            case "-": UtilTaula.inicialitzaHoritzontalMig(taula);
+                break;
+            case "+": UtilTaula.inicialitzaQuarts(taula);
+                break;
+            case "/": UtilTaula.inicialitzaSegonaDiagonal(taula);
+                break;
+            case "x": UtilTaula.inicialitzaCreu(taula);
+                break; 
+            case "=": UtilTaula.inicialitzaPasVianants(taula);
+                break;
+            case "||": UtilTaula.inicialitzaZebra(taula);
+                break;
+            case "++": UtilTaula.inicialitzaEscacs(taula);
+                break;
+            case "*\\": UtilTaula.inicialitzaPrimeraDiagonalPrimerPle(taula);
+                break;
+            case "\\*": UtilTaula.inicialitzaPrimeraDiagonalSegonPle(taula);
+                break;
+            case "*/": UtilTaula.inicialitzaSegonaDiagonalPrimerPle(taula);
+                break;
+            case "/*": UtilTaula.inicialitzaSegonaDiagonalSegonPle(taula);
+                break;
+            case "*|": UtilTaula.inicialitzaVerticalMigPrimerPle(taula);
+                break;
+            case "|*": UtilTaula.inicialitzaVerticalMigSegonPle(taula);
+                break;
+            case "*-": UtilTaula.inicialitzaHoritzontalMigPrimerPle(taula);
+                break;
+            case "-*": UtilTaula.inicialitzaHoritzontalMigSegonPle(taula);
+                break;
+            case "*+": UtilTaula.inicialitzaQuartsNOPle(taula);
+                break;
+            case "**+": UtilTaula.inicialitzaQuartsSOPle(taula);
+                break;
+            case "+*": UtilTaula.inicialitzaQuartsNEPle(taula);
+                break;
+            case "+**": UtilTaula.inicialitzaQuartsSEPle(taula);
+                break;
+            case "*+**": UtilTaula.inicialitzaQuartsNOSEPlens(taula);
+                break;
+            case "**+*": UtilTaula.inicialitzaQuartsSONEPlens(taula);
+                break;
+            case "*x": UtilTaula.inicialitzaCreuNPle(taula);
+                break;
+            case "**x": UtilTaula.inicialitzaCreuOPle(taula);
+                break;
+            case "x*": UtilTaula.inicialitzaCreuSPle(taula);
+                break;
+            case "x**": UtilTaula.inicialitzaCreuEPle(taula);
+                break;
+            case "*x*": UtilTaula.inicialitzaCreuNSPlens(taula);
+                break;
+            case "**x**": UtilTaula.inicialitzaCreuOEPlens(taula);
+                break;
+            default: UtilTaula.inicialitzaFalse(taula);
+        }
+        String resultat = UtilTaula.taulaToString(taula, 'X', '·');
+        System.out.println(resultat);
     }
 }
