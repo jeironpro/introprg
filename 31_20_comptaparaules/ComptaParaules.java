ad793204d6ca6a9346a3e1cf0ae9cc046c136481
jeironpro <jeironprogrammer@gmail.com>
2025-05-05 12:15:58 +0200

    Completat l'exercici 31_20

---
+++ 31_20_comptaparaules/ComptaParaules.java
@@ -0,0 +1,39 @@
+/*
+ * Programa que demana texts i compta les vegades que s'han introduït cada
+ * paraula del text.
+ */
+ 
+import java.util.Map;
+import java.util.HashMap;
+
+public class ComptaParaules {
+	public static void main(String[] args) {
+		Map<String, Integer> paraules = new HashMap<>();
+				
+		while(true) {
+			String text = Entrada.readLine();
+			
+			if (text.isBlank()) {
+				break;
+			}
+			text = text.toLowerCase();
+			String paraula = "";
+			for (int i = 0; i < text.length(); i++) {
+				char c = text.charAt(i);
+				
+				if (Character.isLetter(c)) {
+					paraula += c;
+				} else if (!paraula.isBlank()) {
+					paraula = UtilString.filtraVocalsCatalaV2(paraula);
+					if (paraules.get(paraula) == null) {
+						paraules.put(paraula, 0);
+					}
+					paraules.put(paraula, paraules.get(paraula) + 1);
+					System.out.printf("%s -> %d%n", paraula, paraules.get(paraula));
+					paraula = "";
+				}
+			}
+		}
+		System.out.println("Adéu");
+	}
+}
