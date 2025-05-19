dfd2e24b1a15ea1e7fd43a53b916ecfc28ef0d97
jeironpro <jeironprogrammer@gmail.com>
2025-05-19 17:31:53 +0200

    Completat l'exercici 22_04

---
+++ 22_04_id/Zoo.java
@@ -0,0 +1,131 @@
+/**
+ * Classe que es fa servir d'entorn per gestionar una base de dades fent 
+   connexio, desconnexio i creació, eliminació de taules i recuperació de dades. 
+ */
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.LinkedList;
+
+public class Zoo {
+	private static final String NOM_BASE_DE_DADES = "animals.bd";
+    private static final String CADENA_DE_CONNEXIO = "jdbc:sqlite:" +
+                                                     NOM_BASE_DE_DADES;
+	private Connection conn = null;
+	
+	public void connecta() throws SQLException {
+        if (conn != null) return;
+        conn = DriverManager.getConnection(CADENA_DE_CONNEXIO);
+    }
+	
+	public void desconnecta() throws SQLException {
+		if (conn == null) return;
+		conn.close();
+		conn = null;
+	}
+	
+	public void creaTaulaCategories() throws SQLException {
+		eliminaTaulaCategories();
+		String sentencia = "CREATE TABLE CATEGORIES (" +
+						   "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+						   " nom VARCHAR(40))";
+	    Statement st = null;
+	    
+	    try {
+	    	st = conn.createStatement();
+	    	st.executeUpdate(sentencia);
+	    } finally {
+	    	if (st != null) {
+	    		st.close();
+	    	}
+	    }
+	}
+	
+	public void creaTaulaAnimals() throws SQLException {
+		eliminaTaulaAnimals();
+	}
+	
+	public void eliminaTaulaCategories() throws SQLException {
+		String sentencia = "DROP TABLE IF EXISTS CATEGORIES";
+		Statement st = null;
+		
+		try {
+			st = conn.createStatement();
+			st.executeUpdate(sentencia);
+		} finally {
+			if (st != null) {
+				st.close();
+			}
+		}
+	}
+	
+	public void afegeixCategoria(Categoria categoria) throws SQLException {
+		String sql = String.format(
+		        "INSERT INTO CATEGORIES (nom) VALUES ('%s')",
+		        categoria.getNom());
+		Statement st = null;
+
+		try {
+		    st = conn.createStatement();
+		    st.executeUpdate(sql);
+		    ResultSet rs = st.getGeneratedKeys();
+		    rs.next();
+		    int id = rs.getInt(1);
+		    categoria.setId(id);
+		} finally {
+		    if (st != null) {
+		        st.close();
+		    }
+		}
+	}
+	
+	public List<Categoria> recuperaCategories() throws SQLException {
+		String sql = "SELECT * FROM CATEGORIES ORDER BY nom";
+		Statement st = null;
+
+		try {
+		    st = conn.createStatement();
+		    ResultSet rs = st.executeQuery(sql);
+		    List<Categoria> categories = new LinkedList<>();
+
+		    while (rs.next()) {
+		        int bdId = rs.getInt("id");
+		        String nom = rs.getString("nom");
+		        Categoria categoria = new Categoria(bdId, nom);
+		        categories.add(categoria);
+		    }
+		    rs.close();
+		    return categories;
+		} finally {
+		    if (st != null) {
+		        st.close();
+		    }
+		}
+	}
+	
+	public String obteCategoriaPerNom(String nom) throws SQLException {
+		String sentencia = "SELECT id FROM CATEGORIES WHERE nom = " + "'" + nom + "'" + " ORDER BY id LIMIT 1;";
+		Statement st = null;
+		
+		try {
+			st = conn.createStatement();
+			ResultSet rs = st.executeQuery(sentencia);
+			
+	        int bdId = rs.getInt("id");
+	        
+	        if (bdId > 0) {
+			    Categoria categoria = new Categoria(bdId, nom);
+			    return categoria.toString();
+	        }
+	        return null;
+		} finally {
+			if (st != null) {
+				st.close();
+			}
+		}
+	}
+}
