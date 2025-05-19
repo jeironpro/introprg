acc4b683ab7ffb7e2d43db18d6ebe1c08b5f9ef5
jeironpro <jeironprogrammer@gmail.com>
2025-05-19 13:05:07 +0200

    Completat l'exercici 43_07

---
+++ 43_07_excepcions/Punt.java
@@ -0,0 +1,65 @@
+/**
+ * Classe Punt. En aquesta se faran testing, pel que s'anirá incrementant el 
+   seu codi.
+   De moment tenint un constructor per defecte implicit, i dos getters.
+ *
+ * Afegint les propietats de tipus enter x i y. A més el setter per cadascuna. 
+ * Afegint un constructor específic i un nou mètode que suma les coordenades de
+   dues instàncies; per exemple p1.x += p2.x i  p1.y += p2.y.
+ * Afegint el mètode toString sobrescrit per mostrar el valor de les instàncies
+   personalitzat i no la referència com ho fa java per defecte.
+ * Afegint el mètode suma que reb un Punt i el suma els valor a la instància
+   actual. A més s'ha sobreescrit el mètode equals per permet igualtat entre dues
+   instància de Punt que tinguin les mateixes coordenades.
+ * Afegint una propietat de tipus array de int. El valor de coord[0] és la 
+   coordenada x i el valor de coord[1] és la coordenada y.
+ * Afegint excepcions al constructor específic.
+ */
+
+public class Punt {
+	private int[] coord = new int[2];
+	
+	public Punt() {}
+	
+	public Punt(int x, int y) {
+		if (x < 0  || y <  0) {
+			throw new IllegalArgumentException("Ha de ser un valor positiu");
+		}
+		this.coord[0] = x;
+		this.coord[1] = y;
+	}
+	
+    public int getX() { 
+    	return coord[0]; 
+	}
+	
+	public void setX(int x) {
+		this.coord[0] = x;
+	}
+	
+	public int getY() {
+		return coord[1];
+	}
+	
+	public void setY(int y) {
+		this.coord[1] = y;
+	}
+	
+	public void suma(Punt p) {
+		coord[0]+=p.getX();
+		coord[1]+=p.getY();
+	}
+	
+	public String toString() {
+		return String.format("Punt(%d, %d)", coord[0], coord[1]);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Punt)) {
+			return false;
+		}
+		Punt altre = (Punt) obj;
+		return this.coord[0] == altre.getX() && this.coord[1] == altre.getY();
+	}
+}
---
+++ 43_07_excepcions/TestPunt.java
@@ -0,0 +1,64 @@
+/**
+ * Classe per testejar la classe Punt
+ */
+ 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestPunt {
+    @Test
+    public void constructor() {
+        new Punt();
+    }
+    
+    @Test
+	public void constructorEspecific() {
+		Punt p = new Punt(1, 2);
+		assertEquals(1, p.getX());
+		assertEquals(2, p.getY());
+    }
+    
+    @Test
+    public void constructorDefecteXZero() {
+        Punt p = new Punt();
+        assertEquals(0, p.getX());
+    }
+    
+    @Test
+    public void constructorDefecteYZero() {
+    	Punt p = new Punt();
+    	assertEquals(0, p.getY());
+    }
+    
+    @Test
+    public void setX42() {
+    	Punt p = new Punt();
+    	p.setX(42);
+    	assertEquals(42, p.getX());
+    }
+    
+    @Test
+    public void setY42() {
+    	Punt p = new Punt();
+    	p.setY(42);
+    	assertEquals(42, p.getY());
+    }
+    
+    @Test
+    public void sumaPunt() {
+    	Punt p1 = new Punt(5, 9);
+    	Punt p2 = new Punt(3, 7);
+    	p1.suma(p2);
+    	assertAll (
+		    () -> assertEquals(8, p1.getX()),
+		    () -> assertEquals(16, p1.getY())
+		);
+    }
+    
+    @Test
+    public void ConstructorEspecificAmbValorNegatiu() {
+    	assertThrows(IllegalArgumentException.class, () -> {
+    		new Punt(-5, 9);
+    	});
+    }
+}
